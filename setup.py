37e084724a9ca923ccdaaf6ef1bf66ab7139ce21
David Wolever <david@wolever.net>
2012-03-11 14:23:37 -0700

    Adding license and other things to setup.py, bump version to 0.1.1

--- setup.py
+++ setup.py
@@ -9,11 +9,28 @@ os.chdir(os.path.dirname(sys.argv[0]) or ".")
 
 setup(
     name="pip2pi",
-    version="0.1",
+    version="0.1.1",
     url="https://github.com/wolever/pip2pi",
     author="David Wolever",
     author_email="david@wolever.net",
-    description="pip2pi builds a PyPI-compatible package repository from pip requirements",
+    description="""
+        pip2pi builds a PyPI-compatible package repository from pip
+        requirements
+    """,
+    long_desc=open("README.rst", "U").read(),
     packages=find_packages(),
     scripts=["pip2pi", "pip2tgz", "dir2pi"],
+    license="BSD",
+    classifiers=[ x.strip() for x in """
+        Development Status :: 4 - Beta
+        Environment :: Console
+        Intended Audience :: Developers
+        Intended Audience :: System Administrators
+        License :: OSI Approved :: BSD License
+        Natural Language :: English
+        Operating System :: OS Independent
+        Programming Language :: Python
+        Topic :: Software Development
+        Topic :: Utilities
+    """.split() if x.strip() ],
 )
