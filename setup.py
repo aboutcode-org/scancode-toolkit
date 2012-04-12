2d4597461b54b59fadcad3287a84d92a89cbde55
David Wolever <david@wolever.net>
2012-04-11 20:11:06 -0400

    Fixing bug where pypi package didn't include README.rst

--- setup.py
+++ setup.py
@@ -7,9 +7,17 @@ from setuptools import setup, find_packages
 
 os.chdir(os.path.dirname(sys.argv[0]) or ".")
 
+try:
+    long_description = open("README.rst", "U").read()
+except IOError:
+    long_description = "See https://github.com/wolever/pip2pi"
+
+import libpip2pi
+version = "%s.%s.%s" %libpip2pi.__version__
+
 setup(
     name="pip2pi",
-    version="0.1.1",
+    version="0.1.2",
     url="https://github.com/wolever/pip2pi",
     author="David Wolever",
     author_email="david@wolever.net",
@@ -17,7 +25,7 @@ setup(
         pip2pi builds a PyPI-compatible package repository from pip
         requirements
     """,
-    long_description=open("README.rst", "U").read(),
+    long_description=long_description,
     packages=find_packages(),
     scripts=["pip2pi", "pip2tgz", "dir2pi"],
     license="BSD",
