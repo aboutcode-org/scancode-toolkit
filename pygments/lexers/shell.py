62f2cb600fc98c85e45cd1d018318a88c0dd62b0
Georg Brandl <georg@python.org>
2012-08-19 11:36:11 +0200

    Closes #763: bash: support here-strings.

--- pygments/lexers/shell.py
+++ pygments/lexers/shell.py
@@ -60,6 +60,7 @@ class BashLexer(RegexLexer):
             (r'\\[\w\W]', String.Escape),
             (r'(\b\w+)(\s*)(=)', bygroups(Name.Variable, Text, Operator)),
             (r'[\[\]{}()=]', Operator),
+            (r'<<<', Operator),  # here-string
             (r'<<-?\s*(\'?)\\?(\w+)[\w\W]+?\2', String),
             (r'&&|\|\|', Operator),
         ],
