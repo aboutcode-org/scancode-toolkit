e6aa5b1ca1d323a86da8e93466520d8a0f39e44c
Robert Gobeille <bob.gobeille@hp.com>
2013-04-08 17:34:30 +0000

    New test files
    git-svn-id: http://svn.code.sf.net/p/fossology/code/trunk/fossology@6593 a5643f74-2041-0410-a2b9-94ffef304a07

---
+++ NomosTestfiles/CC/config.h
@@ -0,0 +1,51 @@
+/*
+*
+*	USB infrared remote control receiver transmitter firmware v1.0
+*	License: creative commons - attribution, share-alike 
+*	Copyright Ian Lesnet 2010
+*	http://dangerousprototypes.com
+*
+*/
+//Config words, 20Mhz oscilator, 48Mhz CPU clk 12MIPS 
+#ifndef CONFIG_H
+#define CONFIG_H
+        #pragma config PLLDIV   = 5         
+        #pragma config CPUDIV   = OSC1_PLL2   
+        #pragma config USBDIV   = 2         
+        #pragma config FOSC     = HSPLL_HS
+        #pragma config FCMEN    = OFF
+        #pragma config IESO     = OFF
+        #pragma config PWRT     = OFF
+        #pragma config BOR      = ON
+        #pragma config BORV     = 3
+        #pragma config VREGEN   = ON      
+        #pragma config WDT      = OFF
+        #pragma config WDTPS    = 32768
+        #pragma config MCLRE    = ON
+        #pragma config LPT1OSC  = OFF
+        #pragma config PBADEN   = OFF
+//      #pragma config CCP2MX   = ON
+        #pragma config STVREN   = ON
+        #pragma config LVP      = OFF
+//      #pragma config ICPRT    = OFF       
+        #pragma config XINST    = OFF       
+        #pragma config CP0      = OFF
+        #pragma config CP1      = OFF
+//      #pragma config CP2      = OFF
+//      #pragma config CP3      = OFF
+        #pragma config CPB      = OFF
+//      #pragma config CPD      = OFF
+        #pragma config WRT0     = OFF
+        #pragma config WRT1     = OFF
+//      #pragma config WRT2     = OFF
+//      #pragma config WRT3     = OFF
+        #pragma config WRTB     = OFF   
+        #pragma config WRTC     = OFF
+//      #pragma config WRTD     = OFF
+        #pragma config EBTR0    = OFF
+        #pragma config EBTR1    = OFF
+//      #pragma config EBTR2    = OFF
+//      #pragma config EBTR3    = OFF
+        #pragma config EBTRB    = OFF
+#endif
+
