e97c4cfa43c51b57dc4765991949eb81f5f67827
Robert Gobeille <bob.gobeille@hp.com>
2013-04-08 21:25:03 +0000

    new test file
    git-svn-id: http://svn.code.sf.net/p/fossology/code/trunk/fossology@6607 a5643f74-2041-0410-a2b9-94ffef304a07

---
+++ NomosTestfiles/EUPL/simRender.h
@@ -0,0 +1,46 @@
+////////////////////////////////////////////////////////////////////////////////
+//
+// trueCA Simulator                               http://truecas.sourceforge.net
+//
+// Copyright 2009 - Mario Luzeiro (mrluzeiro@ua.pt)
+//
+//  Licensed under the EUPL, Version 1.1 or - as soon they will be approved by
+//  the European Commission - subsequent versions of the EUPL (the "Licence");
+//  You may not use this work except in compliance with the Licence.
+//  You may obtain a copy of the Licence at:
+//
+//   http://ec.europa.eu/idabc/eupl
+//
+//  Unless required by applicable law or agreed to in writing, software
+//  distributed under the Licence is distributed on an "AS IS" basis,
+//  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//  See the Licence for the specific language governing permissions and
+//  limitations under the Licence.
+//
+////////////////////////////////////////////////////////////////////////////////
+//
+//  FileName:       simRender.h
+//
+//  Description:
+//
+//  Author(s):      Mario Luzeiro (mrluzeiro@ua.pt)
+//
+////////////////////////////////////////////////////////////////////////////////
+// USE TABS SIZE 2!
+////////////////////////////////////////////////////////////////////////////////
+
+
+///
+////////////////////////////////////////////////////////////////////////////////
+
+#ifndef _simRender_h_
+#define _simRender_h_
+
+void simRender_render(int viewPortW, int viewPortH);
+void simRender_setupParameters(void);
+
+#endif
+
+/// End of File ////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////
+
