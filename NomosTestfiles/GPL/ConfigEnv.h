79fe4879a5d4d038315c5bc8acce417cf09e4544
Robert Gobeille <bob.gobeille@hp.com>
2013-04-08 22:10:37 +0000

    new test files
    git-svn-id: http://svn.code.sf.net/p/fossology/code/trunk/fossology@6608 a5643f74-2041-0410-a2b9-94ffef304a07

---
+++ NomosTestfiles/GPL/ConfigEnv.h
@@ -0,0 +1,30 @@
+/*
+ * ArcEmu MMORPG Server
+ * Copyright (C) 2008-2009 <http://www.ArcEmu.org/>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+#if !defined(CONFIGENVIRONMENT_H)
+#define CONFIGENVIRONMENT_H
+
+//! Other libs we depend on.
+#include "Common.h"
+
+//! Our own includes.
+#include "Config.h"
+#include "Log.h"
+
+#endif
