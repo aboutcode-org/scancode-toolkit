a9f4346548522174409571c65594ccccc67e0e19
Robert Gobeille <bob.gobeille@hp.com>
2013-06-07 21:36:18 +0000

    New test file from Armijn
    git-svn-id: http://svn.code.sf.net/p/fossology/code/trunk/fossology@6769 a5643f74-2041-0410-a2b9-94ffef304a07

---
+++ NomosTestfiles/GPL/evd-pki-pubkey-lic.c
@@ -0,0 +1,11 @@
+
+    This library is free software; you can redistribute it and/or
+        modify it under the terms of the GNU Lesser General Public License
+            version 3, or (at your option) any later version as published by
+                the Free Software Foundation. *
+                    This library is distributed in the hope that it will be useful, but
+                        WITHOUT ANY WARRANTY; without even the implied warranty of
+                            MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+                                Lesser General Public License at http://www.gnu.org/licenses/lgpl-3.0.txt
+                                    for more details.
+
