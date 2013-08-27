48ef3979f6ad11ccc6e1cd3a700552316d756f1b
Jero Sutlovic <jsutlovic@gmail.com>
2013-08-27 17:53:00 -0400

    Escape package names

--- libpip2pi/commands.py
+++ libpip2pi/commands.py
@@ -109,7 +109,8 @@ def dir2pi(argv=sys.argv):
         symlink_target = os.path.join(pkg_dir, pkg_new_basename)
         symlink_source = os.path.join("../../", pkg_basename)
         os.symlink(symlink_source, symlink_target)
-        pkg_index += "<a href='{0}/'>{0}</a><br />\n".format(pkg_name)
+        pkg_name_html = cgi.escape(pkg_name)
+        pkg_index += "<a href='{0}/'>{0}</a><br />\n".format(pkg_name_html)
         with open(os.path.join(pkg_dir, "index.html"), "a") as fp:
             pkg_new_basename_html = cgi.escape(pkg_new_basename)
             fp.write("<a href='%s'>%s</a><br />\n"
