4f3f96b3b2ece3a55998cbb8b1eb14a2bbff3397
Georg Brandl <georg@python.org>
2015-01-21 08:33:25 +0100

    Copyright year update.

--- src/packagedcode/pygments/lexers/ruby.py
+++ src/packagedcode/pygments/lexers/ruby.py
@@ -5,7 +5,7 @@
 
     Lexers for Ruby and related languages.
 
-    :copyright: Copyright 2006-2014 by the Pygments team, see AUTHORS.
+    :copyright: Copyright 2006-2015 by the Pygments team, see AUTHORS.
     :license: BSD, see LICENSE for details.
 """
 
