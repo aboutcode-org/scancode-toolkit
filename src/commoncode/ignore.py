f8174022b0052cf71d46c0a452f08816a0e7b540
pombredanne <pombredanne@nexb.com>
2015-07-12 23:08:49 +0200

    Do not ignore files with single letter names.
     * this was due to a pattern with []

--- src/commoncode/ignore.py
+++ src/commoncode/ignore.py
@@ -88,7 +88,7 @@ ignores_MacOSX = {
     '.journal': 'Default ignore: MacOSX DMG/HFS+ artifact',
     '.journal_info_block': 'Default ignore: MacOSX DMG/HFS+ artifact',
     '.Trashes': 'Default ignore: MacOSX DMG/HFS+ artifact',
-    '[HFS+ Private Data]': 'Default ignore: MacOSX DMG/HFS+ artifact',
+    '\[HFS+ Private Data\]': 'Default ignore: MacOSX DMG/HFS+ artifact private data',
 }
 
 ignores_Windows = {
