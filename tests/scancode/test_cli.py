30b453a49bb543acaef4fdc30a638d16780b3a03
pombredanne <pombredanne@nexb.com>
2015-07-30 19:11:33 +0200

    Refined test for #43.  * Ensured that results are tested sorted

--- tests/scancode/test_cli.py
+++ tests/scancode/test_cli.py
@@ -151,6 +151,7 @@ class TestCommandLine(FileBasedTesting):
         """
         Load the result file as utf-8 JSON and strip test_dir prefix from
         locations.
+        Sort the results by location.
         """
         test_dir = as_posixpath(test_dir)
         with codecs.open(result_file, encoding='utf-8') as res:
@@ -159,7 +160,8 @@ class TestCommandLine(FileBasedTesting):
                 loc = result['location']
                 loc = as_posixpath(loc).replace(test_dir, '').strip('/')
                 result['location'] = loc
-            return scan_result
+        scan_result['results'].sort(key=lambda x: x['location'])
+        return scan_result
 
     def test_usage_and_help_return_a_correct_script_name_on_all_platforms(self):
         runner = CliRunner()
