25c154491d8f78143df3a6bdf3facea6e962d99a
pombredanne <pombredanne@nexb.com>
2015-08-06 16:43:09 +0200

    Simplified recognition for now.
     * removed class-based directory-based recognization

--- src/packagedcode/recognize.py
+++ src/packagedcode/recognize.py
@@ -38,35 +38,13 @@ logger = logging.getLogger(__name__)
 
 
 """
-Package recognizers are used to recognize one or more packages in a directory.
-
-A recognizer is a class with a "recon" method:
- - accepting a directory location as an input.
- - returning an iterable of Package objects.
-
-The contract for this method is that the location can be walked down or up
-reasonably, if and only if there is some clues found immediately in the provided
-directory location that can be confirmed by navigating up or down the tree.
-The location is always an absolute path that includes the root of the scanned
-codebase.
+Recognize packages in files or directories.
 """
 
 
-class ArchiveRecognizer(object):
+def recognize_packaged_archives(location):
     """
-    A package recognizer for archive-based packages.
-    """
-    def recon(self, location):
-        for f in  os.listdir(location):
-            loc = join(location, f)
-            parch = packaged_archive(loc)
-            if parch:
-                yield parch
-
-
-def packaged_archive(location):
-    """
-    Return a Package object if one wass recognized or None for this `location`.
+    Return a Package object if one was recognized or None for this `location`.
     """
     if not filetype.is_file(location):
         return
@@ -79,25 +57,21 @@ def packaged_archive(location):
         if not package.packaging == models.Package.as_archive:
             continue
 
-        # we default to True if there is nothing to match against
+        # Note: default to True if there is nothing to match against
+
         if package.filetypes:
             type_matched = any(t in ftype for t in package.filetypes)
         else:
             type_matched = True
-
-        # we default to True if there is nothing to match against
         if package.mimetypes:
             mime_matched = any(m in mtype for m in package.mimetypes)
         else:
             mime_matched = True
-
-        # we default to True if there is nothing to match against
         if package.extensions:
             extension_matched = location.lower().endswith(package.extensions)
         else:
             extension_matched = True
 
         if type_matched and mime_matched and extension_matched:
-            # we return the first match in the sequence:
-            # the order of PACKAGE_TYPES matters
+            # we return the first match in the order of PACKAGE_TYPES
             return package(location)
