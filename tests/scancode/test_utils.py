b9339a4aab111bc4b44f65b56582d9e5a0b8092e
pombredanne <pombredanne@nexb.com>
2015-08-08 11:57:37 +0200

    Impoved Click-based progress reporting.
     * New progressmanager decorator handling a bar or a plain line log
     * Fixed to kinks in the outputs of progress to non-tty

---
+++ tests/scancode/test_utils.py
@@ -0,0 +1,62 @@
+#
+# Copyright (c) 2015 nexB Inc. and others. All rights reserved.
+# http://nexb.com and https://github.com/nexB/scancode-toolkit/
+# The ScanCode software is licensed under the Apache License version 2.0.
+# Data generated with ScanCode require an acknowledgment.
+# ScanCode is a trademark of nexB Inc.
+#
+# You may not use this software except in compliance with the License.
+# You may obtain a copy of the License at: http://apache.org/licenses/LICENSE-2.0
+# Unless required by applicable law or agreed to in writing, software distributed
+# under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
+# CONDITIONS OF ANY KIND, either express or implied. See the License for the
+# specific language governing permissions and limitations under the License.
+#
+# When you publish or redistribute any data created with ScanCode or any ScanCode
+# derivative work, you must accompany this data with the following acknowledgment:
+#
+#  Generated with ScanCode and provided on an "AS IS" BASIS, WITHOUT WARRANTIES
+#  OR CONDITIONS OF ANY KIND, either express or implied. No content created from
+#  ScanCode should be considered or used as legal advice. Consult an Attorney
+#  for any legal advice.
+#  ScanCode is a free software code scanning tool from nexB Inc. and others.
+#  Visit https://github.com/nexB/scancode-toolkit/ for support and download.
+
+from __future__ import absolute_import, print_function
+
+import os
+
+from click.testing import CliRunner
+
+from commoncode.testcase import FileBasedTesting
+from commoncode.fileutils import as_posixpath
+
+from scancode import cli
+import codecs
+import click
+from click.termui import progressbar
+
+
+class TestUtils(FileBasedTesting):
+    test_data_dir = os.path.join(os.path.dirname(__file__), 'data')
+
+    def test_click_progressbar_with_labels(self):
+        @click.command()
+        def mycli():
+            """Sample cmd with progress bar"""
+            click.echo('Start')
+            with progressbar(range(10), label='xyz') as it:
+                for i in it:
+                    pass
+            click.echo('End')
+
+        runner = CliRunner()
+        result = runner.invoke(mycli)
+        assert result.exit_code == 0
+        expected = '''Start
+xyz
+xyz
+End
+'''
+
+        assert expected == result.output
