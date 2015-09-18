f612872f73909f312e4e20f6d322200bd40e0e78
pombredanne <pombredanne@nexb.com>
2015-09-18 16:43:45 +0200

    Added new and improved rules and test.
     * Fix for missing LGPL detection in #75 (and initially reported in #67)

---
+++ tests/licensedcode/data/licenses/lgpl-2.1_or_gpl-2.0.c
@@ -0,0 +1,18 @@
+/* 
+ * Two Levels Segregate Fit memory allocator (TLSF)
+ * Version 2.4.6
+ *
+ * Written by Miguel Masmano Tello <mimastel@doctor.upv.es>
+ *
+ * Thanks to Ismael Ripoll for his suggestions and reviews
+ *
+ * Copyright (C) 2008, 2007, 2006, 2005, 2004
+ *
+ * This code is released using a dual license strategy: GPL/LGPL
+ * You can choose the licence that better fits your requirements.
+ *
+ * Released under the terms of the GNU General Public License Version 2.0
+ * Released under the terms of the GNU Lesser General Public License Version 2.1
+ *
+ */
+
