5b16757a5d31584c72fde2bbcd31495c947d98e6
pombredanne <pombredanne@nexb.com>
2015-10-02 10:47:32 +0200

    Fixed #83: improved copyright trailing garbage detection and removal.

---
+++ tests/cluecode/data/copyrights/copyright_hostapd_trailing_available.c
@@ -0,0 +1,13 @@
+/*
+ * hostapd - command line interface for hostapd daemon
+ * Copyright (c) 2004-2005, Jouni Malinen <jkmaline@cc.hut.fi>
+ * Available under the GPL; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation.
+ *
+ * Alternatively, this software may be distributed under the terms of BSD
+ * license.
+ *
+ * See README and COPYING for more details.
+ */
+
