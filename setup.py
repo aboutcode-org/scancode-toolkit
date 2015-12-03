32c489dcdda724af0e9d01f01ab1b973bb0ae6dd
pombredanne <pombredanne@nexb.com>
2015-12-03 12:22:26 +0100

    New release 1.4.1.

--- setup.py
+++ setup.py
@@ -29,7 +29,7 @@ long_description = '%s\n%s' % (
 
 setup(
     name='scancode-toolkit',
-    version='1.4.0',
+    version='1.4.1',
     license='Apache-2.0 with ScanCode acknowledgment and CC0-1.0 and others',
     description='ScanCode is a tool to scan code for license, copyright and other interesting facts.',
     long_description=long_description,
