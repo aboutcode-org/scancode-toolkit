25d5e56f7183982379b3e2853345f16d745a1519
pombredanne <pombredanne@nexb.com>
2015-12-03 11:15:18 +0100

    Fixed missing packagedcode not included in release for #142  * also fixed #129 including the samples in the release builds.  * improved the release scripts and CI to include test of built    archive  * added release script run to the Ci test suite  * added packagedcode to CI test suites (it was missing)

--- etc/release/release.sh
+++ etc/release/release.sh
@@ -3,21 +3,69 @@
 # Copyright (c) 2015 nexB Inc. http://www.nexb.com/ - All rights reserved.
 #
 
-# A minimal release script
+# ScanCode release script
+# This script creates and tests release archives in the dist/ dir
 
 set -e
-#RELEASE_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-#cd "$RELEASE_DIR"
 
+# un-comment to trace execution
+# set -x
+
+echo "###  BUILDING ScanCode release ###"
+
+echo -n "  RELEASE: Cleaning previous release archives, then setup and config: "
 rm -rf dist/
+
 cp etc/release/MANIFEST.in.release MANIFEST.in
 cp etc/release/setup.cfg.release setup.cfg
-
 ./configure --clean
-source configure etc/conf
-python setup.py sdist --formats=bztar,zip
+export CONFIGURE_QUIET=1
+./configure etc/conf
 
+echo "  RELEASE: Building release archives..."
+
+# build a zip and tar.bz2
+bin/python setup.py --quiet sdist --formats=bztar,zip
+
+
+# Restoring initial dev setup and config...
 cp etc/release/MANIFEST.in.dev MANIFEST.in
 cp etc/release/setup.cfg.dev setup.cfg
 
+
+function test_scan {
+    file_extension=$1
+    extract_command=$2
+    for archive in *.$file_extension;
+        do
+            echo -n "    RELEASE: Testing release archive: $archive ... "
+            $($extract_command $archive)
+            extract_dir=$(ls -d */)
+            cd $extract_dir
+
+            # this is needed for the zip
+            chmod o+x scancode extractcode
+
+            # minimal test: update when new scans are available
+            ./scancode --quiet -lcip apache-2.0.LICENSE test_scan.json
+            ./scancode --quiet -lcip --format html apache-2.0.LICENSE test_scan.html
+            ./scancode --quiet -lcip --format html-app apache-2.0.LICENSE test_scan_app.html
+            ./extractcode --quiet samples/arch
+
+            # cleanup
+            cd ..
+            rm -rf $extract_dir
+            echo "    RELEASE: Success"
+        done
+}
+
+cd dist
+echo "  RELEASE: Testing..."
+test_scan bz2 "tar -xf"
+test_scan zip "unzip -q"
+
+echo "###  RELEASE is ready for publishing ###"
+
 set +e
+set +x
+
