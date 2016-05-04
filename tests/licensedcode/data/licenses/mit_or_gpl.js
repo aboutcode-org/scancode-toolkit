9e37150e7950ffa761c7a7c232142f6ddc78180b
Philippe Ombredanne <pombredanne@nexb.com>
2016-05-04 17:00:43 +0200

    #257 Detect dual mit or gpl in javascript
    Reported-by: Sebastian Schuberth

---
+++ tests/licensedcode/data/licenses/mit_or_gpl.js
@@ -0,0 +1,17 @@
+/**
+ * SyntaxHighlighter
+ * http://alexgorbatchev.com/SyntaxHighlighter
+ *
+ * SyntaxHighlighter is donationware. If you are using it, please donate.
+ * http://alexgorbatchev.com/SyntaxHighlighter/donate.html
+ *
+ * @version
+ * 3.0.83 (July 02 2010)
+ * 
+ * @copyright
+ * Copyright (C) 2004-2010 Alex Gorbatchev.
+ *
+ * @license
+ * Dual licensed under the MIT and GPL licenses.
+ */
+eval(function(p,a,c,k,e,d){e=function(c)
