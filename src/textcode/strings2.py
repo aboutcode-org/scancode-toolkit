a396eda8809bed2fcdf76543c35309a58e89392a
Philippe Ombredanne <pombredanne@nexb.com>
2016-10-30 15:59:20 +0100

    #99 refine fireeye floss strin extraction for our use case
    Signed-off-by: Philippe Ombredanne <pombredanne@nexb.com>

--- src/textcode/strings2.py
+++ src/textcode/strings2.py
@@ -1,93 +1,93 @@
+#
+# Copyright (c) 2016 Fireeye, Inc. All rights reserved.
+#
+# Modifications Copyright (c) 2016 nexB, Inc. and others. All rights reserved.
+# http://nexb.com and https://github.com/nexB/scancode-toolkit/
+# The ScanCode software is licensed under the Apache License version 2.0.
+# ScanCode is a trademark of nexB Inc.
+#
+# You may not use this software except in compliance with the License.
+# You may obtain a copy of the License at: http://apache.org/licenses/LICENSE-2.0
+# Unless required by applicable law or agreed to in writing, software distributed
+# under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
+# CONDITIONS OF ANY KIND, either express or implied. See the License for the
+# specific language governing permissions and limitations under the License.
+#
+# This file contains code derived and modified from Fireeye's flare FLOSS:
+# https://www.github.com/fireeye/flare-floss
+# The original code was taken on 2016-10-28 from:
+# https://raw.githubusercontent.com/fireeye/flare-floss/0db13aff88d0487f818f19a36de879dc27c94e13/floss/strings.py
+# modifications:
+# - do not return offsets
+# - do not check for buffers filled with a single byte
+# - removed main()
+# - do not cache compiled patterns. re does cache patterns alright.
+
+
+from __future__ import absolute_import, print_function
+
 import re
-from collections import namedtuple
 
 
-ASCII_BYTE = " !\"#\$%&\'\(\)\*\+,-\./0123456789:;<=>\?@ABCDEFGHIJKLMNOPQRSTUVWXYZ\[\]\^_`abcdefghijklmnopqrstuvwxyz\{\|\}\\\~\t"
-ASCII_RE_4 = re.compile("([%s]{%d,})" % (ASCII_BYTE, 4))
-UNICODE_RE_4 = re.compile(b"((?:[%s]\x00){%d,})" % (ASCII_BYTE, 4))
-REPEATS = ["A", "\x00", "\xfe", "\xff"]
-SLICE_SIZE = 4096
-
-String = namedtuple("String", ["s", "offset"])
+ASCII_BYTE = (
+    " !\"#\$%&\'\(\)\*\+,-\./0123456789:;<=>\?@ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    "\[\]\^_`abcdefghijklmnopqrstuvwxyz\{\|\}\\\~\t"
+)
 
 
-def buf_filled_with(buf, character):
-    dupe_chunk = character * SLICE_SIZE
-    for offset in xrange(0, len(buf), SLICE_SIZE):
-        new_chunk = buf[offset: offset + SLICE_SIZE]
-        if dupe_chunk[:len(new_chunk)] != new_chunk:
-            return False
-    return True
-
-
-def extract_ascii_strings(buf, n=4):
-    '''
-    Extract ASCII strings from the given binary data.
+def extract_ascii_strings(buf, n=3):
+    """
+    Yield Unicode strings (ASCII) from a buf string of binary data.
 
     :param buf: A bytestring.
     :type buf: str
     :param n: The minimum length of strings to extract.
     :type n: int
-    :rtype: Sequence[String]
-    '''
-
+    :rtype: Sequence[string]
+    """
     if not buf:
         return
 
-    if (buf[0] in REPEATS) and buf_filled_with(buf, buf[0]):
-        return
-
-    r = None
-    if n == 4:
-        r = ASCII_RE_4
-    else:
-        reg = "([%s]{%d,})" % (ASCII_BYTE, n)
-        r = re.compile(reg)
+    reg = '([%s]{%d,})' % (ASCII_BYTE, n)
+    r = re.compile(reg)
     for match in r.finditer(buf):
-        yield String(match.group().decode("ascii"), match.start())
+        yield match.group().decode('ascii')
 
 
-def extract_unicode_strings(buf, n=4):
-    '''
-    Extract naive UTF-16 strings from the given binary data.
+def extract_unicode_strings(buf, n=3):
+    """
+    Yield Unicode strings (UTF-16-LE encoded ASCII) from a buf string of binary data.
 
     :param buf: A bytestring.
     :type buf: str
     :param n: The minimum length of strings to extract.
     :type n: int
-    :rtype: Sequence[String]
-    '''
-
+    :rtype: Sequence[string]
+    """
     if not buf:
         return
 
-    if (buf[0] in REPEATS) and buf_filled_with(buf, buf[0]):
-        return
-
-    if n == 4:
-        r = UNICODE_RE_4
-    else:
-        reg = b"((?:[%s]\x00){%d,})" % (ASCII_BYTE, n)
-        r = re.compile(reg)
+    reg = b'((?:[%s]\x00){%d,})' % (ASCII_BYTE, n)
+    r = re.compile(reg)
     for match in r.finditer(buf):
         try:
-            yield String(match.group().decode("utf-16"), match.start())
+            yield match.group().decode('utf-16')
         except UnicodeDecodeError:
             pass
 
 
-def main():
-    import sys
+def extract_strings(buf, n=3):
+    """
+    Yield unicode strings (ASCII and UTF-16-LE encoded ASCII) from a buf string of binary data.
 
-    with open(sys.argv[1], 'rb') as f:
-        b = f.read()
+    :param buf: A bytestring.
+    :type buf: str
+    :param n: The minimum length of strings to extract.
+    :type n: int
+    :rtype: Sequence[string]
+    """
+    for s in extract_ascii_strings(buf):
+        yield s
 
-    for s in extract_ascii_strings(b):
-        print('0x{:x}: {:s}'.format(s.offset, s.s))
-
-    for s in extract_unicode_strings(b):
-        print('0x{:x}: {:s}'.format(s.offset, s.s))
-
-
-if __name__ == '__main__':
-    main()
+    for s in extract_unicode_strings(buf):
+        yield s
