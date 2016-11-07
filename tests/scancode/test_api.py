50bfc213fd6d9d734aa6afb0dc8aa58faf3fed5a
Philippe Ombredanne <pombredanne@nexb.com>
2016-11-07 09:13:18 -0800

    #267 Add test highlighting a Python pickle bug
     * somehow, some data structures cannot be pickled with HIGHEST_PROTOCOL
     * But they pickle alright with no proptocol set
    
    Signed-off-by: Philippe Ombredanne <pombredanne@nexb.com>

---
+++ tests/scancode/test_api.py
@@ -0,0 +1,49 @@
+#
+# Copyright (c) 2016 nexB Inc. and others. All rights reserved.
+# http://nexb.com and https://github.com/nexB/scancode-toolkit/
+# The ScanCode software is licensed under the Apache License version 2.0.
+# Data generated with ScanCode require an acknowledgment.
+# ScanCode is a trademark of nexB Inc.
+#
+# You may not use this software except in compliance with the License.
+# You may obtain a copy of the License at: http://apache.org/licenses/LICENSE-2.0
+# Unless required by applicable law or agreed to in writing, software distributed
+# under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
+# CONDITIONS OF ANY KIND, either express or implied. See the License for the
+# specific language governing permissions and limitations under the License.
+#
+# When you publish or redistribute any data created with ScanCode or any ScanCode
+# derivative work, you must accompany this data with the following acknowledgment:
+#
+#  Generated with ScanCode and provided on an "AS IS" BASIS, WITHOUT WARRANTIES
+#  OR CONDITIONS OF ANY KIND, either express or implied. No content created from
+#  ScanCode should be considered or used as legal advice. Consult an Attorney
+#  for any legal advice.
+#  ScanCode is a free software code scanning tool from nexB Inc. and others.
+#  Visit https://github.com/nexB/scancode-toolkit/ for support and download.
+
+from __future__ import absolute_import, print_function
+
+import os
+
+from commoncode.testcase import FileBasedTesting
+
+from scancode import api
+
+
+class TestAPI(FileBasedTesting):
+    test_data_dir = os.path.join(os.path.dirname(__file__), 'data')
+
+    def test_get_package_infos_can_pickle(self):
+        test_file = self.get_test_loc('api/package/package.json')
+        package = api.get_package_infos(test_file)
+
+        import pickle
+        import cPickle
+        try:
+            _pickled = pickle.dumps(package, pickle.HIGHEST_PROTOCOL)
+            _cpickled = cPickle.dumps(package, pickle.HIGHEST_PROTOCOL)
+            self.fail('pickle.HIGHEST_PROTOCOL used to fail to pickle this data')
+        except:
+            _pickled = pickle.dumps(package)
+            _cpickled = cPickle.dumps(package)
