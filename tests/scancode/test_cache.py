6b92ccf5340637199dcfbcb4fea7685540b58e5b
Philippe Ombredanne <pombredanne@nexb.com>
2016-11-07 09:19:25 -0800

    #267 Add test highlighting a diskcache bug
     * this is a Python pickle bug
     * somehow, some data structures cannot be pickled with HIGHEST_PROTOCOL
     * But they pickle alright with no proptocol set
    
    Signed-off-by: Philippe Ombredanne <pombredanne@nexb.com>

---
+++ tests/scancode/test_cache.py
@@ -0,0 +1,45 @@
+#
+# Copyright (c) 2016 nexB Inc. and others. All rights reserved.
+# http://nexb.com and https://github.com/nexB/scancode-toolkit/
+# The ScanCode software is licensed under the Apache License version 2.0.
+# Data generated with ScanCode require an acknowledgment.
+# ScanCode is a trademark of nexB Inc.
+#
+# You may not use this software except in compliance with the License.
+# You may obtain a copy of the License at: http://apache.org/licenses/LICENSE-2.0
+# Unless required by applicable law or agreed to in writing, software distributed
+# under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
+# CONDITIONS OF ANY KIND, either express or implied. See the License for the
+# specific language governing permissions and limitations under the License.
+#
+# When you publish or redistribute any data created with ScanCode or any ScanCode
+# derivative work, you must accompany this data with the following acknowledgment:
+#
+#  Generated with ScanCode and provided on an "AS IS" BASIS, WITHOUT WARRANTIES
+#  OR CONDITIONS OF ANY KIND, either express or implied. No content created from
+#  ScanCode should be considered or used as legal advice. Consult an Attorney
+#  for any legal advice.
+#  ScanCode is a free software code scanning tool from nexB Inc. and others.
+#  Visit https://github.com/nexB/scancode-toolkit/ for support and download.
+
+from __future__ import absolute_import, print_function
+
+import os
+
+from commoncode.testcase import FileBasedTesting
+
+from scancode.cache import ScanCache
+
+
+class TestCache(FileBasedTesting):
+    test_data_dir = os.path.join(os.path.dirname(__file__), 'data')
+
+    def test_can_cache(self):
+        test_file = self.get_test_loc('cache/package/package.json')
+        from scancode import api
+        package = api.get_package_infos(test_file)
+
+        test_dir = self.get_temp_dir()
+        cache = ScanCache(test_dir)
+        cache.put_infos(path='abc', file_infos=dict(sha1='def'))
+        cache.put_scan(path='abc', file_infos=dict(sha1='def'), scan_result=package)
