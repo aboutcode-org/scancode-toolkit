2904ea1b9922f2dcbb94a473f728b0b3b343d094
Jono Yang <jyang@nexb.com>
2016-12-07 11:17:02 -0800

    #339 Refactor json2csv.py.
    Add functionality to handle package data.

--- etc/scripts/json2csv.py
+++ etc/scripts/json2csv.py
@@ -39,43 +39,12 @@ etc/scripts/json2csv -h
 """
 
 
-def flatten_on_key(k, d):
-    if k in d and isinstance(d[k], list) and len(d[k]) > 0:
-        flatlist = []
-        for x in d[k]:
-            flat_d = OrderedDict(d)
-            flat_d[k] = x
-            flatlist.append(flat_d)
-        return flatlist
-    else:
-        return [d]
-
-
-def flatten(d):
-    temp = [d]
-    for k in d.keys():
-        flatlist = []
-        for v in temp:
-            flatlist += flatten_on_key(k, v)
-        temp = flatlist
-    return flatlist
-
-
-def promote_inner_keys(d):
-    for k1, v1 in d.items():
-        if isinstance(v1, dict):
-            d.pop(k1)
-            for k2, v2 in v1.items():
-                d[k1 + "_" + k2] = v2
-    return d
-
-
 class OrderedSet(object):
     """
     Ordered set based on list.
     """
-    def __init__(self, arg):
-        self.seq = arg[:]
+    def __init__(self):
+        self.seq = []
 
     def add(self, value):
         if value not in self.seq:
@@ -114,165 +83,110 @@ def load_scan(json_input):
 def json_scan_to_csv(json_input, csv_output, strip=0):
     """
     Convert a scancode JSON output file to a nexb-toolkit-like CSV.
-    Optionally strip up to `strip` path segments from the location paths.
+    Optionally strip up to `strip` path segments from the path paths.
     """
     scan_results = load_scan(json_input)
-    rows = scan_as_list(scan_results, strip)
-    headers = rows[0].keys()
+    rows = list(flatten_scan(scan_results, strip))
+    headers = collect_header_keys(rows)
     with open(csv_output, 'wb') as output:
-        w = unicodecsv.writer(output)
-        w.writerow(headers)
+        w = unicodecsv.DictWriter(output, headers)
+        w.writeheader()
         for r in rows:
-            w.writerow(r.values())
+            w.writerow(r)
 
 
-def scan_as_list(scan, strip=0):
+def flatten_scan(scan, strip=0):
     """
-    Return a list of ordered dictionaries of key/values flattening the data and
-    keying always by location, given a ScanCode scan results list.
-    Optionally strip up to `strip` path segments from the location paths.
+    Yield ordered dictionaries of key/values flattening the data and
+    keying always by path, given a ScanCode scan results list.
+    Optionally strip up to `strip` path segments from the path paths.
     """
-    resources = OrderedDict()
 
-    # unique keys, but ordered
-    keys = OrderedSet(['Resource'])
-
-    for entry in scan:
-        location = entry['path']
+    for scanned_file in scan:
+        path = scanned_file['path']
         if strip:
             # do not keep leading slash but add it back afterwards. keep trailing slashes
-            location = location.lstrip('/')
-            splits = [s for s in location.split('/')]
-            location = '/'.join(splits[strip:])
+            path = path.lstrip('/')
+            splits = [s for s in path.split('/')]
+            path = '/'.join(splits[strip:])
 
-        location = location.startswith('/') and location or '/' + location
+        path = path if path.startswith('/') else '/' + path
 
-        if entry['type'] == 'directory':
-            if not location.endswith('/'):
-                location = location + '/'
+        if scanned_file['type'] == 'directory':
+            if not path.endswith('/'):
+                path = path + '/'
 
-        # element 0 = file info in an OrderedDict
-        # element 1 = license info, each license and its info is in its own OrderedDict, which is then stored in a list
-        # element 2 = copyright info, each copyright and its info is in its own OrderedDict, which is then stored in a list
-        # There will need to be a fourth element for package info
-        resources[location] = [OrderedDict(Resource=location), [], []]
+        path = '/code' + path
 
-        file_info = resources[location][0]
-        licenses = resources[location][1]
-        copyrights = resources[location][2]
+        file_info = OrderedDict(Resource=path)
+        info_details = OrderedDict(((k, v) for k, v in scanned_file.items() if k != 'path' and not isinstance(v, list)))
+        file_info.update(info_details)
+        yield file_info
 
-        keys.add('type')
-        file_info['type'] = entry['type']
+        for licensing in scanned_file.get('licenses', []):
+            lic = OrderedDict(Resource=path)
+            for k, val in licensing.items():
+                if k == 'matched_rule':
+                    continue
+                if k not in ('start_line', 'end_line',):
+                    k = 'license__' + k
+                lic[k] = val
+            yield lic
 
-        for field in entry.keys():
-            # We handled these fields above
-            if field == 'path' or field == 'type':
-                continue
+        for copy_info in scanned_file.get('copyrights', []):
+            start_line = copy_info['start_line']
+            end_line = copy_info['end_line']
+            for cop in copy_info.get('statements', []):
+                inf = OrderedDict(Resource=path)
+                inf['copyright'] = cop
+                inf['start_line'] = start_line
+                inf['end_line'] = end_line
+                yield inf
 
-            elif field == 'licenses':
-                for licensing in entry.get('licenses', []):
-                    lic = OrderedDict(Resource=location)
-                    for k, val in licensing.items():
-                        if k == 'matched_rule':
-                            # We deal with this after processing the rest of the license fields
-                            continue
-                        if k not in ('start_line', 'end_line',):
-                            k = 'license_' + k
-                        lic[k] = val
-                        keys.add(k)
-                    licenses.append(lic)
+            for hold in copy_info.get('holders', []):
+                inf = OrderedDict(Resource=path)
+                inf['copyright_holder'] = hold
+                inf['start_line'] = start_line
+                inf['end_line'] = end_line
+                yield inf
 
-                    matched_rule = licensing.get('matched_rule')
-                    # We create a new row of matched rule info for each license associated with a rule
-                    for matched_rule_license in matched_rule.get('licenses', []):
-                        matched_rule_info = OrderedDict(Resource=location)
-                        for k, val in matched_rule.items():
-                            k = 'license_matched_rule_' + k
-                            if k == 'license_matched_rule_licenses':
-                                matched_rule_info[k] = matched_rule_license
-                            else:
-                                matched_rule_info[k] = val
-                            keys.add(k)
-                        licenses.append(matched_rule_info)
+            for auth in copy_info.get('authors', []):
+                inf = OrderedDict(Resource=path)
+                inf['author'] = auth
+                inf['start_line'] = start_line
+                inf['end_line'] = end_line
+                yield inf
 
-            elif field == 'copyrights':
-                for copy_info in entry.get('copyrights', []):
-                    start_line = copy_info['start_line']
-                    end_line = copy_info['end_line']
-                    for cop in copy_info.get('statements', []):
-                        inf = OrderedDict(Resource=location)
-                        inf['copyright'] = cop
-                        inf['start_line'] = start_line
-                        inf['end_line'] = end_line
-                        copyrights.append(inf)
-                        keys.update(inf.keys())
+        for package in scanned_file.get('packages', []):
+            pack = OrderedDict(Resource=path)
+            for k, val in package.items():
+                nk = 'package__' + k
+                if not isinstance(val, (list, dict, OrderedDict)):
+                    pack[nk] = val
+                elif k in ('authors', 'download_urls', 'copyrights', 'asserted_licenses'):
+                    pack[nk] = repr(val)
+            yield pack
 
-                    for hold in copy_info.get('holders', []):
-                        inf = OrderedDict(Resource=location)
-                        inf['copyright_holder'] = hold
-                        inf['start_line'] = start_line
-                        inf['end_line'] = end_line
-                        copyrights.append(inf)
-                        keys.update(inf.keys())
 
-                    for auth in copy_info.get('authors', []):
-                        inf = OrderedDict(Resource=location)
-                        inf['author'] = auth
-                        inf['start_line'] = start_line
-                        inf['end_line'] = end_line
-                        copyrights.append(inf)
-                        keys.update(inf.keys())
-
-            elif field == 'packages':
-                # TODO: Add package option support
-                continue
-
-            elif field == 'scan_errors':
-                # TODO: Add scan error support
-                continue
-
-            else:
-                keys.add(field)
-                file_info[field] = entry[field]
-
-    all_rows = []
-    for resource in resources:
-        file_info = resources[resource][0]
-        licenses = resources[resource][1]
-        copyrights = resources[resource][2]
-
-        # Flatten file info
-        row = OrderedDict()
-        for k in keys.keys():
-            row[k] = file_info.get(k, '')
-        all_rows.append(row)
-
-        # Flatten license info
-        for licensing in licenses:
-            row = OrderedDict()
-            for k in keys.keys():
-                row[k] = licensing.get(k, '')
-            all_rows.append(row)
-
-        # Flatten copyright info
-        for copy_info in copyrights:
-            row = OrderedDict()
-            for k in keys.keys():
-                row[k] = copy_info.get(k, '')
-            all_rows.append(row)
-
-    return all_rows
+def collect_header_keys(scan_data):
+    """
+    Return a list of keys collected from a list of scanned data dictionaries.
+    """
+    keys = OrderedSet()
+    for scan in scan_data:
+        keys.update(scan.keys())
+    return keys.keys()
 
 
 @click.command()
 @click.argument('json_input', type=click.Path(exists=True, readable=True))
 @click.argument('csv_output', type=click.Path(exists=False, readable=True))
-@click.option('-s', '--strip', help='Number of leading path segments to strip from location paths', type=click.INT, default=0)
+@click.option('-s', '--strip', help='Number of leading path segments to strip from path paths', type=click.INT, default=0)
 @click.help_option('-h', '--help')
 def cli(json_input, csv_output, strip=0):
     """
     Convert a ScanCode JSON scan file to a nexb-toolkit-like CSV.
-    Optionally strip up to `strip` leading path segments from the location paths.
+    Optionally strip up to `strip` leading path segments from the path paths.
 
     JSON_INPUT is either a ScanCode json format scan or the data.json file from a ScanCode html-app format scan.
     """
