cf37d21d43ef933144820e97cc51bcf478dd7f47
Philippe Ombredanne <pombredanne@nexb.com>
2017-01-06 22:31:37 +0100

    #413 Move portable_new_name to commoncode.paths.portable_name
     * also remove name transforms for the "new_name" function
    
    Signed-off-by: Philippe Ombredanne <pombredanne@nexb.com>

--- src/extractcode/__init__.py
+++ src/extractcode/__init__.py
@@ -1,5 +1,5 @@
 #
-# Copyright (c) 2015 nexB Inc. and others. All rights reserved.
+# Copyright (c) 2017 nexB Inc. and others. All rights reserved.
 # http://nexb.com and https://github.com/nexB/scancode-toolkit/
 # The ScanCode software is licensed under the Apache License version 2.0.
 # Data generated with ScanCode require an acknowledgment.
@@ -30,6 +30,8 @@ import posixpath
 import re
 import shutil
 import sys
+
+from text_unidecode import unidecode
 
 from commoncode import fileutils
 from commoncode.text import toascii
@@ -113,7 +115,7 @@ def remove_archive_suffix(path):
     return re.sub(EXTRACT_SUFFIX, '', path)
 
 
-def remove_backslashes(directory):
+def remove_backslashes_and_dotdots(directory):
     """
     Walk a directory and rename the files if their names contain backslashes.
     Return a list of errors if any.
@@ -121,189 +123,82 @@ def remove_backslashes(directory):
     errors = []
     for top, _, files in os.walk(str(directory)):
         for filename in files:
-            if '\\' in filename or '..' in filename:
-
-                try:
-                    new_path = fileutils.as_posixpath(filename)
-                    new_path = new_path.strip('/')
-                    new_path = posixpath.normpath(new_path)
-                    new_path = new_path.replace('..', '/')
-                    new_path = new_path.strip('/')
-                    new_path = posixpath.normpath(new_path)
-                    segments = new_path.split('/')
-                    directory = os.path.join(top, *segments[:-1])
-                    fileutils.create_dir(directory)
-                    shutil.move(os.path.join(top, filename), os.path.join(top, *segments))
-                except Exception:
-                    errors.append(os.path.join(top, filename))
+            if not ('\\' in filename or '..' in filename):
+                continue
+            try:
+                new_path = fileutils.as_posixpath(filename)
+                new_path = new_path.strip('/')
+                new_path = posixpath.normpath(new_path)
+                new_path = new_path.replace('..', '/')
+                new_path = new_path.strip('/')
+                new_path = posixpath.normpath(new_path)
+                segments = new_path.split('/')
+                directory = os.path.join(top, *segments[:-1])
+                fileutils.create_dir(directory)
+                shutil.move(os.path.join(top, filename), os.path.join(top, *segments))
+            except Exception:
+                errors.append(os.path.join(top, filename))
     return errors
-
-
-def extracted_files(location):
-    """
-    Yield the locations of extracted files in a directory location.
-    """
-    assert location
-    logger.debug('extracted_files for: %(location)r' % locals())
-    return fileutils.file_iter(location)
 
 
 def new_name(location, is_dir=False):
     """
-    Return a new non-existing location usable to write a file or create
-    directory without overwriting existing files or directories in the same
-    parent directory, ignoring the case of the name.
-    The case of the name is ignored to ensure that similar results are returned
-    across case sensitive (*nix) and case insensitive file systems.
-    Some characters illegal for us in file names on some OS are replaced with _
-    such as a colon ":" on Windows.
+    Return a new non-existing location from a `location` usable to write a file
+    or create directory without overwriting existing files or directories in the same
+    parent directory, ignoring the case of the filename.
 
-    To find a new unique name:
+    The case of the filename is ignored to ensure that similar results are returned
+    across case sensitive (*nix) and case insensitive file systems.
+
+    To find a new unique filename, this tries new names this way:
      * pad a directory name with _X where X is an incremented number.
      * pad a file base name with _X where X is an incremented number and keep
        the extension unchanged.
     """
     assert location
-
     location = location.rstrip('\\/')
-    name = fileutils.file_name(location).strip()
-    if (not name or name == '.'
-        # windows bare drive path as in c: or z:
-        or (name and len(name) == 2 and name.endswith(':'))):
-        name = 'file'
-
+    assert location
+        
     parent = fileutils.parent_directory(location)
+
     # all existing files or directory as lower case
     siblings_lower = set(s.lower() for s in os.listdir(parent))
 
-    portable_name = name.replace(':', '')
+    filename = fileutils.file_name(location)
 
-    if portable_name.lower() not in siblings_lower:
-        return posixpath.join(parent, portable_name)
+    # corner case
+    if filename in ('.', '..'):
+        filename = '_'
 
-    ext = fileutils.file_extension(portable_name)
-    base_name = fileutils.file_base_name(portable_name)
+    # if unique, return this
+    if filename.lower() not in siblings_lower:
+        return os.path.join(parent, filename)
+
+    # otherwise seek a unique name
     if is_dir:
-        # directories have no extension
+        # directories do not have an "extension"
+        base_name = filename
         ext = ''
-        base_name = portable_name
+    else:
+        base_name, dot, ext = filename.partition('.')
+        if dot:
+            ext = dot + ext
+        else:
+            base_name = filename
+            ext = ''
 
+    # find a unique filename, adding a counter int to the base_name
     counter = 1
     while 1:
-        new_name = base_name + '_' + str(counter) + ext
-        if new_name.lower() not in siblings_lower:
+        filename = base_name + '_' + str(counter) + ext
+        if filename.lower() not in siblings_lower:
             break
         counter += 1
-    return os.path.join(parent, new_name)
-
-
-def portable_new_name(filename):
-    """
-    Return a new name for `filename` that is portable across operating systems.
-    """
-    # https://github.com/pallets/werkzeug/blob/master/werkzeug/utils.py#L253
-    # https://raw.githubusercontent.com/pallets/werkzeug/8c2d63ce247ba1345e1b9332a68ceff93b2c07ab/werkzeug/utils.py
-
-    # See also https://github.com/dbr/tvnamer/blob/master/tvnamer/utils.py
-
-    _filename_ascii_strip_re = re.compile(r'[^A-Za-z0-9_.-]')
-    _windows_device_files = ('CON', 'AUX', 'COM1', 'COM2', 'COM3', 'COM4', 'LPT1', 'LPT2', 'LPT3', 'PRN', 'NUL')
-
-    # https://raw.githubusercontent.com/pallets/werkzeug/8c2d63ce247ba1345e1b9332a68ceff93b2c07ab/werkzeug/_compat.py
-    PY2 = sys.version_info[0] == 2
-    if PY2:
-        text_type = unicode
-    else:
-        text_type = str    
-
-    def secure_filename(filename):
-        r"""Pass it a filename and it will return a secure version of it.  This
-        filename can then safely be stored on a regular file system and passed
-        to :func:`os.path.join`.  The filename returned is an ASCII only string
-        for maximum portability.
-
-        On windows systems the function also makes sure that the file is not
-        named after one of the special device files.
-
-        >>> secure_filename("My cool movie.mov")
-        'My_cool_movie.mov'
-        >>> secure_filename("../../../etc/passwd")
-        'etc_passwd'
-        >>> secure_filename(u'i contain cool \xfcml\xe4uts.txt')
-        'i_contain_cool_umlauts.txt'
-
-        The function might return an empty filename.  It's your responsibility
-        to ensure that the filename is unique and that you generate random
-        filename if the function returned an empty one.
-
-        .. versionadded:: 0.5
-
-        :param filename: the filename to secure
-        """
-        toascii
-        if isinstance(filename, unicode):
-            from unicodedata import normalize
-            filename = normalize('NFKD', filename).encode('ascii', 'ignore')
-        for sep in os.path.sep, os.path.altsep:
-            if sep:
-                filename = filename.replace(sep, ' ')
-        re.sub
-        filename = str(_filename_ascii_strip_re.sub('', '_'.join(
-                       filename.split()))).strip('._')
-
-        # on nt a couple of special files are present in each folder.  We
-        # have to ensure that the target file is not such a filename.  In
-        # this case we prepend an underline
-        if os.name == 'nt' and filename and \
-           filename.split('.')[0].upper() in _windows_device_files:
-            filename = '_' + filename
-
-        return filename
+    return os.path.join(parent, filename)
 
 
 
-
-def is_portable_file_name(name):
-    """
-    Return True if `name` is a legal and portable file name on all supported OSes,
-    e.g. on Linux/POSIX, Windows and MacOSX.
-
-    See for more details:
-    https://msdn.microsoft.com/en-us/library/windows/desktop/aa365247(v=vs.85).aspx
-    http://stackoverflow.com/questions/122400/what-are-reserved-filenames-for-various-platforms
-    http://www.boost.org/doc/libs/1_36_0/libs/filesystem/doc/portability_guide.htm
-    """
-
-    windows_reserved_chars1 = '<>:"/\\|?*'
-    # null to 31.
-    windows_reserved_chars2 = ''.join(map(chr, range(32)))
-    # these are illegal both upper and lowercase and with or without an extension
-    windows_reserved_base_names1 = [n.lower() for n in (
-        'CON', 'PRN', 'AUX', 'NUL',
-        'COM1', 'COM2', 'COM3', 'COM4', 'COM5', 'COM6', 'COM7', 'COM8', 'COM9',
-        'LPT1', 'LPT2', 'LPT3', 'LPT4', 'LPT5', 'LPT6', 'LPT7', 'LPT8', 'LPT9'
-    )]
-    windows_reserved_base_names2 = [re.compile('^' + n + '\..*$').match for n in windows_reserved_base_names1]
-
-    # not trailing dot in a windows name
-    windows_illegal_trailing_chars = ('.',)
-
-    illegal_char = windows_reserved_chars1 + windows_reserved_chars2
-
-    namel = name.lower()
-    if (any(c in name for c in illegal_char)
-     or any(namel == rn for rn in windows_reserved_base_names1)
-     or any(ill(namel) for ill in windows_reserved_base_names2)
-     or name.endswith(windows_illegal_trailing_chars)):
-        return False
-    return True
-
-
-# http://www.opengroup.org/onlinepubs/007904975/basedefs/xbd_chap03.html
-is_portable_posix = re.compile('^[0-9a-zA-Z\._\-]*$').match
-
-
-
+# TODO: use attrs and slots
 class Entry(object):
     """
     An archive entry presenting the common data that exists in all entries
@@ -312,10 +207,16 @@ class Entry(object):
     """
     # the actual posix as in the archive (relative, absolute, etc)
     path = None
+
     # path to use for links, typically a normalized target
+    # FIXME: not used
     actual_path = None
+
     # where we will really extract, relative to the archive root
+    # FIXME: not used
     extraction_path = None
+
+    # in bytes
     size = 0
     date = None
     is_file = True
@@ -332,6 +233,7 @@ class Entry(object):
         Fix paths that are absolute, relative, backslashes and other
         shenanigans. Update the extraction path.
         """
+        # TODO: Implement ME
 
     def __repr__(self):
         msg = (
