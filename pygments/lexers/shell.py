a1f88568c66e073eba6ee47edfa1163e2ea1a14f
Georg Brandl <georg@python.org>
2017-01-22 18:38:11 +0100

    Copyright update.

--- pygments/lexers/shell.py
+++ pygments/lexers/shell.py
@@ -5,7 +5,7 @@
 
     Lexers for various shells.
 
-    :copyright: Copyright 2006-2015 by the Pygments team, see AUTHORS.
+    :copyright: Copyright 2006-2017 by the Pygments team, see AUTHORS.
     :license: BSD, see LICENSE for details.
 """
 
