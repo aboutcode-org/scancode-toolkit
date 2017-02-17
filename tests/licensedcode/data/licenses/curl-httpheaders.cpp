89f996a982bb622c2eb3fd96a7e339b16e73a15a
Philippe Ombredanne <pombredanne@nexb.com>
2017-02-17 17:40:24 +0100

    #107 new rule and test for LGPL 3.0 detection
    Signed-off-by: Philippe Ombredanne <pombredanne@nexb.com>

---
+++ tests/licensedcode/data/licenses/curl-httpheaders.cpp
@@ -0,0 +1,27 @@
+/**
+ *
+ *  Download Upload Messaging Manager
+ *
+ *  Copyright (C) 2012-2013  Open-RnD Sp. z o.o. All rights reserved.
+ * @verbatim
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU Lesser General Public License version 3 as
+ *  published by the Free Software Foundation.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public License version 3
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ * @endverbatim
+
+ * @file curl-headers.h
+ * @author Bartlomiej Jozwiak (bj@open-rnd.pl)
+ * @date 01-04-2013
+ * @brief __BRIEF_HERE_TBD__
+ *
+ * __DETAILS__
+ */
