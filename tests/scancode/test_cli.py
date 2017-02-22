00ce9061c59e7ff803909bcafca3439176d51d7a
Philippe Ombredanne <pombredanne@nexb.com>
2017-02-22 09:49:42 +0100

    #495 Return only True or False for "is_*" flags in get_file_infos
    Signed-off-by: Philippe Ombredanne <pombredanne@nexb.com>

--- tests/scancode/test_cli.py
+++ tests/scancode/test_cli.py
@@ -426,7 +426,6 @@ def test_scan_can_run_from_other_directory():
     result_file = test_env.get_temp_file('json')
     scan_cmd = os.path.join(scancode.root_dir, 'scancode')
     work_dir = os.path.dirname(result_file)
-    rc, _sop, _ser = execute(scan_cmd, ['-ci', test_file, result_file], cwd=work_dir)    
+    rc, _sop, _ser = execute(scan_cmd, ['-ci', test_file, result_file], cwd=work_dir)
     assert rc == 0
     check_scan(test_env.get_test_loc(expected_file), result_file, regen=False)
-
