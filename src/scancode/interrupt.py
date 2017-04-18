02766cfbcf1c54313043ce2f6e70786e7d47d8d0
Philippe Ombredanne <pombredanne@nexb.com>
2017-04-18 19:27:44 +0200

    Improve timeout handling on POSIX and Windows for #521
     * Windows tests were failing. The robotframework code was not working
       I replaced this with the older thread-based implmentation and cleanup
       the code for POSIX.
    
    Link: https://github.com/nexB/scancode-toolkit/issues/521
    Signed-off-by: Philippe Ombredanne <pombredanne@nexb.com>

--- src/scancode/interrupt.py
+++ src/scancode/interrupt.py
@@ -2,8 +2,6 @@
 # Copyright (c) 2017 nexB Inc. and others. All rights reserved.
 # http://nexb.com and https://github.com/nexB/scancode-toolkit/
 # The ScanCode software is licensed under the Apache License version 2.0.
-# Data generated with ScanCode require an acknowledgment.
-# ScanCode is a trademark of nexB Inc.
 #
 # You may not use this software except in compliance with the License.
 # You may obtain a copy of the License at: http://apache.org/licenses/LICENSE-2.0
@@ -12,58 +10,144 @@
 # CONDITIONS OF ANY KIND, either express or implied. See the License for the
 # specific language governing permissions and limitations under the License.
 #
-# When you publish or redistribute any data created with ScanCode or any ScanCode
-# derivative work, you must accompany this data with the following acknowledgment:
-#
-#  Generated with ScanCode and provided on an "AS IS" BASIS, WITHOUT WARRANTIES
-#  OR CONDITIONS OF ANY KIND, either express or implied. No content created from
-#  ScanCode should be considered or used as legal advice. Consult an Attorney
-#  for any legal advice.
-#  ScanCode is a free software code scanning tool from nexB Inc. and others.
-#  Visit https://github.com/nexB/scancode-toolkit/ for support and download.
 
 from __future__ import print_function
 from __future__ import absolute_import
 
-from scancode.timeouts import Timeout
-
+from commoncode.system import on_windows
 
 DEFAULT_TIMEOUT = 120  # seconds
 
+
+"""
+This modules povides an interruptible() function to run a callable and
+stop it after a timeout with a windows and POSIX implementation.
+
+Call `func` function with `args` and `kwargs` arguments and return a
+tuple of (success, return value). `func` is invoked through an OS-
+specific wrapper and will be interrupted if it does not return within
+`timeout` seconds.
+
+`func` returned results must be pickable.
+`timeout` in seconds defaults to DEFAULT_TIMEOUT.
+
+`args` and `kwargs` are passed to `func` as *args and **kwargs.
+
+In the returned tuple of (success, value), success is True or False. If
+success is True, the call was successful and the second item in the
+tuple is the returned value of `func`.
+
+If success is False, the call did not complete within `timeout`
+seconds and was interrupted. In this case, the second item in the
+tuple is an error message string.
+"""
 
 class TimeoutError(Exception):
     pass
 
 
-def interruptible(func, args=(), kwargs={}, timeout=DEFAULT_TIMEOUT):
+if not on_windows:
     """
-    Call `func` function with `args` and `kwargs` arguments and return a tuple of
-    (success, return value). `func` is invoked through an OS-specific wrapper and
-    will be interrupted if it does not return within `timeout` seconds.
+    Some code based in part and inspired from the RobotFramework and
+    heavily modified.
 
-    `func` returned results must be pickable.
-    `timeout` in seconds defaults to DEFAULT_TIMEOUT.
+    Copyright 2008-2015 Nokia Networks
+    Copyright 2016-     Robot Framework Foundation
 
-    `args` and `kwargs` are passed to `func` as *args and **kwargs.
+    Licensed under the Apache License, Version 2.0 (the "License");
+    you may not use this file except in compliance with the License.
+    You may obtain a copy of the License at
 
-    In the returned tuple of (success, value), success is True or False.
-    If success is True, the call was successful and the second item in the tuple is
-    the returned value of `func`.
-    
-    If success is False, the call did not complete within `timeout`
-    seconds and was interrupted. In this case, the second item in the
-    tuple is an error message string.
+        http://www.apache.org/licenses/LICENSE-2.0
+
+    Unless required by applicable law or agreed to in writing, software
+    distributed under the License is distributed on an "AS IS" BASIS,
+    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+    implied. See the License for the specific language governing
+    permissions and limitations under the License.
     """
 
-    runner = Timeout(timeout, TimeoutError)
+    import signal
 
-    def runnable(): 
-        return func(*args, **kwargs)
+    def interruptible(func, args=None, kwargs=None, timeout=DEFAULT_TIMEOUT):
+        """
+        POSIX, signals-based interruptible runner.
+        """
 
+        def handler(signum, frame):
+            raise TimeoutError
+
+        try:
+            signal.signal(signal.SIGALRM, handler)
+            signal.setitimer(signal.ITIMER_REAL, timeout)
+            return True, func(*(args or ()), **(kwargs or {}))
+        except TimeoutError:
+            return False, ('ERROR: Processing interrupted: timeout after '
+                           '%(timeout)d seconds.' % locals())
+        finally:
+            signal.setitimer(signal.ITIMER_REAL, 0)
+
+else:
+    """
+    Run a function in an interruptible thread with a timeout.
+    Based on an idea of dano "Dan O'Reilly"
+    http://stackoverflow.com/users/2073595/dano
+    But not code has been reused from this post.
+    """
+
+    import ctypes
+    import multiprocessing
+    import Queue
     try:
-        return True, runner.execute(runnable)
-    except:
-        import traceback
-        traceback.print_exc()
-        return False, ('ERROR: Processing interrupted: timeout after '
-                       '%(timeout)d seconds.' % locals())
+        import thread
+    except ImportError:
+        import _thread as thread
+
+
+    def interruptible(func, args=None, kwargs=None, timeout=DEFAULT_TIMEOUT):
+        """
+        Windows, threads-based interruptible runner. It can work also on
+        POSIX, but is not reliable and works only if everything is pickable.
+        """
+        # We run `func` in a thread and run a loop until timeout
+        results = Queue.Queue()
+
+        def runner():
+            results.put(func(*(args or ()), **(kwargs or {})))
+
+        tid = thread.start_new_thread(runner, ())
+
+        try:
+            res = results.get(timeout=timeout)
+            return True, res
+        except (Queue.Empty, multiprocessing.TimeoutError):
+            return False, ('ERROR: Processing interrupted: timeout after '
+                           '%(timeout)d seconds.' % locals())
+        finally:
+            try:
+                async_raise(tid, Exception)
+            except (SystemExit, ValueError):
+                pass
+
+
+    def async_raise(tid, exctype=Exception):
+        """
+        Raise an Exception in the Thread with id `tid`. Perform cleanup if
+        needed.
+
+        Based on Killable Threads By Tomer Filiba
+        from http://tomerfiliba.com/recipes/Thread2/
+        license: public domain.
+        """
+        assert isinstance(tid, int), 'Invalid  thread id: must an integer'
+
+        tid = ctypes.c_long(tid)
+        exception = ctypes.py_object(Exception)
+        res = ctypes.pythonapi.PyThreadState_SetAsyncExc(tid, exception)
+        if res == 0:
+            raise ValueError('Invalid thread id.')
+        elif res != 1:
+            # if it returns a number greater than one, you're in trouble,
+            # and you should call it again with exc=NULL to revert the effect
+            ctypes.pythonapi.PyThreadState_SetAsyncExc(tid, 0)
+            raise SystemError('PyThreadState_SetAsyncExc failed.')
