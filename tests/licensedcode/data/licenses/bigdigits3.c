10471d0747f48318ec90771fb4a3d81a901bcef5
Philippe Ombredanne <pombredanne@nexb.com>
2017-07-30 12:02:01 +0200

    New bigdigit license rules and tests #682
     * also add notes to the bigdigits license that starting with 2.5 the
       new license is mpl-2.0
    
    Signed-off-by: Philippe Ombredanne <pombredanne@nexb.com>

---
+++ tests/licensedcode/data/licenses/bigdigits3.c
@@ -0,0 +1,10 @@
+/******************** SHORT COPYRIGHT NOTICE**************************
+This source code is part of the BigDigits multiple-precision
+arithmetic library Version 2.0 originally written by David Ireland,
+copyright (c) 2001-5 D.I. Management Services Pty Limited, all rights
+reserved. It is provided "as is" with no warranties. You may use
+this software under the terms of the full copyright notice
+"bigdigitsCopyright.txt" that should have been included with this
+library or can be obtained from <www.di-mgt.com.au/bigdigits.html>.
+This notice must always be retained in any copy.
+******************* END OF COPYRIGHT NOTICE***************************/
