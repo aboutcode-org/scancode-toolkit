2ffe5e5c3a24e282710990840d5fed06c5965d64
Philippe Ombredanne <pombredanne@nexb.com>
2017-10-05 09:57:38 -0700

    Add license test for #107
    Signed-off-by: Philippe Ombredanne <pombredanne@nexb.com>

---
+++ tests/licensedcode/data/licenses/genivi.c
@@ -0,0 +1,41 @@
+/**
+ *
+ *  Download Upload Messaging Manager
+ *
+ *  Copyright (C) 2012-2013  Open-RnD Sp. z o.o. All rights reserved.
+ * @verbatim
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU Lesser General Public License version 3 as
+ *  published by the Free Software Foundation.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public License version 3
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ * @endverbatim
+
+/**
+ * Copyright (C) <2013>, GENIVI Alliance, Inc.
+ * Author: bj@open-rnd.pl
+ *
+ * This file is part of <GENIVI Download Upload Messaging Manager>.
+ *
+ * Contributions are licensed to the GENIVI Alliance under one or more
+ * Contributor License Agreements.29
+ *
+ * This Source Code Form is subject to the terms of the
+ * Mozilla Public License (MPL), v. 2.0.
+ * If a copy of the MPL was not distributed with this file,
+ * You can obtain one at http://mozilla.org/MPL/2.0/.
+ *
+ * For further information see http://www.genivi.org/.
+ *
+ * List of changes:
+ * <01.04.2013>, <Bartlomiej Jozwiak>, <Initial version> 
+ */
+
+
