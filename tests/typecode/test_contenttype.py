2e6d71fd84d31c949b3b1ec60e321666d9f71e14
Supragya Raj <supragyaraj@gmail.com>
2017-10-30 21:41:11 +0530

    Test resolve Signed-off-by: Supragya Raj <supragyaraj@gmail.com>

--- tests/typecode/test_contenttype.py
+++ tests/typecode/test_contenttype.py
@@ -94,8 +94,10 @@ class TestContentType(FileBasedTesting):
             expected = 'text/plain'
         assert expected == get_mimetype_file(test_file)
 
+    @skipIf(on_windows, 'Windows: Unicode test expectedFailure')
     def test_filetype_file_on_unicode_file_name2(self):
-        test_zip = self.extract_test_zip('contenttype/unicode/unicode2.zip')
+        zip_file_name = 'contenttype/unicode/unicode2.zip'
+        test_zip = self.extract_test_zip(zip_file_name.encode('utf-8'))
         test_dir = os.path.join(test_zip, 'a')
         f = [f for f in os.listdir(test_dir) if f.startswith('g')][0]
         test_file = os.path.join(test_dir, f)
