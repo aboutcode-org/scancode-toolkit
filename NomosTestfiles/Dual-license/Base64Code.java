38769fb90c353c9a9b43325711c6b18e7a474504
rlintu <raino.lintulampi@bittium.com>
2017-11-13 10:52:12 +0200

    feat(licenses): adding new licenses and correcting license handling
    New SPDX licenses:
    - Abstyles, APAFML, Adobe-2006, Adobe-DNG, Adobe-Glyph, Afmparse, AMPAS
    - bzip2-1.0.5, bzip2-1.0.6
    - CrystalStacker, curl
    - gnuplot
    - MIT-CMU
    - TCL, TMate
    
    New other licenses:
    - 3GPP
    - ANT+SharedSource
    - copyleft-next-0.3.0, copyleft-next-0.3.1
    - IBM-as-is, IBM-copyleft, IBM-dhcp, IBM-reciprocal
    - ITU-T
    - Intel-acpi
    - OZPLB-1.0, OZPLB-1.1
    - Purdue
    - SCEA
    
    Lot of corrections and improvements:
    - spdx-license-identifier detections added
    - BSD license detection structure changed
    - MIT/X11/XFree86 detection improved
    - GPL and LGPL detection errors corrected
    - new dual-license cases
    - xxx-possibility detection improved
    - IBM and Sun detection clarified
    - trademark detection removed
    - and lot of other improvements

---
+++ NomosTestfiles/Dual-license/Base64Code.java
@@ -0,0 +1,28 @@
+//Copyright 2003-2010 Christian d'Heureuse, Inventec Informatik AG, Zurich, Switzerland
+// www.source-code.biz, www.inventec.ch/chdh
+//
+// This module is multi-licensed and may be used under the terms
+// of any of the following licenses:
+//
+// EPL, Eclipse Public License, V1.0 or later, http://www.eclipse.org/legal
+// LGPL, GNU Lesser General Public License, V2.1 or later, http://www.gnu.org/licenses/lgpl.html
+// GPL, GNU General Public License, V2 or later, http://www.gnu.org/licenses/gpl.html
+// AL, Apache License, V2.0 or later, http://www.apache.org/licenses
+// BSD, BSD License, http://www.opensource.org/licenses/bsd-license.php
+//
+// Please contact the author if you need another license.
+// This module is provided "as is", without warranties of any kind.
+/**
+* A Base64 encoder/decoder.
+*
+* <p>
+* This class is used to encode and decode data in Base64 format as described in RFC 1521.
+*
+* <p>
+* Project home page: <a href="http://www.source-code.biz/base64coder/java/">www.source-code.biz/base64coder/java</a><br>
+* Author: Christian d'Heureuse, Inventec Informatik AG, Zurich, Switzerland<br>
+* Multi-licensed: EPL / LGPL / GPL / AL / BSD.
+*
+* @author Christian d'Heureuse
+* @author vaxquis
+*/
---
+++ NomosTestfiles/See-doc/libmng_filter.c
@@ -0,0 +1,9 @@
+/* ************************************************************************** */
+/* * For conditions of distribution and use, * */
+/* * see copyright notice in libmng.h * */
+/* ************************************************************************** */
+/* * * */
+/* * project : libmng * */
+/* * file : libmng_filter.c copyright (c) 2000-2004 G.Juyn * */
+/* * version : 1.0.9 * */
+/* * * */
