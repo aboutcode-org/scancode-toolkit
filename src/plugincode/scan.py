861e9a1a08744f8b6e20b59faeebf53643ee01bb
Philippe Ombredanne <pombredanne@nexb.com>
2018-01-17 22:50:50 +0100

    Add new scan stage and plugins #787 #552 #698
     * A scan plugin handles the scan of one resource at a time
     * Existing scans have been reworked as plugins
    
    Signed-off-by: Philippe Ombredanne <pombredanne@nexb.com>

---
+++ src/plugincode/scan.py
@@ -0,0 +1,72 @@
+#
+# Copyright (c) 2018 nexB Inc. and others. All rights reserved.
+# http://nexb.com and https://github.com/nexB/scancode-toolkit/
+# The ScanCode software is licensed under the Apache License version 2.0.
+# Data generated with ScanCode require an acknowledgment.
+# ScanCode is a trademark of nexB Inc.
+#
+# You may not use this software except in compliance with the License.
+# You may obtain a copy of the License at: http://apache.org/licenses/LICENSE-2.0
+# Unless required by applicable law or agreed to in writing, software distributed
+# under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
+# CONDITIONS OF ANY KIND, either express or implied. See the License for the
+# specific language governing permissions and limitations under the License.
+#
+# When you publish or redistribute any data created with ScanCode or any ScanCode
+# derivative work, you must accompany this data with the following acknowledgment:
+#
+#  Generated with ScanCode and provided on an "AS IS" BASIS, WITHOUT WARRANTIES
+#  OR CONDITIONS OF ANY KIND, either express or implied. No content created from
+#  ScanCode should be considered or used as legal advice. Consult an Attorney
+#  for any legal advice.
+#  ScanCode is a free software code scanning tool from nexB Inc. and others.
+#  Visit https://github.com/nexB/scancode-toolkit/ for support and download.
+
+from __future__ import absolute_import
+from __future__ import unicode_literals
+
+from plugincode import BasePlugin
+from plugincode import PluginManager
+from plugincode import HookimplMarker
+from plugincode import HookspecMarker
+
+
+stage = 'scan'
+entrypoint = 'scancode_scan'
+
+scan_spec = HookspecMarker(stage)
+scan_impl = HookimplMarker(stage)
+
+
+@scan_spec
+class ScanPlugin(BasePlugin):
+    """
+    A scan plugin base class that all scan plugins must extend. A scan plugin
+    provides a single `get_scanner()` method that returns a scanner function.
+    The key under which scan results are retruned for a scanner is the plugin
+    "name" attribute. This attribute is set automatically as the "entrypoint"
+    name used for this plugin.
+    """
+    
+    # a relative sort order number (integer or float). In scan results, results
+    # from scanners are sorted by this sorted_order then by "key" which is the
+    # scanner plugin name
+    sort_order = 100
+
+    # TODO: pass own command options name/values as concrete kwargs
+    def get_scanner(self, **kwargs):
+        """
+        Return a scanner callable that takes a single `location` argument.
+        This callable (typically a bare function) should carry as little state
+        as possible as it may be executed through multiprocessing.
+        Subclasses must override.
+        """
+        raise NotImplementedError
+
+
+scan_plugins = PluginManager(
+    stage=stage,
+    module_qname=__name__,
+    entrypoint=entrypoint,
+    plugin_base_class=ScanPlugin
+)
---
+++ src/scancode/plugin_license.py
@@ -0,0 +1,114 @@
+#
+# Copyright (c) 2018 nexB Inc. and others. All rights reserved.
+# http://nexb.com and https://github.com/nexB/scancode-toolkit/
+# The ScanCode software is licensed under the Apache License version 2.0.
+# Data generated with ScanCode require an acknowledgment.
+# ScanCode is a trademark of nexB Inc.
+#
+# You may not use this software except in compliance with the License.
+# You may obtain a copy of the License at: http://apache.org/licenses/LICENSE-2.0
+# Unless required by applicable law or agreed to in writing, software distributed
+# under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
+# CONDITIONS OF ANY KIND, either express or implied. See the License for the
+# specific language governing permissions and limitations under the License.
+#
+# When you publish or redistribute any data created with ScanCode or any ScanCode
+# derivative work, you must accompany this data with the following acknowledgment:
+#
+#  Generated with ScanCode and provided on an "AS IS" BASIS, WITHOUT WARRANTIES
+#  OR CONDITIONS OF ANY KIND, either express or implied. No content created from
+#  ScanCode should be considered or used as legal advice. Consult an Attorney
+#  for any legal advice.
+#  ScanCode is a free software code scanning tool from nexB Inc. and others.
+#  Visit https://github.com/nexB/scancode-toolkit/ for support and download.
+
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+from __future__ import unicode_literals
+
+from functools import partial
+
+from plugincode.scan import ScanPlugin
+from plugincode.scan import scan_impl
+from plugincode.housekeeping import HousekeepingPlugin
+from plugincode.housekeeping import housekeeping_impl
+from scancode import CommandLineOption
+from scancode import MISC_GROUP
+from scancode import SCAN_OPTIONS_GROUP
+from scancode import SCAN_GROUP
+from scancode.api import DEJACODE_LICENSE_URL
+
+
+@scan_impl
+class LicenseScanner(ScanPlugin):
+    """
+    Scan a Resource for licenses.
+    """
+    sort_order = 2
+
+    options = [
+        CommandLineOption(('-l', '--license'),
+            is_flag=True, default=False,
+            help='Scan <input> for licenses.',
+            help_group=SCAN_GROUP),
+
+        CommandLineOption(('--license-score',),
+            type=int, default=0, show_default=True,
+            help='Do not return license matches with a score lower than this score. '
+                 'A number between 0 and 100.',
+            help_group=SCAN_OPTIONS_GROUP),
+
+        CommandLineOption(('--license-text',),
+            is_flag=True, default=False,
+            help='Include the detected licenses matched text.',
+            help_group=SCAN_OPTIONS_GROUP),
+
+        CommandLineOption(('--license-url-template',),
+            default=DEJACODE_LICENSE_URL, show_default=True,
+            help='Set the template URL used for the license reference URLs. '
+                 'Curly braces ({}) are replaced by the license key.',
+            help_group=SCAN_OPTIONS_GROUP),
+
+        CommandLineOption(('--license-diag',),
+            is_flag=True, default=False,
+            help='Include diagnostic information in license scan results.',
+            help_group=SCAN_OPTIONS_GROUP),
+    ]
+
+    def is_enabled(self):
+        return self.is_command_option_enabled('license')
+
+    def get_scanner(self, license_score=0, license_text=False,
+                    license_url_template=DEJACODE_LICENSE_URL, license_diag=False, **kwargs):
+        from scancode.api import get_licenses
+        return partial(get_licenses, min_score=license_score,
+                       include_text=license_text, diag=license_diag,
+                       license_url_template=license_url_template)
+
+
+
+def reindex_licenses(ctx, param, value):
+    if not value or ctx.resilient_parsing:
+        return
+
+    # TODO: check for temp file configuration and use that for the cache!!!
+    from licensedcode.cache import reindex
+    import click
+    click.echo('Checking and rebuilding the license index...')
+    reindex()
+    click.echo('Done.')
+    ctx.exit()
+
+
+@housekeeping_impl
+class LicenseIndexer(HousekeepingPlugin):
+
+    options = [
+        CommandLineOption(
+            ('--reindex-licenses',),
+            is_eager=True, is_flag=True, default=False,
+            callback=reindex_licenses,
+            help='Check the license index cache and reindex if needed.',
+            help_group=MISC_GROUP)
+    ]
