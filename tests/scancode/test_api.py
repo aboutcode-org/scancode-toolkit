cfa3f5611a159c6794013a342d6666df3ff12995
Philippe Ombredanne <pombredanne@nexb.com>
2018-02-01 20:17:55 +0100

    Correct API tests
    Signed-off-by: Philippe Ombredanne <pombredanne@nexb.com>

--- tests/scancode/test_api.py
+++ tests/scancode/test_api.py
@@ -51,26 +51,25 @@ class TestAPI(FileBasedTesting):
             _pickled = pickle.dumps(package)
             _cpickled = cPickle.dumps(package)
 
-    def test_get_file_info_flag_are_not_null(self):
+    def test_get_file_info_include_size(self):
         # note the test file is EMPTY on purpose to generate all False is_* flags
-        test_dir = self.get_test_loc('api/info')
+        test_dir = self.get_test_loc('api/info/test.txt')
         info = api.get_file_info(test_dir)
         expected = [
-            (u'date', None),
-            (u'size', 4096),
+            (u'size', 0),
             (u'sha1', None),
             (u'md5', None),
-            (u'mime_type', None),
-            (u'file_type', None),
+            (u'mime_type', u'inode/x-empty'),
+            (u'file_type', u'empty'),
             (u'programming_language', None),
             (u'is_binary', False),
-            (u'is_text', False),
+            (u'is_text', True),
             (u'is_archive', False),
             (u'is_media', False),
             (u'is_source', False),
             (u'is_script', False)
         ]
-        assert expected == info.items()
+        assert expected == [(k, v) for k, v in info.items() if k != 'date']
 
     def test_get_package_info_works_for_maven_dot_pom(self):
         test_file = self.get_test_loc('api/package/p6spy-1.3.pom')
@@ -83,26 +82,6 @@ class TestAPI(FileBasedTesting):
         packages = api.get_package_info(test_file)
         assert len(packages) == 1
         assert packages['packages'][0]['version'] == '1.3'
-
-    def test_get_file_info_include_size(self):
-        test_dir = self.get_test_loc('api/info/test.txt')
-        info = api.get_file_info(test_dir)
-        expected = [
-            (u'date', '2017-10-03'),
-            (u'size', 0),
-            (u'sha1', None),
-            (u'md5', None),
-            (u'mime_type',
-            u'inode/x-empty'),
-            (u'file_type', u'empty'),
-            (u'programming_language', None),
-            (u'is_binary', False),
-            (u'is_text', True),
-            (u'is_archive', False),
-            (u'is_media', False),
-            (u'is_source', False),
-            (u'is_script', False)]
-        assert expected == info.items()
 
     def test_get_copyrights_include_copyrights_and_authors(self):
         test_file = self.get_test_loc('api/copyright/iproute.c')
