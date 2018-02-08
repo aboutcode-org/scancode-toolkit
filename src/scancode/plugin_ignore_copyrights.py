ece8b56da5a629648c713dff2bf96da72a299bd8
Haiko Schol <ext-haiko.schol@here.com>
2018-02-08 20:42:24 +0100

    Add a plugin for ignoring certain copyrights
    The "ignore copyrights" plugin takes one or more regular expressions
    to match against the "authors" and "holders" attribute of the copyright
    information. Matching findings are marked as filtered and will be
    skipped in the output.
    
    Fixes #106
    
    Signed-off-by: Haiko Schol <ext-haiko.schol@here.com>

---
+++ src/scancode/plugin_ignore_copyrights.py
@@ -0,0 +1,90 @@
+#
+# Copyright (c) 2018 nexB Inc. and others. All rights reserved.
+# http://nexb.com and https://github.com/nexB/scancode-toolkit/
+# The ScanCode software is licensed under the Apache License version 2.0.
+# Data generated with ScanCode require an acknowledgment.
+# ScanCode is a trademark of nexB Inc.
+#
+# You may not use this software except in compliance with the License.
+# You may obtain a copy of the License at: http://apache.org/licenses/LICENSE-2.0
+# Unless required by applicable law or agreed to in writing, software distributed
+# under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
+# CONDITIONS OF ANY KIND, either express or implied. See the License for the
+# specific language governing permissions and limitations under the License.
+#
+# When you publish or redistribute any data created with ScanCode or any ScanCode
+# derivative work, you must accompany this data with the following acknowledgment:
+#
+#  Generated with ScanCode and provided on an "AS IS" BASIS, WITHOUT WARRANTIES
+#  OR CONDITIONS OF ANY KIND, either express or implied. No content created from
+#  ScanCode should be considered or used as legal advice. Consult an Attorney
+#  for any legal advice.
+#  ScanCode is a free software code scanning tool from nexB Inc. and others.
+#  Visit https://github.com/nexB/scancode-toolkit/ for support and download.
+
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+from __future__ import unicode_literals
+
+import re
+
+from plugincode.output_filter import OutputFilterPlugin
+from plugincode.output_filter import output_filter_impl
+from scancode import CommandLineOption
+from scancode import OUTPUT_FILTER_GROUP
+
+
+@output_filter_impl
+class IgnoreCopyrights(OutputFilterPlugin):
+    """
+    Filter findings that match given copyright holder or author patterns.
+    Has no effect unless the --copyright scan is requested.
+    """
+
+    options = [
+        CommandLineOption(('--ignore-copyright-holders',),
+               multiple=True,
+               metavar='<pattern>',
+               requires=['copyright'],
+               help='Ignore findings with copyright holders matching <pattern>.',
+               help_group=OUTPUT_FILTER_GROUP),
+        CommandLineOption(
+            ('--ignore-authors',),
+            multiple=True,
+            metavar='<pattern>',
+            requires=['copyright'],
+            help='Ignore findings with authors matching <pattern>.',
+            help_group=OUTPUT_FILTER_GROUP)
+    ]
+
+    def is_enabled(self, copyright, ignore_copyright_holders, ignore_authors, **kwargs):
+        return copyright and bool(ignore_copyright_holders or ignore_authors)
+
+    def process_codebase(self, codebase, ignore_copyright_holders, ignore_authors, **kwargs):
+        h_regex = [re.compile(r) for r in ignore_copyright_holders]
+        a_regex = [re.compile(r) for r in ignore_authors]
+
+        for resource in codebase.walk():
+
+            if self._matches(resource, h_regex, 'holders') or self._matches(resource, a_regex, 'authors'):
+
+                resource.is_filtered = True
+                codebase.save_resource(resource)
+
+    def _matches(self, resource, patterns, attr):
+        identities = self._extract_identities(resource, attr)
+
+        for i in identities:
+            if any(p.search(i) for p in patterns):
+                return True
+
+        return False
+
+    def _extract_identities(self, resource, attr):
+        identities = set()
+
+        for copyright in resource.copyrights:
+            identities = identities.union(set(copyright.get(attr, [])))
+
+        return identities
