041b78b76fd5735c929256b9857f154cad084b3f
Philippe Ombredanne <pombredanne@nexb.com>
2018-02-14 00:27:41 +0100

    Updated script and expectation
     * since most of these copyright tests are primarily from Debian they
       highly simialr and biased towards a Debian copyright file convention
    
    Signed-off-by: Philippe Ombredanne <pombredanne@nexb.com>

--- tests/cluecode/test_copyrights_fosso.py
+++ tests/cluecode/test_copyrights_fosso.py
@@ -29,63 +29,95 @@ from __future__ import unicode_literals
 
 import os
 import re
-from unittest.case import expectedFailure
 
-from commoncode.testcase import FileBasedTesting
-from cluecode_assert_utils import check_detection
+from commoncode.testcase import FileDrivenTesting
 import cluecode.copyrights
 
 """
-A test suite for ScanCode using Fossology copyright test data.
+A WIP test suite for ScanCode using Fossology copyright test data.
 """
 
+test_env = FileDrivenTesting()
+test_env.test_data_dir = os.path.join(os.path.dirname(__file__), 'data')
 
-class TestDetectCopyrights(FileBasedTesting):
-    test_data_dir = os.path.join(os.path.dirname(__file__), 'data')
 
-    def test_copyright_detection_with_fossology_data(self):
-        base_dir = self.get_test_loc('copyright_fossology')
-        test_files = [os.path.join(base_dir, tf)
-                      for tf in os.listdir(base_dir) if (not tf.endswith('~'))
-                      # and '36' in tf
-                      ]
+def test_copyright_detection_with_fossology_data():
+    base_dir = test_env.get_test_loc('copyright_fossology')
+    test_files = [os.path.join(base_dir, tf)
+                  for tf in os.listdir(base_dir) if (not tf.endswith('~'))
+                  # and '36' in tf
+                  ]
 
-        expected_files = []
-        files_to_test = []
-        for tf in test_files:
-            if tf.endswith('_raw'):
-                expected_files.append(tf)
-                files_to_test.append(tf.replace('_raw', ''))
+    expected_files = []
+    files_to_test = []
+    for tf in test_files:
+        if tf.endswith('_raw'):
+            expected_files.append(tf)
+            files_to_test.append(tf.replace('_raw', ''))
 
-        assert sorted(test_files) == sorted(files_to_test + expected_files)
+    assert sorted(test_files) == sorted(files_to_test + expected_files)
 
-        copyregex = re.compile('<s>(.*?)</s>', re.DOTALL)
-        authregex = re.compile('<t>(.*>)</t>', re.DOTALL)
-        for expected_file, test_file in zip(expected_files, files_to_test):
-            expected_text = open(expected_file, 'rb').read()
+    copyregex = re.compile('<s>(.*?)</s>', re.DOTALL | re.UNICODE)
+    for expected_file, test_file in zip(expected_files, files_to_test):
+        expected_text = open(expected_file, 'rb').read()
 
-            expected_copyr = []
-            for match in copyregex.finditer(expected_text):
-                expected_copyr.extend(match.groups())
+        expected_copyr = []
+        for match in copyregex.finditer(expected_text):
+            expected_copyr.extend(match.groups())
 
-            expected_auth = []
-            for match in authregex.finditer(expected_text):
-                expected_auth.extend(match.groups())
+        reps = [
+            (b'. All rights reserved.', b'.'),
+            (b'All Rights Reserved except as specified below.', b''),
+            (b' All # Rights Reserved.', b''),
+            (b'#', b' '),
+            (b'  ', b' '),
+            (b'* All Rights Reserved.', b''),
+            (b'All rights reserved', b''),
+            (b'All Rights Reserved', b''),
+            (b'Copyright:', b'Copyright '),
+            (b'. .', b'.'),
+            (b' *% ', b' '),
+            (b'&copy;', b'(c)'),
+            (b' * ', b' '),
+            (b'Copyright  Copyright', b'Copyright'),
+            (b'Copyright Copyright', b'Copyright'),
+            (b'All rights reserved.', b''),
+            (b'Created 1991.', b''),
+            (b'Created 1990.', b''),
+            (b'copyright under', b''),
+            (b'@copyright{}', b''),
+            (b'. .', b'.'),
 
-            copyrights, authors, _years, _holders = cluecode.copyrights.detect(test_file)
+            (b'', b''),
+        ]
+        expected_copyr2 = []
+        for a in expected_copyr:
+            if a.lower().startswith((b'written',  b'auth',b'maint', b'put', b'contri', b'indiv', b'mod')):
+                continue
+            a = b' '.join(a.split())
+            for x, y in reps:
+                a = a.replace(x, y)
+            a= a.strip()
+            a= a.rstrip(b',;:')
+            a= a.strip()
+            a = b' '.join(a.split())
+            expected_copyr2.append(a.strip())
 
+        expected_copyr = [e for e in expected_copyr2 if e and e .strip()]
+
+
+        copyrights, _authors, _years, _holders = cluecode.copyrights.detect(test_file)
+        copyrights = [c.encode('utf-8') for c in copyrights]
+
+        if copyrights != expected_copyr:
             print()
             print('file://' + expected_file)
-            print('file://' + test_file)
+            for ex, cop in map(None, expected_copyr, copyrights):
+                if ex != cop:
+                    print(b'   EX:', ex) 
+                    print(b'   AC:', cop)
+                    print()
 
-            if copyrights != expected_copyr:
-                print('Copyrights')
-                for x, r in map(None, expected_copyr, copyrights):
-                    if x != r:
-                        print(' ', x, ' but -->', r)
 
-            if authors != expected_auth:
-                print('Authors')
-                for x, r in map(None, expected_auth, authors):
-                    if x != r:
-                        print(' ', x, ' but -->', r)
+if __name__ == '__main__':
+    test_copyright_detection_with_fossology_data()
