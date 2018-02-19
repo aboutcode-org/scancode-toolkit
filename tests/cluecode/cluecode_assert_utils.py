7b4cba652461cf2eafce702ba0ebe2bd399749b0
Philippe Ombredanne <pombredanne@nexb.com>
2018-02-19 22:37:06 +0100

    Create multiple tests for copyright, holders, etc
     * and this from a simgle yml files.
     * the schema has been updated accordingly
    
    Signed-off-by: Philippe Ombredanne <pombredanne@nexb.com>

--- tests/cluecode/cluecode_assert_utils.py
+++ tests/cluecode/cluecode_assert_utils.py
@@ -43,7 +43,6 @@ from commoncode.text import python_safe_name
 from commoncode.fileutils import parent_directory
 from commoncode.fileutils import file_name
 
-
 """
 Data-driven Copyright test utilities.
 """
@@ -71,11 +70,10 @@ def check_detection(expected, test_file,
     tst.data_file = data_file
 
     tst.what = what
-    tst.expectations = expected
+    tst.expected = expected
     tst.notes = notes
     tst.expected_failure = expected_failure
     tst.dump()
-
 
 
 @attr.s(slots=True)
@@ -100,9 +98,16 @@ class CopyrightTest(object):
     """
     data_file = attr.ib(default=None)
     test_file = attr.ib(default=None)
-    what = attr.ib(default='copyrights')
-    expectations = attr.ib(default=attr.Factory(list))
-    expected_failure = attr.ib(default=False)
+    # one of holders, copyrights, authors, years
+    what = attr.ib(default=attr.Factory(list))
+    copyrights = attr.ib(default=attr.Factory(list))
+    copyrights_expected_failure = attr.ib(default=False)
+    holders = attr.ib(default=attr.Factory(list))
+    holders_expected_failure = attr.ib(default=False)
+    years = attr.ib(default=attr.Factory(list))
+    years_expected_failure = attr.ib(default=False)
+    authors = attr.ib(default=attr.Factory(list))
+    authors_expected_failure = attr.ib(default=False)
     notes = attr.ib(default=None)
 
     def __attrs_post_init__(self, *args, **kwargs):
@@ -120,7 +125,9 @@ class CopyrightTest(object):
                     if b'_file' in field.name]
         fields_filter = attr.filters.exclude(*filtered)
         data = attr.asdict(self, filter=fields_filter, dict_factory=OrderedDict)
-        return OrderedDict([(key, value) for key, value in data.items() if value])
+        return OrderedDict([(key, value) for key, value in data.items()
+                            # do not dump false and empties
+                            if value])
 
     def dump(self, check_exists=False):
         """
@@ -171,7 +178,7 @@ def load_copyright_tests(test_dir=test_env.test_data_dir):
         print('Dangling missingcopyright test files')
         for o in sorted(dangling):
             print(o)
-            raise Exception('Dangling/missing copyright test files: '+ repr(o))
+            raise Exception('Dangling/missing copyright test files: ' + repr(o))
 
     # ensure that each data file has a corresponding test file
     diff = set(data_files.keys()).symmetric_difference(set(test_files.keys()))
@@ -191,39 +198,55 @@ def load_copyright_tests(test_dir=test_env.test_data_dir):
         yield CopyrightTest(data_file, test_file)
 
 
-def make_copyright_test_function(test, test_name):
+def make_copyright_test_functions(test, test_data_dir=test_env.test_data_dir):
     """
-    Build and return a test function closing on tests arguments.
+    Build and yield test functions closing on tests arguments and the function
+    name.
     """
 
-    def closure_test_function(*args, **kwargs):
-        copyrights, authors, years, holders = cluecode.copyrights.detect(test.test_file)
-        results = OrderedDict([
-            ('copyrights', copyrights),
-            ('authors', authors),
-            ('years', years),
-            ('holders', holders),
-        ])
-        result = results[test.what]
-        try:
-            assert test.expectations == result
-        except:
-            # On failure, we compare against more result data to get additional
-            # failure details, including the test_file and full results
-            # this assert will always fail and provide a more detailed failure trace
-            assert test.expectations == results.items() + [
-                test_name, 
-                'data file: file://' + test.data_file,
-                'test file: file://' + test.test_file, 
-            ]
+    for what in test.what:
 
-    closure_test_function.__name__ = test_name
-    closure_test_function.funcname = test_name
+        def closure_test_function(*args, **kwargs):
+            copyrights, authors, years, holders = cluecode.copyrights.detect(test_file)
+            results = OrderedDict([
+                ('copyrights', copyrights),
+                ('authors', authors),
+                ('years', years),
+                ('holders', holders),
+            ])
+            result = results[what]
+            try:
+                assert expected == result
+            except:
+                # On failure, we compare against more result data to get additional
+                # failure details, including the test_file and full results
+                # this assert will always fail and provide a more detailed failure trace
+                assert expected == results.items() + [
+                    test_name,
+                    'data file: file://' + data_file,
+                    'test file: file://' + test_file,
+                ]
 
-    if test.expected_failure:
-        closure_test_function = expectedFailure(closure_test_function)
+        data_file = test.data_file
+        test_file = test.test_file
 
-    return closure_test_function
+        tfn = test_file.replace(test_data_dir, '').strip('\/\\')
+        test_name = 'test_%(what)s_%(tfn)s' % locals()
+        test_name = python_safe_name(test_name)
+        if isinstance(test_name, unicode):
+            test_name = test_name.encode('utf-8')
+
+        expected = getattr(test, what, [])
+
+        closure_test_function.__name__ = test_name
+        closure_test_function.funcname = test_name
+
+        expected_failure = getattr(test, what + '_expected_failure', False)
+
+        if expected_failure:
+            closure_test_function = expectedFailure(closure_test_function)
+
+        yield closure_test_function, test_name
 
 
 def build_tests(copyright_tests, clazz, test_data_dir=test_env.test_data_dir):
@@ -232,13 +255,7 @@ def build_tests(copyright_tests, clazz, test_data_dir=test_env.test_data_dir):
     these method to the clazz test class.
     """
     for test in copyright_tests:
-        tfn = test.test_file
-        tfn = tfn.replace(test_data_dir, '').strip('\/\\')
-        test_name = 'test_%(tfn)s' % locals()
-        test_name = python_safe_name(test_name)
-        if isinstance(test_name, unicode):
-            test_name = test_name.encode('utf-8')
         # closure on the test params
-        test_method = make_copyright_test_function(test, test_name)
-        # attach that method to our test class
-        setattr(clazz, test_name, test_method)
+        for test_method, test_name in make_copyright_test_functions(test, test_data_dir):
+            # attach that method to our test class
+            setattr(clazz, test_name, test_method)
