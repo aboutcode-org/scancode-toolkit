8ca9921f4be2c742926219348a1ef333a9d4304b
Philippe Ombredanne <pombredanne@nexb.com>
2018-03-02 13:15:09 -0800

    Bump version to 2.9.0b1
     * this is a stable beta release from the 2.x series
    
    Signed-off-by: Philippe Ombredanne <pombredanne@nexb.com>

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ from setuptools import find_packages
 from setuptools import setup
 
 
-version = '2.3.0'
+version = '2.9.0b1'
 
 
 #### Small hack to force using a plain version number if the option
