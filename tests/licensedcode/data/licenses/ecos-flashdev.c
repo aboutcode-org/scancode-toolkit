1291608bb8d983d952a47904778ac0ec77af16dd
Philippe Ombredanne <pombredanne@nexb.com>
2018-03-10 11:06:32 -0800

    Add new ecos detections rules and tests
    Signed-off-by: Philippe Ombredanne <pombredanne@nexb.com>

---
+++ tests/licensedcode/data/licenses/ecos-flashdev.c
@@ -0,0 +1,51 @@
+//=================================================================
+//
+//        flashdev.c
+//
+//        Simple tests for FLASHdev driver
+//
+//=================================================================
+// ####ECOSGPLCOPYRIGHTBEGIN####                                            
+// -------------------------------------------                              
+// This file is part of eCos, the Embedded Configurable Operating System.   
+// Copyright (C) 1998, 1999, 2000, 2001, 2002, 2004, 2005, 2006, 2008 Free Software Foundation, Inc.
+//
+// eCos is free software; you can redistribute it and/or modify it under    
+// the terms of the GNU General Public License as published by the Free     
+// Software Foundation; either version 2 or (at your option) any later      
+// version.                                                                 
+//
+// eCos is distributed in the hope that it will be useful, but WITHOUT      
+// ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or    
+// FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License    
+// for more details.                                                        
+//
+// You should have received a copy of the GNU General Public License        
+// along with eCos; if not, write to the Free Software Foundation, Inc.,    
+// 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.            
+//
+// As a special exception, if other files instantiate templates or use      
+// macros or inline functions from this file, or you compile this file      
+// and link it with other works to produce a work based on this file,       
+// this file does not by itself cause the resulting work to be covered by   
+// the GNU General Public License. However the source code for this file    
+// must still be made available in accordance with section (3) of the GNU   
+// General Public License v2.                                               
+//
+// This exception does not invalidate any other reasons why a work based    
+// on this file might be covered by the GNU General Public License.         
+// -------------------------------------------                              
+// ####ECOSGPLCOPYRIGHTEND####                                              
+//=================================================================
+//#####DESCRIPTIONBEGIN####
+//
+// Author(s):     nickg, gthomas
+// Contributors:  
+// Date:          2005-10-25
+// Description:   Simple test of FLASH I/O device. This is really
+//                just a copy of the flash1 test, but using the
+//                /dev/flash interface instead of the flash library.
+//
+// Options:
+//####DESCRIPTIONEND####
+//=================================================================
