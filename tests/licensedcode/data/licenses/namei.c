b04ae05e1c48dcf42f9d96edf1cf5445718a2974
Philippe Ombredanne <pombredanne@nexb.com>
2018-03-21 14:39:09 -0700

    Add new license tests
    Signed-off-by: Philippe Ombredanne <pombredanne@nexb.com>

---
+++ tests/licensedcode/data/licenses/namei.c
@@ -0,0 +1,11 @@
+/*
+ * Copyright (C) 2011 Novell Inc.
+ * Copyright (C) 2016 Red Hat, Inc.
+ *
+// SPDX-License-Identifier: gpl-2.0 */
+
+#include <linux/fs.h>
+#include <linux/cred.h>
+#include <linux/namei.h>
+#include <linux/xattr.h>
+
