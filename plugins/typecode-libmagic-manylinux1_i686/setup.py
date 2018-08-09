faa7db366ef37232913d6b3fcc8257c74579d38f
Philippe Ombredanne <pombredanne@nexb.com>
2018-08-09 12:57:17 +0200

    Add plugins for libmagic #469
    Signed-off-by: Philippe Ombredanne <pombredanne@nexb.com>

---
+++ plugins/typecode-libmagic-manylinux1_i686/setup.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python
+# -*- encoding: utf-8 -*-
+
+from __future__ import absolute_import
+from __future__ import print_function
+
+from glob import glob
+from os.path import basename
+from os.path import join
+from os.path import splitext
+
+from setuptools import find_packages
+from setuptools import setup
+
+
+desc = '''A ScanCode path provider plugin to provide a prebuilt native libmagic binary and database.'''
+
+setup(
+    name='typecode-libmagic',
+    version='5.22',
+    license='bsd-simplified',
+    description=desc,
+    long_description=desc,
+    author='nexB',
+    author_email='info@aboutcode.org',
+    url='https://github.com/nexB/scancode-toolkit',
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
+    py_modules=[splitext(basename(path))[0] for path in glob('src/*.py')],
+    include_package_data=True,
+    zip_safe=False,
+    classifiers=[
+        # complete classifier list: http://pypi.python.org/pypi?%3Aaction=list_classifiers
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        #'TODO'
+        'License :: OSI Approved :: Apache Software License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.7',
+        'Topic :: Utilities',
+    ],
+    keywords=[
+        'open source', 'typecode', 'libmagic'
+    ],
+    install_requires=[
+        'scancode-toolkit',
+    ],
+    entry_points={
+        'scancode_location_provider': [
+            'typecode-libmagic = typecode_libmagic:LibmagicPaths',
+        ],
+    },
+)
