99bb0c21a8dd967a8762c238655a20e083c7eefd
Philippe Ombredanne <pombredanne@nexb.com>
2018-08-20 16:11:45 +0200

    Use attr instead of schematics for models
    Also remove very generic zip/tar/rar package types
    
    
    Signed-off-by: Philippe Ombredanne <pombredanne@nexb.com>

--- src/commoncode/datautils.py
+++ src/commoncode/datautils.py
@@ -29,6 +29,8 @@ from __future__ import unicode_literals
 from collections import OrderedDict
 
 import attr
+import typing
+
 
 """
 Utilities and helpers for data classes.
@@ -45,11 +47,15 @@ except NameError:
     # Python 3
     unicode = str  # NOQA
 
+
 HELP_METADATA = '__field_help'
+LABEL_METADATA = '__field_label'
 
 
-def attribute(default=attr.NOTHING, validator=None, repr=False, cmp=True,  # NOQA
-              init=True, metadata=None, type=None, converter=None, help=None):  # NOQA
+def attribute(default=attr.NOTHING, validator=None,
+              repr=False, cmp=True,  # NOQA
+              init=True, type=None, converter=None,  # NOQA
+              help=None, label=None, metadata=None,):  # NOQA
     """
     A generic attribute with help metadata and that is not included in the
     representation by default.
@@ -58,6 +64,9 @@ def attribute(default=attr.NOTHING, validator=None, repr=False, cmp=True,  # NOQ
     if help:
         metadata[HELP_METADATA] = help
 
+    if label:
+        metadata[LABEL_METADATA] = label
+
     return attr.attrib(
         default=default, validator=validator, repr=repr, cmp=cmp, init=init,
         metadata=metadata, type=type, converter=converter
@@ -65,67 +74,131 @@ def attribute(default=attr.NOTHING, validator=None, repr=False, cmp=True,  # NOQ
 
 
 def Boolean(default=False, validator=None, repr=False, cmp=True,  # NOQA
-            init=True, metadata=None, converter=None, help=None):  # NOQA
+            converter=None, label=None, help=None,):  # NOQA
     """
     A boolean attribute.
     """
     return attribute(
-        default=default, validator=validator, repr=repr, cmp=cmp, init=init,
-        metadata=metadata, type=bool, converter=converter, help=help
+        default=default,
+        validator=validator,
+        repr=repr,
+        cmp=cmp,
+        init=True,
+        type=bool,
+        converter=converter,
+        help=help,
+        label=label,
     )
 
 
-def String(default='', validator=None, repr=False, cmp=True,  # NOQA
-           init=True, metadata=None, converter=None, help=None):  # NOQA
+def String(default=None, validator=None, repr=False, cmp=True,  # NOQA
+           converter=None, label=None, help=None,):  # NOQA
     """
     A string attribute.
     """
     return attribute(
-        default=default, validator=validator, repr=repr, cmp=cmp, init=init,
-        metadata=metadata, type=str, converter=converter, help=help
+        default=default,
+        validator=validator,
+        repr=repr, cmp=cmp,
+        init=True,
+        type=str,
+        converter=converter,
+        help=help,
+        label=label,
     )
 
 
-def Int(default=0, validator=None, repr=False, cmp=True,  # NOQA
-        init=True, metadata=None, converter=None, help=None):  # NOQA
+def Integer(default=0, validator=None, repr=False, cmp=True,  # NOQA
+            converter=None, label=None, help=None,):  # NOQA
     """
     An integer attribute.
     """
+    converter = converter or attr.converters.optional(int)
     return attribute(
-        default=default, validator=validator, repr=repr, cmp=cmp, init=init,
-        metadata=metadata, type=int, converter=converter, help=help
+        default=default,
+        validator=validator,
+        repr=repr,
+        cmp=cmp,
+        init=True,
+        type=int,
+        converter=converter,
+        help=help,
+        label=label,
     )
 
 
 def Float(default=0.0, validator=None, repr=False, cmp=True,  # NOQA
-          init=True, metadata=None, converter=None, help=None):  # NOQA
+          converter=None, label=None, help=None,):  # NOQA
     """
     A float attribute.
     """
     return attribute(
-        default=default, validator=validator, repr=repr, cmp=cmp, init=init,
-        metadata=metadata, type=float, converter=converter, help=help
+        default=default,
+        validator=validator,
+        repr=repr,
+        cmp=cmp,
+        init=True,
+        type=float,
+        converter=converter,
+        help=help,
+        label=label,
     )
 
 
-def List(validator=None, repr=False, cmp=True,  # NOQA
-         init=True, metadata=None, converter=None, help=None):  # NOQA
+def List(item_type=typing.Any, default=attr.NOTHING, validator=None, repr=False, cmp=True,  # NOQA
+         converter=None, label=None, help=None,):  # NOQA
     """
-    A list attribute.
+    A list attribute: the optional item_type defines the type of items it stores.
     """
+    if default is attr.NOTHING:
+        default = attr.Factory(list)
+
     return attribute(
-        default=attr.Factory(list), validator=validator, repr=repr, cmp=cmp,
-        init=init, metadata=metadata, type=list, converter=converter, help=help
+        default=default,
+        validator=validator,
+        repr=repr,
+        cmp=cmp,
+        init=True,
+        type=typing.List[item_type],
+        converter=converter,
+        help=help,
+        label=label,
     )
 
 
-def Mapping(validator=None, repr=False, cmp=True,  # NOQA
-            init=True, metadata=None, converter=None, help=None):  # NOQA
+def Mapping(value_type=typing.Any, default=attr.NOTHING, validator=None, repr=False, cmp=True,  # NOQA
+            converter=None, help=None, label=None):  # NOQA
     """
-    A mapping attribute.
+    A mapping attribute: the optional value_type defines the type of values it
+    stores. The key is always a string.
+
+    Notes: in Python 2 the type is Dict as there is no typing available for
+    OrderedDict for now.
     """
+    if default is attr.NOTHING:
+        default = attr.Factory(OrderedDict)
+
     return attribute(
-        default=attr.Factory(OrderedDict), validator=validator, repr=repr,
-        cmp=cmp, init=init, metadata=metadata, type=OrderedDict,
-        converter=converter, help=help
+        default=default,
+        validator=validator,
+        repr=repr,
+        cmp=cmp,
+        init=True,
+        type=typing.Dict[str, value_type],
+        converter=converter,
+        help=help,
+        label=label,
     )
+
+
+##################################################
+# FIXME: add proper support for dates!!!
+##################################################
+
+def Date(default=None, validator=None, repr=False, cmp=True,  # NOQA
+           converter=None, label=None, help=None,):  # NOQA
+    """
+    A date attribute. It always serializes to an ISO date string.
+    Behavior is TBD and for now this is exactly a string.
+    """
+    return String(default, validator, repr, cmp, converter, label, help)
