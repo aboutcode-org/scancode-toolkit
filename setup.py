658cbd266a7bf478294ded92aa2e809b7bccc496
Philippe Ombredanne <pombredanne@nexb.com>
2018-09-28 16:02:07 +0200

    Bump py2-ipaddress #1183 and #1193
    Signed-off-by: Philippe Ombredanne <pombredanne@nexb.com>

--- setup.py
+++ setup.py
@@ -126,7 +126,7 @@ setup(
         # Some nltk version ranges are buggy
         'nltk >= 3.2, < 4.0',
         'publicsuffix2',
-        'py2-ipaddress >= 2.0, <3.0',
+        'py2-ipaddress >= 2.0, <3.5',
         'url >= 0.1.4, < 0.1.6',
         'fingerprints == 0.5.4',
 
