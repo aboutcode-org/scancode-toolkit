f4a832378ee2e65f6319d671161a13e47ed06fb7
Philippe Ombredanne <pombredanne@nexb.com>
2018-10-25 18:55:40 -0700

    Bump to 2.9.7
    Signed-off-by: Philippe Ombredanne <pombredanne@nexb.com>

--- src/scancode_config.py
+++ src/scancode_config.py
@@ -97,7 +97,7 @@ try:
     __version__ = get_distribution('scancode-toolkit').version
 except DistributionNotFound:
     # package is not installed ??
-    __version__ = '2.9.5'
+    __version__ = '2.9.7'
 
 system_temp_dir = tempfile.gettempdir()
 scancode_src_dir = dirname(__file__)
