557146188ae811cf18746668febea8a00a040a73
Philippe Ombredanne <pombredanne@nexb.com>
2018-10-30 14:06:37 -0700

    Update Windows test with a copy of the env var #1231
    * Windows needs a proper set of base env vars. So we copy it and only
      update this copy for our command test runs.
    
    
    Signed-off-by: Philippe Ombredanne <pombredanne@nexb.com>

--- tests/scancode/test_cli.py
+++ tests/scancode/test_cli.py
@@ -781,7 +781,8 @@ def test_scan_keep_temp_files_is_false_by_default():
     result_file = test_env.get_temp_file('json')
     # mock using a well defined temporary directory
     temp_directory = test_env.get_temp_dir()
-    env = {'SCANCODE_TEMP': temp_directory}
+    env = dict(os.environ)
+    env.update({'SCANCODE_TEMP': temp_directory})
     args = [
         '--info', test_file, '--json', result_file,
         # this forces using a temp file cache
@@ -796,7 +797,8 @@ def test_scan_keep_temp_files_keeps_files():
     result_file = test_env.get_temp_file('json')
     # mock using a well defined temporary directory
     temp_directory = test_env.get_temp_dir()
-    env = {'SCANCODE_TEMP': temp_directory}
+    env = dict(os.environ)
+    env.update({'SCANCODE_TEMP': temp_directory})
     args = [
         '--keep-temp-files', '--info', test_file, '--json', result_file,
         # this forces using a temp file cache
