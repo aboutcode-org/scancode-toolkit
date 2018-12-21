ba9860fda379eb8f3a3b9e33f60baaa43419e7fd
Matth?us G. Chajdas <dev@anteru.net>
2018-12-21 17:34:15 +0100

    Small cleanups to the Slurm lexer.
    Remove debug output, add a test file for the Slurm lexer.

--- pygments/lexers/shell.py
+++ pygments/lexers/shell.py
@@ -129,23 +129,25 @@ class BashLexer(RegexLexer):
 class SlurmBashLexer(BashLexer):
     """
     Lexer for (ba|k|z|)sh Slurm scripts.
+
+    .. versionadded:: 2.4
     """
 
     name = 'Slurm'
     aliases = ['slurm', 'sbatch']
     filenames = ['*.sl']
+    mimetypes = []
     EXTRA_KEYWORDS = {'srun'}
 
     def get_tokens_unprocessed(self, text):
         for index, token, value in BashLexer.get_tokens_unprocessed(self, text):
-            print(index, token, value)
             if token is Text and value in self.EXTRA_KEYWORDS:
                 yield index, Name.Builtin, value
             elif token is Comment.Single and 'SBATCH' in value:
                 yield index, Keyword.Pseudo, value
             else:
                 yield index, token, value
-        
+
 class ShellSessionBaseLexer(Lexer):
     """
     Base lexer for simplistic shell sessions.
