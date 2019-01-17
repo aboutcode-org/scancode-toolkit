4f46c73630468780a4ca4d995e2ecd7d7092d852
Steven Esser <sesser@nexb.com>
2019-01-17 15:55:17 -0800

    Fix duplicate vcs_tool string in vcs_url for npm packages #1314
    Signed-off-by: Steven Esser <sesser@nexb.com>

--- tests/packagedcode/test_packagedcode_utils.py
+++ tests/packagedcode/test_packagedcode_utils.py
@@ -59,3 +59,8 @@ class TestParseUrl(TestCase):
         expected = 'https://bitbucket.org/vendor/my-private-repo.git'
         assert expected == result
 
+    def test_parse_repo_url_git_plus_https(self):
+        url = 'git+https://github.com/stevepapa/angular2-autosize.git'
+        result = parse_repo_url(url)
+        expected = 'git+https://github.com/stevepapa/angular2-autosize.git'
+        assert expected == result
