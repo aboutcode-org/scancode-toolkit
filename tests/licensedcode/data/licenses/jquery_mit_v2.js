0520277dcf0cbc19793fb8bdafc1d739f553fca8
Philippe Ombredanne <pombredanne@nexb.com>
2019-01-22 13:17:03 +0100

    Update bare GPL license detection rules #1335
     * Fix some cases of JQuery-related multiple licenses
     * Fix some imcorrect linux-openib that were retruning also a GPL
     * Refine and review other bare GPL rules
    
    Reported-by: Thomas Gleixner <tglx@linutronix.de>
    Signed-off-by: Philippe Ombredanne <pombredanne@nexb.com>

---
+++ tests/licensedcode/data/licenses/jquery_mit_v2.js
@@ -0,0 +1,12 @@
+• jQuery JavaScript Library v1.4.2
+• http://jquery.com/
+• 
+• Copyright 2010, John Resig
+• Dual licensed under the MIT or GPL Version 2 licenses.
+• http://jquery.org/license
+• 
+• Includes Sizzle.js
+• http://sizzlejs.com/
+• Copyright 2010, The Dojo Foundation
+• Released under the MIT, BSD, and GPL Licenses.
+ *  More information: http://sizzlejs.com/
