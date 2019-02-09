2d3e7cc5e12cecb3d5733fbce1ab8c049b8899f7
Philippe Ombredanne <pombredanne@nexb.com>
2019-02-09 02:36:19 +0100

    Add new javaproperties libraries
    This is ti support parsing pom.properties foudn sometimes side by side
    with a pom file to improve the quality of collected data.
    
    Signed-off-by: Philippe Ombredanne <pombredanne@nexb.com>

--- setup.py
+++ setup.py
@@ -176,6 +176,7 @@ setup(
         'requests >= 2.7.0, < 3.0.0',
         'packageurl-python >= 0.7.0',
         'xmltodict >= 0.11.0',
+        'javaproperties >= 0.5',
 
         # scancode
         'click >= 6.0.0, < 7.0.0',
