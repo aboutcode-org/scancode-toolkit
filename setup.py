b4db7243ff379ecc5e1e1883bcaf8ff5915759b3
Philippe Ombredanne <pombredanne@nexb.com>
2019-02-15 15:24:39 +0100

    Fix version to 3.0.1
    Signed-off-by: Philippe Ombredanne <pombredanne@nexb.com>

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ import sys
 from setuptools import find_packages
 from setuptools import setup
 
-version = '3.0.'
+version = '3.0.1'
 
 #### Small hack to force using a plain version number if the option
 #### --plain-version is passed to setup.py
