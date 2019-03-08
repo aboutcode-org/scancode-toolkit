4e68ca55edfbefe8d6febeed0f0c21f5bb072133
Abhishek-Dev09 <46627868+Abhishek-Dev09@users.noreply.github.com>
2019-03-09 04:33:11 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -127,7 +127,7 @@ setup(
         # Some nltk version ranges are buggy
         'nltk >= 3.2, < 4.0',
         'publicsuffix2',
-        'py2-ipaddress >= 2.0, <3.5',
+        'ipaddress',
         'url >= 0.1.4, < 0.1.6',
         'fingerprints == 0.5.4',
 
