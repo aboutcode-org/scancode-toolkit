84f47a67a905f05d39c80ebae8c56e7959ece935
Philippe Ombredanne <pombredanne@nexb.com>
2019-04-30 15:31:06 +0200

    Refactor automaton matching in smaller functions
    Signed-off-by: Philippe Ombredanne <pombredanne@nexb.com>

--- src/licensedcode/match_aho.py
+++ src/licensedcode/match_aho.py
@@ -1,5 +1,5 @@
 #
-# Copyright (c) 2015 nexB Inc. and others. All rights reserved.
+# Copyright (c) nexB Inc. and others. All rights reserved.
 # http://nexb.com and https://github.com/nexB/scancode-toolkit/
 # The ScanCode software is licensed under the Apache License version 2.0.
 # Data generated with ScanCode require an acknowledgment.
@@ -25,6 +25,7 @@
 from __future__ import print_function, absolute_import, division
 
 from array import array
+from itertools import groupby
 
 import ahocorasick
 
@@ -92,67 +93,98 @@ def exact_match(idx, query_run, automaton):
     """
     if TRACE: logger_debug(' #exact_AHO: start ... ')
     if TRACE_DEEP: logger_debug(' #exact_AHO: query_run:', query_run)
+    matcher = MATCH_AHO_EXACT
+
+    matches = []
+    matches_append = matches.append
+
+    qbegin = query_run.start
+
+    matched_positions = get_matched_positions(query_run.tokens, qbegin, automaton)
+    matched_spans = get_matched_spans(matched_positions, query_run.matchables)
 
     len_junk = idx.len_junk
     rules_by_rid = idx.rules_by_rid
-
-    qtokens = query_run.tokens
-    qbegin = query_run.start
-    query_run_matchables = query_run.matchables
+    tids_by_rid = idx.tids_by_rid
     query = query_run.query
 
-    qtokens_as_str = array('h', qtokens).tostring()
-    matches = []
+    for rid, qspan, ispan in matched_spans:
+        itokens = tids_by_rid[rid]
+        hispan = Span(p for p in ispan if itokens[p] >= len_junk)
 
-    # iterate over matched strings: the matched value is (rule id, index start pos, index end pos)
-    for qend, matched_rule_segments in automaton.iter(qtokens_as_str):
-
-        for rid, istart, iend in matched_rule_segments:
-            rule = rules_by_rid[rid]
-            if TRACE_DEEP: logger_debug('   #exact_AHO: found match to rule:', rule.identifier)
-
-            ################################
-            # FIXME: use a trie of ints or a trie or Unicode characters to avoid this shenaningan
-            ################################
-            # Since the Tries stores bytes and we have two bytes per tokenid, the
-            # real end must be adjusted
-            real_qend = (qend - 1) / 2
-            # ... and there is now a real possibility of a false match.
-            # For instance say we have these tokens :
-            #   gpl encoded as 0012 and lgpl encoded as 1200 and mit as 2600
-            # And if we scan this "mit lgpl" we get this encoding 2600 1200.
-            # The automaton will find a matched string  of 0012 to gpl in the middle
-            # matching falsely so we check that the corrected end qposition
-            # must be always an integer.
-            real_qend_int = int(real_qend)
-            if real_qend != real_qend_int:
-                if TRACE: logger_debug('   #exact_AHO: real_qend != int(real_qend), discarding rule match:', rule.identifier)
-                continue
-
-            match_len = iend + 1 - istart
-            matcher = match_len == rule.length and MATCH_AHO_EXACT or MATCH_AHO_FRAG
-
-            real_qend = real_qend_int
-            qposses = range(qbegin + real_qend - match_len + 1, qbegin + real_qend + 1)
-
-            if any(p not in query_run_matchables for p in qposses):
-                if TRACE: logger_debug(
-                    '   #exact_AHO: not matchable match: '
-                    'any(p not in query_run_matchables for p in qposses), discarding rule:',
-                    rule.identifier)
-                continue
-
-            qspan = Span(qposses)
-            ispan = Span(range(istart, iend + 1))
-
-            itokens = idx.tids_by_rid[rid]
-            hispan = Span(p for p in ispan if itokens[p] >= len_junk)
-
-            match = LicenseMatch(rule, qspan, ispan, hispan, qbegin, matcher=matcher, query=query)
-            matches.append(match)
+        rule = rules_by_rid[rid]
+        match = LicenseMatch(
+            rule, qspan, ispan, hispan, qbegin, matcher=matcher, query=query)
+        matches_append(match)
 
     if TRACE and matches:
         logger_debug(' ##exact_AHO: matches found#', matches)
         map(print, matches)
 
     return matches
+
+
+def get_matched_spans(positions, matchables):
+    """
+    Yield tuples of matched spans as (rid, qspan, ispan) from an iterable of
+    (rid, qstart, qend, istart, iend). Skip position that is not entirely
+    within the `matchables` set of matchable positions.
+    """
+    for rid, match_qstart, match_qend, istart, iend in positions:
+        
+        qspan = Span(list(range(match_qstart, match_qend)))
+        # TODO: is this about negatives? this should be optimized?
+        # if any(p not in query_run_matchables for p in qspan):
+        if any(p not in matchables for p in qspan):  # not qspan.set.issubset(matchables):
+            if TRACE: logger_debug(
+                '   #exact_AHO:get_matched_spans not matchable match: any(p not in '
+                'query_run_matchables for p in qspan), discarding rule:', rid)
+            continue
+        ispan = Span(list(range(istart, iend)))
+        yield rid, qspan, ispan
+
+
+def get_matched_positions(tokens, qbegin, automaton):
+    """
+    Yield tuples of matched positions as (rid, qstart, qend, istart, iend) such
+    that these start and end positions are suitable as range() arguments. Match
+    `tokens` sequence of token ids starting at the `qbegin` absolute query start
+    position position using the `automaton`.
+    """
+    # iterate over matched strings: the matched value is (rule id, index start
+    # pos, index end pos)
+    qtokens_as_str = array('h', tokens).tostring()
+    for qend, matched_rule_segments in automaton.iter(qtokens_as_str):
+
+        for rid, istart, iend in matched_rule_segments:
+            if TRACE_DEEP: logger_debug(
+                '   #EXACT get_matches: found match to rule:', rid)
+
+            ################################
+            # FIXME: use a trie of ints or a trie of Unicode characters to avoid
+            # this shenaningan. NB: this will be possible when using Python 3
+            ################################
+            # Since the Tries stores bytes and we have two bytes per tokenid,
+            # the real end must be adjusted
+            real_qend = (qend - 1) / 2
+            # ... and there is now a real possibility of a false match. For
+            # instance say we have these tokens : gpl encoded as 0012 and lgpl
+            # encoded as 1200 and mit as 2600 And if we scan this "mit lgpl" we
+            # get this encoding 2600 1200. The automaton will find a matched
+            # string of 0012 to gpl in the middle matching falsely so we check
+            # that the corrected end qposition must be always an integer.
+            real_qend_int = int(real_qend)
+            if real_qend != real_qend_int:
+                if TRACE: logger_debug(
+                    '   #EXACT get_matches: real_qend != int(real_qend), '
+                    'discarding match to rule:', rid)
+                continue
+
+            iend = iend + 1
+            match_len = iend - istart
+
+            qend = qbegin + int(real_qend) + 1
+            qstart = qend - match_len
+
+            yield rid, qstart, qend, istart, iend
+
