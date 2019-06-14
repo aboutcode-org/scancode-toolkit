ed10b6598dbfe2e9c5a3e7cf796f6daabab9e25d
Philippe Ombredanne <pombredanne@nexb.com>
2019-06-14 17:34:09 +0200

    Add new pytest plugin #1598 #1519
    This plugin can now do the following:
    
    - selectively run test marked as supported on Python 3 when running on
      Python 3
    - selectively run all tests (including slow and smoke tests) otherwise
      only runs the "regular" non-slow, non-smoke tests
    - run only tests impacted by code changes when doing a diff either
      against the current branch as a base or a provided base branch (such
      as the develop) for #1519
    
    Use pytest --help and pytest --markers for details.
    
    Signed-off-by: Philippe Ombredanne <pombredanne@nexb.com>

--- conftest.py
+++ conftest.py
@@ -19,32 +19,225 @@ import os
 from os import path
 from subprocess import check_output
 from subprocess import STDOUT
+import sys
 
 import pytest
 
 
+TRACE = True
+
 """
-A pytest conftest.py for scancode-toolkit to speed test runs. 
-It tries to find which code modules have changed using git, and selectively runs
-only the tests for the impacted modules when running in a feature branch. When
-running on the develop or master branches all the tests run: none are skipped.
+A pytest conftest.py for scancode-toolkit to control which tests to run and when.
+
+We use custom markers and command line options to determine which test suite to run.
+
+To retest only code impacted by a change, it tries to find which code modules
+have changed using git, and selectively runs only the tests for the impacted
+modules when running in a feature branch. When running on the develop or master
+branches all the tests run: none are skipped.
 """
+
+
+_sys_v0 = sys.version_info[0]
+PY2 = _sys_v0 == 2
+PY3 = _sys_v0 == 3
+
+
+################################################################################
+# pytest custom markers and CLI options
+################################################################################
+
+
+def pytest_configure(config):
+    config.addinivalue_line('markers', 'scanpy3: Mark a ScanCode test as supported on Python 3.')
+    config.addinivalue_line('markers', 'scansmoke: Mark a ScanCode test as a smoke test.')
+    config.addinivalue_line('markers', 'scanslow: Mark a ScanCode test as a slow, long running test.')
+
+
+TEST_SUITES = 'smoke', 'standard', 'all',
+
+
+def pytest_addoption(parser):
+    """
+    Add options used for ScanCode tests customization.
+    """
+    group = parser.getgroup('scancode', 'Test suite options for ScanCode')
+
+    group.addoption(
+        '--force-py3',
+        dest='force_py3',
+        action='store_true',
+        default=False,
+        help='Run all the scancode tests on Python 3, irrespective of their '
+             'markers. If not selected, only the tests marked as compatible '
+             'with Python 3 will run on Python 3. '
+             'Use the @pytest.mark.scanpy3 marker to mark test functions, '
+             'classes or modules that are supported on Python3. '
+             'This marker is there to help with the Python 3 port and will be '
+             'removed in the future.',
+    )
+
+    group.addoption(
+        '--test-suite',
+        action='store',
+        choices=TEST_SUITES,
+        dest='test_suite',
+        default='standard',
+        help='Select which test suite to run: '
+             '"smoke" is a minimal integration test suite; '
+             '"standard" is the standard test suite that is designed to run '
+             'reasonably fast; it does not include "smoke" tests and "slow" tests.'
+             '"all" runs all the tests including "slow" (long running), "smoke" and "standard" tests. '
+             'Use the @pytest.mark.scansmoke marker to mark a test as a "smoke" test.'
+             'Use the @pytest.mark.scanslow marker to mark a test as "slow" test '
+             'that will only run when running "all" tests.'
+        )
+
+    group.addoption(
+        '--changed-only',
+        dest='changed_only',
+        action='store_true',
+        default=False,
+        help='Run only the subset of tests impacted by your changes.'
+             'If selected, you can provide an optional --base-branch  and the '
+             'changes are checked against that branch. '
+             'Otherwise, a git diff is made against the current branch.',
+    )
+
+    group.addoption(
+        '--base-branch',
+        dest='base_branch',
+        action='store',
+        default=None,
+        help='Optional name branch  of the branch diff against to find what has '
+             'changed if --changed-only is selected.',
+    )
+
+    group.addoption(
+        '--dry-run',
+        dest='dry_run',
+        action='store_true',
+        default=False,
+        help='Only print selected and deselected tests. Do not run anything.',
+    )
+
+################################################################################
+# Filter whcih tests  to collect based on our CLI options and our custom markers
+################################################################################
+
+@pytest.mark.trylast
+def pytest_collection_modifyitems(config, items):
+    test_suite = config.getvalue('test_suite')
+    force_py3 = config.getoption('force_py3')
+    changed_only = config.getoption('changed_only')
+    base_branch = config.getoption('base_branch')
+    dry_run = config.getoption('dry_run')
+
+    is_on_py3 = PY3
+
+    tests_to_run = []
+    tests_to_skip = []
+
+    if changed_only:
+        base_branch = base_branch or get_git_branch()
+        impacted_modules = get_impacted_modules(base_branch) or set()
+        all_is_changed = not(impacted_modules)
+        impacted_modules_paths = ['/{}/'.format(m) for m in impacted_modules]
+        print()
+        if not impacted_modules:
+            print('All modules impacted')
+        else:
+            print('Run tests only in these changed modules:', ', '.join(sorted(impacted_modules)))
+
+    for item in items:
+        is_smoke = bool(item.get_closest_marker('scansmoke'))
+        is_slow = bool(item.get_closest_marker('scanslow'))
+        runs_on_py3 = bool(item.get_closest_marker('scanpy3'))
+
+        if is_slow and test_suite != 'all':
+            tests_to_skip.append(item)
+            continue
+
+        if is_smoke and test_suite not in ('smoke', 'all'):
+            tests_to_skip.append(item)
+            continue
+
+        if is_on_py3 and not (runs_on_py3 or force_py3):
+            tests_to_skip.append(item)
+            continue
+
+        if changed_only and not all_is_changed:
+            if not is_changed(item.fspath, impacted_modules_paths):
+                tests_to_skip.append(item)
+                continue
+
+        tests_to_run.append(item)
+
+    print()
+    print('{} tests selected, {} tests skipped.'.format(len(tests_to_run), len(tests_to_skip)))
+
+    if dry_run:
+        if config.getvalue('verbose'):
+            print()
+            print('The following tests will run: ')
+            for test in tests_to_run:
+                print(test.nodeid)
+
+            print('The following tests are skipped: ')
+            for test in tests_to_skip:
+                print(test.nodeid)
+
+        tests = items[:]
+        items[:] = []
+        config.hook.pytest_deselected(items=tests)
+        return
+
+
+    items[:] = tests_to_run
+    config.hook.pytest_deselected(items=tests_to_skip)
+
+
+################################################################################
+# Retest only tests for changed modules
+################################################################################
+
+
+def is_changed(path_string, impacted_module_paths, _cache={}):
+    """
+    Return True if a `path_string` is for a path that has changed.
+    """
+    path_string = str(path_string).replace('\\', '/')
+    cached = _cache.get(path_string)
+    if cached is not None:
+        return cached
+
+    if path_string.endswith(('setup.py', 'conftest.py')):
+        return False
+    changed = any(p in path_string for p in impacted_module_paths)
+    if TRACE and changed:
+        print('is_changed:', path_string, changed)
+    _cache[path_string] = changed
+    return changed
 
 
 def get_all_modules():
     """
     Return a set of top level modules.
     """
-    return set([p for p in os.listdir('src') if p.endswith('code')])
+    all_modules = set([p for p in os.listdir('src') if p.endswith('code')])
+    if TRACE:
+        print()
+        print('get_all_modules:', all_modules)
+    return all_modules
 
 
-def get_impacted_modules(base_branch='develop'):
+def get_impacted_modules(base_branch=None):
     """
     Return a set of impacted top level modules under tests or src.
     Return None if all modules are impacted and should be re-tested.
     """
     try:
-        current_branch = get_git_branch()
+        base_branch = base_branch or get_git_branch()
         changed_files = get_changed_files(base_branch)
         locally_changed_files = get_changed_files(None)
         changed_files.extend(locally_changed_files)
@@ -53,18 +246,9 @@ def get_impacted_modules(base_branch='develop'):
         print(e)
         return
 
-    if current_branch in ('develop', 'master'):
-        # test it all when merging
-        return
-
     changed_modules = set()
     for changed_file in changed_files:
         segments = [s for s in changed_file.split('/') if s]
-
-        if (len(segments) == 1 or segments[0] == 'etc' 
-        or segments[1] == 'scancode_config.py'):
-            # test it all when changing root or etc/config files
-            return
 
         if segments[0] == 'thirdparty':
             # test it all when changing thirdparty deps
@@ -101,6 +285,9 @@ def get_impacted_modules(base_branch='develop'):
         changed_modules.add('formattedcode')
         changed_modules.add('scancode')
 
+    if TRACE:
+        print()
+        print('get_impacted_modules:', changed_modules)
     return changed_modules
 
 
@@ -118,6 +305,9 @@ def get_changed_files(base_branch='develop'):
     changed_files = changed_files.replace('\\', '/')
     changed_files = changed_files.splitlines(False)
     changed_files = [cf for cf in changed_files if cf.strip()]
+    if TRACE:
+        print()
+        print('get_changed_files:', changed_files)
     return changed_files
 
 
@@ -131,45 +321,7 @@ def get_git_branch():
     branch = check_output(cmd, stderr=STDOUT).splitlines(False)[0]
     _, _, branch = branch.partition('On branch')
     branch = branch.strip()
+    if TRACE:
+        print()
+        print('get_git_branch:', branch)
     return branch
-
-
-################################################################################
-# py test collection configuration
-
-impacted_modules = get_impacted_modules() or set()
-all_modules = get_all_modules() or set()
-ignorable_modules = all_modules.difference(impacted_modules)
-ignorable_module_paths = set(['/{}/'.format(m) for m in ignorable_modules])
-
-base_dir = path.dirname(__file__)
-
-print('====> ALL MODULES      :', ', '.join(sorted(all_modules)))
-print('====> IMPACTED MODULES :', ', '.join(sorted(impacted_modules)))
-print('====> IGNORED MODULES  :', ', '.join(sorted(ignorable_modules)))
-
-
-def pytest_ignore_collect(path, config):
-    """
-    Return True to skip this path for tests collection.
-    This is a pytest hook.
-    """
-    pth = path.strpath
-    skip = should_skip(pth)
-    return skip
-
-
-def should_skip(path_string):
-    """
-    Return True if a `path_string` should be skipped.
-    """
-    path_string = path_string.replace('\\', '/')
-
-    if path_string.endswith('setup.py'):
-        return True
-
-    if not impacted_modules:
-        # everything is impacted and we ignore nothing
-        return False
-    else:
-        return any(p in path_string for p in ignorable_module_paths)
