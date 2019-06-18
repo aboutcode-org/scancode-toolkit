9ae24967451015dcd8c7e22ddb6238b6e66b146f
Philippe Ombredanne <pombredanne@nexb.com>
2019-06-18 18:21:42 +0200

    Ensure commoncode test code is importable #295
    Do not use bytes for paths
    
    Signed-off-by: Philippe Ombredanne <pombredanne@nexb.com>

--- tests/commoncode/commoncode/test_command.py
+++ tests/commoncode/commoncode/test_command.py
@@ -22,21 +22,22 @@
 #  ScanCode is a free software code scanning tool from nexB Inc. and others.
 #  Visit https://github.com/nexB/scancode-toolkit/ for support and download.
 
-from __future__ import absolute_import, print_function
+from __future__ import absolute_import
+from __future__ import print_function
 
 import os
+from unittest.case import skipIf
 
 from commoncode import command
+from commoncode import compat
 from commoncode.testcase import FileBasedTesting
-from unittest.case import skipIf
 from commoncode.system import on_linux
 from commoncode.system import on_mac
 from commoncode.system import on_windows
-from commoncode import compat
 
 
 class TestCommand(FileBasedTesting):
-    test_data_dir = os.path.join(os.path.dirname(__file__), b'data')
+    test_data_dir = os.path.join(os.path.dirname(__file__), 'data')
 
     def test_execute2_non_ascii_output(self):
         # Popen returns a *binary* string with non-ascii chars: skips these
