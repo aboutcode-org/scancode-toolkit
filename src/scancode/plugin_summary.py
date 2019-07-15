29b39d4ae8d5000876a3440bddfb1b803a61912d
Jono Yang <jyang@nexb.com>
2019-07-15 15:05:04 -0700

    Yield fileset when directories have a majority
        * We do not need to check if child directories have majorities since we yield them when we traverse the codebase bottom-up
    
    Signed-off-by: Jono Yang <jyang@nexb.com>

--- src/scancode/plugin_summary.py
+++ src/scancode/plugin_summary.py
@@ -257,7 +257,6 @@ def get_license_exp_holders_filesets(codebase, origin_summary_threshold=None):
             resource.save(codebase)
 
             origin, top_count = origin_count.most_common(1)[0]
-            # TODO: Check for contradictions when performing summarizations
             if is_majority(top_count, resource.files_count, origin_summary_threshold):
                 majority_holders, majority_license_expression = origin
                 resource.origin_summary['license_expression'] = majority_license_expression
@@ -265,19 +264,7 @@ def get_license_exp_holders_filesets(codebase, origin_summary_threshold=None):
                 resource.origin_summary['count'] = top_count
                 resource.save(codebase)
 
-        # If we have a summary for the directory we're in, we check to see if any of our children
-        # have summaries and yield filesets for them
-        # We do this to make sure we report each fileset that has a majority, so it doesn't get
-        # lost when another license expression, holders combination is more dominant later on
-        if resource.origin_summary:
-            for child in children:
-                if child.is_file:
-                    continue
-                # We only care about the children with different expressions and holders from ours
-                if (child.origin_summary['license_expression'] == majority_license_expression
-                        and child.origin_summary['holders'] == majority_holders):
-                    continue
-                fs = create_license_exp_holders_fileset(child, codebase)
+                fs = create_license_exp_holders_fileset(resource, codebase)
                 if fs:
                     yield fs
 
