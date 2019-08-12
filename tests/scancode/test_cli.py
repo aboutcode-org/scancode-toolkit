22e6914b2b394aca1523d1b6f2c1d2735645f8b5
Philippe Ombredanne <pombredanne@nexb.com>
2019-08-12 18:25:51 +0200

    Update tests for matched license text #1668 #260
    They have changed since we do not return highlighting by default
    
    Signed-off-by: Philippe Ombredanne <pombredanne@nexb.com>

--- tests/scancode/test_cli.py
+++ tests/scancode/test_cli.py
@@ -460,7 +460,7 @@ def test_scan_can_return_matched_license_text():
     result_file = test_env.get_temp_file('json')
     args = ['--license', '--license-text', '--strip-root', test_file, '--json', result_file]
     run_scan_click(args)
-    check_json_scan(test_env.get_test_loc(expected_file), result_file)
+    check_json_scan(test_env.get_test_loc(expected_file), result_file, regen=False)
 
 
 @skipIf(on_windows, 'This test cannot run on windows as these are not legal file names.')
