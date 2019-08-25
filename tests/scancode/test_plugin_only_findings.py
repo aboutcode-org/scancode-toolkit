a0aea49c9d3a6fa1be05da832c596cc640a9f060
Abhishek Kumar <abhishek.kasyap09@gmail.com>
2019-08-26 02:35:23 +0530

    Port remainder part of scancode to Python 3 #295
    Signed-off-by: Abhishek Kumar <abhishek.kasyap09@gmail.com>

--- tests/scancode/test_plugin_only_findings.py
+++ tests/scancode/test_plugin_only_findings.py
@@ -32,6 +32,9 @@ from commoncode.testcase import FileDrivenTesting
 from scancode.cli_test_utils import run_scan_click
 from scancode.cli_test_utils import check_json_scan
 
+import pytest
+pytestmark = pytest.mark.scanpy3  # NOQA
+
 
 class TestHasFindings(FileDrivenTesting):
 
