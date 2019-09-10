df93eb9caf811ebda12dc384a1c11ca5a53a0e36
Philippe Ombredanne <pombredanne@nexb.com>
2019-09-10 08:54:36 +0200

    Use structured object for rule data
    Signed-off-by: Philippe Ombredanne <pombredanne@nexb.com>

--- etc/scripts/buildrules.py
+++ etc/scripts/buildrules.py
@@ -30,6 +30,7 @@ from __future__ import print_function
 import io
 import os
 
+import attr
 import click
 click.disable_unicode_literals_warning = True
 from license_expression import Licensing
@@ -38,6 +39,27 @@ import saneyaml
 from licensedcode import cache
 from licensedcode import models
 from licensedcode import match_hash
+
+
+@attr.attrs(slots=True)
+class LicenseRule(object):
+    data = attr.ib()
+    text = attr.ib()
+    raw_data = attr.ib(default=None)
+
+    def __attrs_post_init__(self, *args, **kwargs):
+        self.raw_data = rdat = '\n'.join(self.data).strip()
+        self.text = '\n'.join(self.text).strip()
+
+        # validate YAML syntax
+        try:
+            self.data = saneyaml.load(rdat)
+        except:
+            print('########################################################')
+            print('Invalid YAML:')
+            print(rdat)
+            print('########################################################')
+            raise
 
 
 def load_data(location='00-new-licenses.txt'):
@@ -61,7 +83,7 @@ def load_data(location='00-new-licenses.txt'):
             in_data = True
             in_text = True
             if data and ''.join(text).strip():
-                rules.append((data, text))
+                rules.append(LicenseRule(data, text))
             data = []
             text = []
             continue
@@ -88,12 +110,15 @@ def load_data(location='00-new-licenses.txt'):
 
 def rule_exists(text):
     """
-    Return the matched rule if the text is an existing rule matched exactly, False otherwise.
+    Return the matched rule identifier if the text is an existing rule matched
+    exactly, False otherwise.
     """
     idx = cache.get_index()
 
     matches = idx.match(query_string=text)
-    if len(matches) != 1:
+    if not matches:
+        return False
+    if len(matches) > 1:
         return False
     match = matches[0]
     if match.matcher == match_hash.MATCH_HASH:
@@ -105,7 +130,13 @@ def find_rule_base_loc(license_expression):
     Return a new, unique and non-existing base name location suitable to create a new
     rule.
     """
-    template = license_expression.lower().strip().replace(' ', '_').replace('(', '').replace(')', '') + '_{}'
+    template = (license_expression
+        .lower()
+        .strip()
+        .replace(' ', '_')
+        .replace('(', '')
+        .replace(')', '')
+        +'_{}')
     idx = 1
     while True:
         base_name = template.format(idx)
@@ -134,38 +165,25 @@ def cli(licenses_file):
         ----------------------------------------
     """
 
-    rule_data = load_data(licenses_file)
+    rules_data = load_data(licenses_file)
     rules_tokens = set()
 
     licenses = cache.get_licenses_db()
     licensing = Licensing(licenses.values())
 
     print()
-    for data, text in rule_data:
-        rdat = '\n'.join(data)
-        rtxt = '\n'.join(text)
-        existing = rule_exists(rtxt)
+    for rule in rules_data:
+        existing = rule_exists(rule.text)
         if existing:
-            print('Skipping existing rule:', existing, 'with text:\n', rtxt[:50].strip(), '...')
+            print('Skipping existing rule:', existing, 'with text:\n', rule.text[:50].strip(), '...')
             continue
 
-        # validate YAML syntax
-        try:
-            parsed = saneyaml.load(rdat)
-        except:
-            print('########################################################')
-            print('Invalid YAML:')
-            print(rdat)
-            print('########################################################')
-            raise
-
-        if parsed.get('is_negative'):
+        if rule.data.get('is_negative'):
             base_name = 'not-a-license'
         else:
-            _, _, license_expression = data[0].partition(': ')
-            license_expression = license_expression.strip()
+            license_expression = rule.data.get('license_expression')
             if not license_expression:
-                raise Exception('Missing license_expression for text:', rtxt)
+                raise Exception('Missing license_expression for text:', rule)
             licensing.parse(license_expression, validate=True, simple=True)
             base_name = license_expression
 
@@ -173,11 +191,12 @@ def cli(licenses_file):
 
         data_file = base_loc + '.yml'
         with io.open(data_file, 'w', encoding='utf-8') as o:
-            o.write(rdat)
+            o.write(rule.raw_data)
 
         text_file = base_loc + '.RULE'
         with io.open(text_file, 'w', encoding='utf-8') as o:
-            o.write(rtxt)
+            o.write(rule.text)
+
         rule = models.Rule(data_file=data_file, text_file=text_file)
         rule_tokens = tuple(rule.tokens())
         if rule_tokens in rules_tokens:
