eaf4c26cfe0de578e3f1f59347afa7edc27089f6
Philippe Ombredanne <pombredanne@nexb.com>
2019-10-02 11:13:42 +0200

    Add new license and improved license rules
    Signed-off-by: Philippe Ombredanne <pombredanne@nexb.com>

---
+++ tests/licensedcode/data/licenses/tb.js
@@ -0,0 +1,7 @@
+/*
+ * Thickbox 3.1 - One Box To Rule Them All.
+ * By Cody Lindley (http://www.codylindley.com)
+ * Copyright (c) 2007 cody lindley
+ * Licensed under the MIT License: http://www.opensource.org/licenses/mit-license.php
+*/
+
