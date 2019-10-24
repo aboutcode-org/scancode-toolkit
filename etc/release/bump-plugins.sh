6b0267fa8df1bd44abc40b6f83cd77649cecdcc7
Philippe Ombredanne <pombredanne@nexb.com>
2019-10-24 10:28:01 +0200

    Add new script to bumpd builtin plugins versions
    Signed-off-by: Philippe Ombredanne <pombredanne@nexb.com>

---
+++ etc/release/bump-plugins.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+#
+# Copyright (c) nexB Inc. http://www.nexb.com/ - All rights reserved.
+#
+
+# ScanCode release script
+# This script uses bumpversion to update the version of plugins in the plugins-builtin/ directory
+
+set -e
+
+# un-comment to trace execution
+# set -x
+
+
+for i in `ls plugins-builtin`
+  do 
+    pushd plugins-builtin/$i
+    bumpversion patch
+    popd
+  done
