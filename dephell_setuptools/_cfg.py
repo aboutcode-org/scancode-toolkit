2a21119b43287c47eb30ca38b224915aa4bdc643
Gram <master_fess@mail.ru>
2019-11-11 17:40:09 +0100

    +cfg reader

---
+++ dephell_setuptools/_cfg.py
@@ -0,0 +1,57 @@
+from configparser import ConfigParser
+from pathlib import Path
+from typing import Dict, List, Optional, Union
+
+
+class CfgReader:
+    def __init__(self, path: Union[str, Path]):
+        if isinstance(path, str):
+            path = Path(path)
+        self.path = path
+
+    @property
+    def content(self) -> Optional[Dict[str, Union[List, Dict]]]:
+        parser = ConfigParser()
+        parser.read(str(self.path))
+
+        name = None
+        version = None
+        if parser.has_option('metadata', 'name'):
+            name = parser.get('metadata', 'name')
+
+        if parser.has_option('metadata', 'version'):
+            version = parser.get('metadata', 'version')
+
+        install_requires = []
+        extras_require = {}
+        python_requires = None
+        if parser.has_section('options'):
+            if parser.has_option('options', 'install_requires'):
+                for dep in parser.get('options', 'install_requires').split('\n'):
+                    dep = dep.strip()
+                    if not dep:
+                        continue
+
+                    install_requires.append(dep)
+
+            if parser.has_option('options', 'python_requires'):
+                python_requires = parser.get('options', 'python_requires')
+
+        if parser.has_section('options.extras_require'):
+            for group in parser.options('options.extras_require'):
+                extras_require[group] = []
+                deps = parser.get('options.extras_require', group)
+                for dep in deps.split('\n'):
+                    dep = dep.strip()
+                    if not dep:
+                        continue
+
+                    extras_require[group].append(dep)
+
+        return dict(
+            name=name,
+            version=version,
+            install_requires=install_requires,
+            extras_require=extras_require,
+            python_requires=python_requires,
+        )
---
+++ tests/test_cfg.py
@@ -0,0 +1,11 @@
+from pathlib import Path
+
+from dephell_setuptools._cfg import CfgReader
+
+
+def test_cfg():
+    path = Path(__file__).parent / 'setups' / 'setup.cfg'
+    actual = CfgReader(path).content
+    assert actual['name'] == 'with-setup-cfg'
+    assert actual['install_requires'] == ['six', 'tomlkit']
+    assert actual['version'] == '1.2.3'
