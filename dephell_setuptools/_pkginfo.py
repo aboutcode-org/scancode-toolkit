de4257a06f86e583163436ac1be5e1206aaddbe6
Gram <master_fess@mail.ru>
2019-11-14 14:46:00 +0100

    check type annotations

--- dephell_setuptools/_pkginfo.py
+++ dephell_setuptools/_pkginfo.py
@@ -1,20 +1,22 @@
 # built-in
 import json
 import subprocess
+from typing import Any, Dict
 
 # app
 from ._base import BaseReader
+from ._cached_property import cached_property
 
 
 class PkgInfoReader(BaseReader):
-    @property
-    def content(self):
+    @cached_property
+    def content(self) -> Dict[str, Any]:
         if self.path.is_file() and self.path.name != 'setup.py':
-            return None
+            raise NameError('cannot parse non setup.py named files')
 
         cmd = ['pkginfo', '--json', str(self.path)]
         result = subprocess.run(cmd, stderr=subprocess.PIPE, stdout=subprocess.PIPE)
         if result.returncode != 0:
-            return None
+            raise RuntimeError(result.stderr.decode().split('\n')[-1])
         content = json.loads(result.stdout.decode())
         return self._clean(content)
