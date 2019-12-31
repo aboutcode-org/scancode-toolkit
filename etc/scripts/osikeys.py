b1bbe33d3e5f72ade565ea4fdc28bdfff30923dd
Gaupeng <gaurang.r.rao@gmail.com>
2019-12-31 13:34:48 +0530

    Fix Camel Case & Comments
    Signed-off-by: Gaupeng <gaurang.r.rao@gmail.com>

--- etc/scripts/osikeys.py
+++ etc/scripts/osikeys.py
@@ -6,39 +6,43 @@ from licensedcode.models import load_licenses
 from licensedcode.models import Rule
 from licensedcode.models import rules_data_dir
 
-def findOSIMap(license):
+
+def find_osi_map(license: "OSI License Object"):
     """
     Return Scancode key mapped to OSI License `license`
     """
     idx = get_index()
-    osiKey = license['id']
-    matches = list(idx.match(query_string = osiKey))
+    osi_key = license['id']
+    matches = list(idx.match(query_string = osi_key))
     if not matches:
         return None
-    return matches[0].rule.license_expression, osiKey
+    return matches[0].rule.license_expression
 
-def addOSIKey(obj):
+
+def add_osi_key(obj: "OSI License Object"):
     """
-    Add OSI License Key to licenses found to have a Scancode key matching an OSI key
+    Add OSI License Key derived from OSI License Object `obj`
     """
     licenses = licensedcode.cache.get_licenses_db()
-    result, osiKey = findOSIMap(obj)
+    result = find_osi_map(obj)
+    osi_key = obj['id']
     if not result:
         return None
-    mappedLicense = licenses[result] # license object from Scancode 
+    mapped_license = licenses[result] # license object from Scancode 
     # modify this license to have an OSI Key
-    # mappedLicense.osi_license_key = osiKey
+    # mapped_icense.osi_license_key = osi_key
     # throws attribute error
     
-def addOSIKeys():
+
+def add_osi_keys():
     """
     For every possible OSI License, match and add OSI Key to exiting Scancode License
     """
     contents = urllib.request.urlopen("https://api.opensource.org/licenses/").read().decode()
     data = json.loads(contents)
     for obj in data:
-        addOSIKey(obj)
+        add_osi_key(obj)
     
 
 if __name__ == '__main__':
-    addOSIKeys()
+    add_osi_keys()
