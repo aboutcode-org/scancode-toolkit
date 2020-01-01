af120625f579bea171d87a6992ec8497254c9131
Gaupeng <gaurang.r.rao@gmail.com>
2020-01-01 13:55:54 +0530

    Add Comments
    Signed-off-by: Gaupeng <gaurang.r.rao@gmail.com>

--- etc/scripts/osikeys.py
+++ etc/scripts/osikeys.py
@@ -3,8 +3,6 @@ import licensedcode.index
 import json
 from licensedcode.cache import get_index
 from licensedcode.models import load_licenses
-from licensedcode.models import Rule
-from licensedcode.models import rules_data_dir
 
 
 def find_osi_map(license: "OSI License Object"):
@@ -13,6 +11,7 @@ def find_osi_map(license: "OSI License Object"):
     """
     idx = get_index()
     osi_key = license['id']
+    # search for Scancode License matching OSI Key
     matches = list(idx.match(query_string = osi_key))
     if not matches:
         return None
@@ -24,22 +23,26 @@ def add_osi_key(obj: "OSI License Object"):
     Add OSI License Key derived from OSI License Object `obj`
     """
     licenses = licensedcode.cache.get_licenses_db()
-    result = find_osi_map(obj)
-    osi_key = obj['id']
-    if not result:
-        return None
+    # find matching Scancode License Object for `obj`
+    result = find_osi_map(obj) 
+    # derive OSI License Key from `obj`
+    osi_key = obj['id'] 
+    if not result: # no matching Scancode License found
+        return None 
     mapped_license = licenses[result] # license object from Scancode
-    print(mapped_license.key)
-    licenses_path = "src/licensedcode/data/licenses/"
+    licenses_path = "src/licensedcode/data/licenses/" # licenses directory
     file_to_modify = open(licenses_path + mapped_license.key + ".yml", "a")
     file_to_modify.write("osi_license_key: " + osi_key)
 
+
 def add_osi_keys():
     """
-    For every possible OSI License, match and add OSI Key to exiting Scancode License
+    For every possible OSI License, match and add OSI Key to existing Scancode License
     """
+    #retrieve OSI License Objects
     contents = urllib.request.urlopen("https://api.opensource.org/licenses/").read().decode()
     data = json.loads(contents)
+    # for each OSI License Object, update matching Scancode License Object
     for obj in data:
         add_osi_key(obj)
     
