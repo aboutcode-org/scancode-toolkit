50471ecda9939da4119aa37252d1d9c661126b64
arthur657834 <kingkom7834@126.com>
2020-03-04 10:41:49 +0800

    Update test_rpm.py
    chang from packagedcode.pyrpm import RPM to from packagedcode.pyrpm import rpm at line 97

--- tests/packagedcode/test_rpm.py
+++ tests/packagedcode/test_rpm.py
@@ -94,7 +94,7 @@ class TestRpmBasics(FileBasedTesting):
 
     def test_pyrpm_basic(self):
         test_file = self.get_test_loc('rpm/header/python-glc-0.7.1-1.src.rpm')
-        from packagedcode.pyrpm import RPM
+        from packagedcode.pyrpm import rpm
         raw_rpm = RPM(open(test_file, 'rb'))
         alltags = raw_rpm.get_tags()
         expected = {
