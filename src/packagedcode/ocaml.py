32fc378b0f13504bc4911e3cf5a89ab7c791afa5
rpotter12 <rohitpotter12@gmail.com>
2020-03-20 20:45:15 +0530

    adds spaces between functions
    Signed-off-by: rpotter12 <rohitpotter12@gmail.com>

--- src/packagedcode/ocaml.py
+++ src/packagedcode/ocaml.py
@@ -115,17 +115,23 @@ def load(file_name):
     file_data = []
     with io.open(file_name, "r", encoding="utf-8") as f:
         file_data = [line.rstrip('\n') for line in f]
-    return file_data        
+    return file_data      
+
+
 def getversion(the_list):
     for individual in the_list:
         if 'opam-version' in individual:
             version=individual.split('"')
             return version[1]
+
+
 def getmaintainer(the_list):
     for individual in the_list:
         if 'maintainer' in individual:
             version=individual.split('"')
             return version[1]
+
+            
 def getsynopsis(the_list):
     for individual in the_list:
         if 'synopsis' in individual:
