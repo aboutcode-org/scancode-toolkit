9fc4f00923cd5a6f19620cfa199d35b9b4317ba1
rpotter12 <rohitpotter12@gmail.com>
2020-07-06 08:39:02 +0530

    tests for sample go.mod file
    Signed-off-by: rpotter12 <rohitpotter12@gmail.com>

---
+++ tests/packagedcode/test_golang.py
@@ -0,0 +1,68 @@
+
+# Copyright (c) 2019 nexB Inc. and others. All rights reserved.
+# http://nexb.com and https://github.com/nexB/scancode-toolkit/
+# The ScanCode software is licensed under the Apache License version 2.0.
+# Data generated with ScanCode require an acknowledgment.
+# ScanCode is a trademark of nexB Inc.
+#
+# You may not use this software except in compliance with the License.
+# You may obtain a copy of the License at: http://apache.org/licenses/LICENSE-2.0
+# Unless required by applicable law or agreed to in writing, software distributed
+# under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
+# CONDITIONS OF ANY KIND, either express or implied. See the License for the
+# specific language governing permissions and limitations under the License.
+#
+# When you publish or redistribute any data created with ScanCode or any ScanCode
+# derivative work, you must accompany this data with the following acknowledgment:
+#
+#  Generated with ScanCode and provided on an "AS IS" BASIS, WITHOUT WARRANTIES
+#  OR CONDITIONS OF ANY KIND, either express or implied. No content created from
+#  ScanCode should be considered or used as legal advice. Consult an Attorney
+#  for any legal advice.
+#  ScanCode is a free software code scanning tool from nexB Inc. and others.
+#  Visit https://github.com/nexB/scancode-toolkit/ for support and download.
+
+from __future__ import absolute_import
+from __future__ import print_function
+from __future__ import unicode_literals
+
+import os
+import pytest
+
+from packagedcode import golang
+
+from packages_test_utils import PackageTester
+
+
+class TestGolang(PackageTester):
+    test_data_dir = os.path.join(os.path.dirname(__file__), 'data')
+
+    def test_parse_kingpin(self):
+        test_file = self.get_test_loc('go/kingpin/go.mod')
+        expected_loc = self.get_test_loc('go/kingpin/output.expected.json')
+        package = golang.GolangPackage.recognize(test_file)
+        self.check_packages(package, expected_loc, regen=False)
+
+    def test_parse_participle(self):
+        test_file = self.get_test_loc('go/participle/go.mod')
+        expected_loc = self.get_test_loc('go/participle/output.expected.json')
+        package = golang.GolangPackage.recognize(test_file)
+        self.check_packages(package, expected_loc, regen=False)
+
+    def test_parse_sample(self):
+        test_file = self.get_test_loc('go/sample/go.mod')
+        expected_loc = self.get_test_loc('go/sample/output.expected.json')
+        package = golang.GolangPackage.recognize(test_file)
+        self.check_packages(package, expected_loc, regen=False)
+
+    def test_parse_uap_go(self):
+        test_file = self.get_test_loc('go/uap-go/go.mod')
+        expected_loc = self.get_test_loc('go/uap-go/output.expected.json')
+        package = golang.GolangPackage.recognize(test_file)
+        self.check_packages(package, expected_loc, regen=False)
+
+    def test_parse_user_agent(self):
+        test_file = self.get_test_loc('go/user_agent/go.mod')
+        expected_loc = self.get_test_loc('go/user_agent/output.expected.json')
+        package = golang.GolangPackage.recognize(test_file)
+        self.check_packages(package, expected_loc, regen=False)
