1ae7ccf31ab26f1933fd5ae5c538996de78eaf6f
Mayur Agarwal <mr.mayurrrr@gmail.com>
2020-07-23 12:10:07 +0530

    Update pubspec.py
    Signed-off-by: Mayur Agarwal <mr.mayurrrr@gmail.com>

--- src/packagedcode/pubspec.py
+++ src/packagedcode/pubspec.py
@@ -1,3 +1,35 @@
+#
+# Copyright (c) 2019 nexB Inc. and others. All rights reserved.
+# http://nexb.com and https://github.com/nexB/scancode-toolkit/
+# The ScanCode software is licensed under the Apache License version 2.0.
+# Data generated with ScanCode require an acknowledgment.
+# ScanCode is a trademark of nexB Inc.
+#
+# You may not use this software except in compliance with the License.
+# You may obtain a copy of the License at: http://apache.org/licenses/LICENSE-2.0
+# Unless required by applicable law or agreed to in writing, software distributed
+# under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
+# CONDITIONS OF ANY KIND, either express or implied. See the License for the
+# specific language governing permissions and limitations under the License.
+#
+# When you publish or redistribute any data created with ScanCode or any ScanCode
+# derivative work, you must accompany this data with the following acknowledgment:
+#
+#  Generated with ScanCode and provided on an "AS IS" BASIS, WITHOUT WARRANTIES
+#  OR CONDITIONS OF ANY KIND, either express or implied. No content created from
+#  ScanCode should be considered or used as legal advice. Consult an Attorney
+#  for any legal advice.
+#  ScanCode is a free software code scanning tool from nexB Inc. and others.
+#  Visit https://github.com/nexB/scancode-toolkit/ for support and download.
+
+from __future__ import absolute_import
+from __future__ import print_function
+from __future__ import unicode_literals
+
+import io
+import logging
+import sys
+
 import yaml
 import attr
 from packageurl import PackageUrl
@@ -6,12 +38,25 @@ from packagedcode import models
 
 from packagedcode import models
 
+TRACE = False
+
+def logger_debug(*args):
+    pass
+
+logger = logginf.getLogger(__name__)
+
+if TRACE:
+    logging.basicConfig(stream = sys.stdout)
+    logger.setLevel(logging.DEBUG)
+    
+    def logger_debug(*args):
+        return logger.debug(''.join(isinstance(a, string_types) and a or repr(a) for a in args))
+
 @attr.s()
 class PubspecPackage(models.Package):
-    metafiles = ('*.yaml',)
+    metafiles = ('pubspec.yaml',)
     extensions = ('.yaml',)
-    """ default_type = ''
-    """
+    default_type = 'pubspec'
     default_primary_language = 'dart'
     default_web_baseurl = 'https://dart.dev/tools/pub/pubspec
     default_download_baseurl = 'https://dart.dev/tools/pub/pubspec'
