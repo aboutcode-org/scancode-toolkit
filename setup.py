a2910a9d236e13b55f15eda5fcefa276a15f5d4d
Philippe Ombredanne <pombredanne@nexb.com>
2020-09-04 19:16:25 +0200

    Add workaround for #2201
    Do not use version 20.1.0 and add tests.
    
    Signed-off-by: Philippe Ombredanne <pombredanne@nexb.com>

--- setup.py
+++ setup.py
@@ -200,7 +200,7 @@ setup(
         'click >= 6.0.0, < 7.0.0',
         'colorama >= 0.3.9',
         'pluggy >= 0.4.0, < 1.0',
-        'attrs >=18.1',
+        'attrs >=18.1, !=20.1.0',
         'typing >=3.6, < 3.7',
 
         # scancode outputs
