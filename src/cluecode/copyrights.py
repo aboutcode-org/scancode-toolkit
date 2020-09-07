e5da170e4de8d4678d1715e36bed2e1a79c65657
Philippe Ombredanne <pombredanne@nexb.com>
2020-09-07 08:46:20 +0200

    Improve author and copyright detection
    Signed-off-by: Philippe Ombredanne <pombredanne@nexb.com>

--- src/cluecode/copyrights.py
+++ src/cluecode/copyrights.py
@@ -505,7 +505,8 @@ patterns = [
     (r'^[Rr]eleased?$', 'JUNK'),
     (r'^[Cc]opyrighting$', 'JUNK'),
     (r'^Authori.*$', 'JUNK'),
-
+    (r'^such$', 'JUNK'),
+    (r'^[Aa]ssignments?[.,]?$', 'JUNK'),
     (r'^[Bb]uild$', 'JUNK'),
     (r'^[Ss]tring$', 'JUNK'),
     (r'^Implementation-Vendor$', 'JUNK'),
@@ -744,6 +745,8 @@ patterns = [
     (r'^GA$', 'JUNK'),
     (r'^unzip$', 'JUNK'),
     (r'^EULA', 'JUNK'),
+    (r'^Terms?[.,]?$', 'JUNK'),
+    (r'^Non-Assertion$', 'JUNK'),
 
     # this is not Copr.
     (r'^Coproduct,?[,\.]?$$', 'JUNK'),
@@ -852,6 +855,7 @@ patterns = [
     (r'^GPL\'d', 'NN'),
     (r'^Gnome$', 'NN'),
     (r'^GnuPG$', 'NN'),
+    (r'^Government.', 'NNP'),
     (r'^Government', 'NN'),
     (r'^Grants?\.?,?$', 'NN'),
     (r'^Header', 'NN'),
@@ -952,6 +956,7 @@ patterns = [
     (r'^Section', 'NN'),
     (r'^Send$', 'NN'),
     (r'^Separa', 'NN'),
+    (r'^Service$', 'NN'),
     (r'^Several$', 'NN'),
     (r'^SIGN$', 'NN'),
     (r'^Site\.?$', 'NN'),
@@ -998,6 +1003,7 @@ patterns = [
     (r'^WPA$', 'NN'),
     (r'^Xalan$', 'NN'),
     (r'^YOUR', 'NN'),
+    (r'^Your', 'NN'),
     (r'^DateTime', 'NN'),
     (r'^Create$', 'NN'),
     (r'^Engine\.$', 'NN'),
@@ -1098,9 +1104,14 @@ patterns = [
     (r'werken$', 'NNP'),
     (r'various\.?$', 'NNP'),
 
+    # treat Attributable as proper noun as it is seen in Author tags such as in:
+    # @author not attributable
+    (r'^[Aa]ttributable$', 'NNP'),
+
     # rarer caps
     # EPFL-LRC/ICA
     (r'^[A-Z]{3,6}-[A-Z]{3,6}/[A-Z]{3,6}', 'NNP'),
+
 
     ############################################################################
     # Named entities: companies, groups, universities, etc
@@ -1227,6 +1238,9 @@ patterns = [
 
     (r'^[Cc]ontribut(ors|ing)\.?$', 'CONTRIBUTORS'),
     (r'^contributors,$', 'CONTRIBUTORS'),
+
+    (r'^Contributor[,.]?$', 'NN'),
+    (r'^Licensor[,.]?$', 'NN'),
 
     # same for developed, etc...
     (r'^[Cc]oded$', 'AUTH2'),
@@ -2055,6 +2069,7 @@ grammar = """
     # Copyright 2008 TJ <linux@tjworld.net>
     COPYRIGHT: {<COPYRIGHT2> <EMAIL>}       #2636
 
+    # Copyright RUSS DILL Russ <Russ.Dill@asu.edu>
     COPYRIGHT: {<COPYRIGHT> <CAPS> <NAME-EMAIL>}       #2637
 
     # maintainer Norbert Tretkowski <nobse@debian.org> 2005-04-16
@@ -2156,7 +2171,7 @@ grammar = """
     COPYRIGHT: {<COMPANY> <ALLRIGHTRESERVED> <COPYRIGHT2>} #3030
 
     # Copyright (c) 2000 United States Government as represented by the Secretary of the Navy. All rights reserved.
-    COPYRIGHT: {<COPYRIGHT> <NN> <NN> <NN> <BY> <NN> <NAME> <ALLRIGHTRESERVED>} #3035
+    COPYRIGHT: {<COPYRIGHT> <NN> <NN> <NN|NNP> <BY> <NN> <NAME> <ALLRIGHTRESERVED>} #3035
 
     # Copyright (c) 2007-2008, Y Giridhar Appaji Nag <giridhar@appaji.net>
     COPYRIGHT: {<COPYRIGHT> <COMPANY|NAME|NAME-EMAIL|NAME-YEAR>+} #3040
@@ -2228,6 +2243,9 @@ grammar = """
     # Copyright (C) 2005-2006  dann frazier <dannf@dannf.org>
     COPYRIGHT: {<COPYRIGHT2>  <NN>  <NN>  <EMAIL>} #999991
 
+    # Copyright (c) 2008 Intel Corporation / Qualcomm Inc.
+    COPYRIGHT: {<COPYRIGHT>  <DASH>  <COMPANY>} #copydash-co
+
 #######################################
 # Authors
 #######################################
@@ -2271,13 +2289,22 @@ grammar = """
     # developed by the XML DB Initiative http//www.xmldb.org
     AUTHOR: {<AUTH2> <COMPANY>} #2645-7
 
+    # Author not attributable
+    AUTHOR: {<AUTH>  <NN>  <NNP>} #not attributable
+
+    # author (Panagiotis Tsirigotis)
+    AUTHOR: {<AUTH>  <NNP><NNP>+} #author Foo Bar
+
+
 #######################################
-# Mixed AUTHORS and COPYRIGHTS
+# Mixed AUTHOR and COPYRIGHT
 #######################################
 
     # Compounded statements usings authors
-    # found in some rare cases with a long list of authors.
-    COPYRIGHT: {<COPY> <BY> <AUTHOR>+ <YR-RANGE>*}        #2800
+
+    # Copyright by Daniel K. Gebhart
+    # Also found in some rare cases with a long list of authors.
+    COPYRIGHT: {<COPY> <BY>? <AUTHOR>+  <YR-RANGE>*}        #2800-1
 
     COPYRIGHT: {<AUTHOR> <COPYRIGHT2>}        #2820
     COPYRIGHT: {<AUTHOR> <YR-RANGE>}        #2830
@@ -2331,6 +2358,7 @@ def refine_copyright(c):
     c = strip_balanced_edge_parens(c)
     c = strip_suffixes(c, suffixes=COPYRIGHTS_SUFFIXES)
     c = strip_trailing_period(c)
+    c = c.strip("'")
     return c.strip()
 
 
@@ -2353,8 +2381,6 @@ def refine_holder(h):
     h = h.strip()
     h = strip_trailing_period(h)
     h = h.strip()
-    h = strip_balanced_edge_parens(h)
-    h = h.strip()
     if h and h.lower() not in HOLDERS_JUNK:
         return h
 
@@ -2370,6 +2396,10 @@ def refine_author(a):
     a = a.strip()
     a = strip_trailing_period(a)
     a = a.strip()
+    a = strip_balanced_edge_parens(a)
+    a = a.strip()
+    a = refine_names(a, prefixes=AUTHORS_PREFIXES)
+    a = a.strip()
     if a and a.lower() not in AUTHORS_JUNK:
         return a
 
@@ -2383,6 +2413,8 @@ def refine_names(s, prefixes):
     s = strip_leading_numbers(s)
     s = strip_all_unbalanced_parens(s)
     s = strip_some_punct(s)
+    s = s.strip()
+    s = strip_balanced_edge_parens(s)
     s = s.strip()
     s = strip_prefixes(s, prefixes)
     s = s.strip()
@@ -2562,6 +2594,7 @@ AUTHORS_PREFIXES = frozenset(set.union(
         'author\'',
         'authors,',
         'authorship',
+        'or',
     ])
 ))
 
@@ -2580,6 +2613,11 @@ AUTHORS_JUNK = frozenset([
     'company',
     'contributing project',
     'its author',
+    'gnomovision',
+    'would',
+    'may',
+    'attributions',
+    'the',
 ])
 
 ################################################################################
