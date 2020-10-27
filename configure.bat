847564ead159cfcebe0f04066daa6f23e1a5a123
Jono Yang <jyang@nexb.com>
2020-10-27 10:40:38 -0700

    Fix path to Scripts directory
        * Remove unused variables and options
    
    Signed-off-by: Jono Yang <jyang@nexb.com>

--- configure.bat
+++ configure.bat
@@ -13,41 +13,12 @@
 @rem #    the path to the Python executable to use. If < path to python.exe> is
 @rem #    set to "path", then the executable will be the python.exe available
 @rem #    in the PATH.
-@rem #
-@rem #  <some conf path> : this must be the last argument and sets the path to a
-@rem #    configuration directory to use.
 @rem ################################
-
-@rem ################################
-@rem # Defaults. Change these variables to customize this script locally
-@rem ################################
-@rem # you can define one or more thirdparty dirs, each where the varibale name
-@rem # is prefixed with TPP_DIR
-set "TPP_DIR=thirdparty"
-
-@rem # default configurations for dev
-set "CONF_DEFAULT=etc/conf/dev"
-
-@rem # default thirdparty dist for dev
-if ""%CONF_DEFAULT%""==""etc/conf/dev"" (
-    set "TPP_DIR_DEV=thirdparty/dev"
-)
-
-@rem # default supported version for Python 3
-set SUPPORTED_PYTHON3=3.6
-
-@rem #################################
-
-@rem python --version
-@rem python -c "import sys;print(sys.executable)"
-
 
 @rem Current directory where this .bat files lives
 set CFG_ROOT_DIR=%~dp0
-
 @rem path where a configured Python should live in the current virtualenv if installed
-set CONFIGURED_PYTHON=%CFG_ROOT_DIR%Scripts\python.exe
-
+set CONFIGURED_PYTHON=%CFG_ROOT_DIR%tmp\Scripts\python.exe
 set PYTHON_EXECUTABLE=
 
 @rem parse command line options and arguments
@@ -55,13 +26,6 @@ set PYTHON_EXECUTABLE=
 if "%1" EQU "--help" (goto cli_help)
 if "%1" EQU "--clean" (call rmdir /s /q "%CFG_ROOT_DIR%tmp") && call exit /b
 if "%1" EQU "--python" (set PROVIDED_PYTHON=%~2) && shift && shift && goto collectopts
-
-@rem We are not cleaning: Either we have a provided configure config path or we use a default.
-if ""%1""=="""" (
-    set CFG_CMD_LINE_ARGS=%CONF_DEFAULT%
-) else (
-    set CFG_CMD_LINE_ARGS=%1
-)
 
 @rem If we have a pre-configured Python in our virtualenv, reuse this as-is and run
 if exist ""%CONFIGURED_PYTHON%"" (
@@ -83,7 +47,6 @@ if exist ""%PROVIDED_PYTHON%"" (
 
 @rem otherwise we search for a suitable Python interpreter
 :find_python
-
 @rem First check the existence of the "py" launcher (available in Python 3)
 @rem if we have it, check if we have a py -3 installed with the good version or a py 2.7
 @rem if not, check if we have an old py 2.7
@@ -116,8 +79,8 @@ if %ERRORLEVEL% == 0 (
     )
 )
 
-:run
 
+:run
 @rem without this things may not always work on Windows 10, but this makes things slower
 set PYTHONDONTWRITEBYTECODE=1
 
@@ -141,9 +104,6 @@ echo A configuration script for Windows
 echo usage: configure [options] [path/to/config/directory]
 echo.
 echo The options and arguments are:
-echo  [path/to/config/directory] : this optionally sets the path to a
-echo    configuration directory to use. Defaults to etc/conf/dev if not set
-echo.
 echo  --clean : this is exclusive of anything else and cleans the environment
 echo    from built and installed files
 echo.
@@ -155,6 +115,6 @@ echo.
 
 :activate
 @rem Activate the virtualenv
-if exist "%CFG_ROOT_DIR%Scripts\activate" (
-    "%CFG_ROOT_DIR%Scripts\activate"
+if exist "%CFG_ROOT_DIR%tmp\Scripts\activate" (
+    "%CFG_ROOT_DIR%tmp\Scripts\activate"
 )
