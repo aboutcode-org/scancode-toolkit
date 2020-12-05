bdf8d6486856f08d323019c38c308adb463aeb25
Philippe Ombredanne <pombredanne@nexb.com>
2020-12-05 17:33:13 +0100

    Reapir code after botched merge
    Signed-off-by: Philippe Ombredanne <pombredanne@nexb.com>

--- src/licensedcode/legal.py
+++ src/licensedcode/legal.py
@@ -29,66 +29,64 @@ from commoncode import fileutils
 Recognition of typical "legal" files such as "LICENSE", "COPYING", etc.
 """
 
-special_names    'COPYING',
-    'COPYLEFT',
+special_names = (
+    'COPYING',
     'COPYRIGHT',
+    'COPYRIGHTS',
     'NOTICE',
+    'NOTICES',
     'LICENSE',
     'LICENCE',
     'LICENSES',
     'LICENCES',
     'LICENSING',
     'LICENCING',
-    'UNLICENSE',
+    'COPYLEFT',
     'LEGAL',
     'EULA',
     'AGREEMENT',
+    'AGREEMENTS',
     'ABOUT',
-FT',
+    'UNLICENSE',
     'COMMITMENT'
+    'COMMITMENTS'
 )
 
 
 special_names_lower = tuple(x.lower() for x in special_names)
 
 
-def is_special_egal_file(location):
+def is_special_legal_file(location):
     """
     Return an indication that a file may be a "special" legal-like file.
     """
-    file_base_name = fileutils.file_base_name(location).lower()
-    file_extens    file_base_name = fileutils.file_base_name(location)
+    file_base_name = fileutils.file_base_name(location)
     file_base_name_lower = file_base_name.lower()
     file_extension = fileutils.file_extension(location)
     file_extension_lower = file_extension.lower()
 
-    name_contains_special = any(
+    name_contains_special = (
         special_name in file_base_name or
         special_name in file_extension
             for special_name in special_names
     )
 
-    name_lower_is_special = any(
+    name_lower_is_special = (
         special_name_lower == file_base_name_lower or
         special_name_lower == file_extension_lower
             for special_name_lower in special_names_lower
     )
 
-    name_lower_contains_special = any(
+    name_lower_contains_special = (
         special_name_lower in file_base_name_lower or
         special_name_lower in file_extension_lower
             for special_name_lower in special_names_lower
     )
-_extension
-            for special_name in special_names_lower)
-     or any(special_name in file_base_name
-            or special_name in file_extension
-            for special_name in special_names)):
+
+    if any(name_contains_special) or any(name_lower_is_special):
         return 'yes'
 
-    elif any(special_name in file_base_name
-             or special_name in file_extension
-            for special_name in special_names_lower):
+    elif any(name_lower_contains_special):
         return 'maybe'
     else:
         # return False for now?
--- tests/licensedcode/test_legal.py
+++ tests/licensedcode/test_legal.py
@@ -22,7 +22,6 @@
 #  ScanCode is a free software code scanning tool from nexB Inc. and others.
 #  Visit https://github.com/nexB/scancode-toolkit/ for support and download.
 
-
 import os
 
 from commoncode.testcase import FileBasedTesting
@@ -34,16 +33,37 @@ TEST_DATA_DIR = os.path.join(os.path.dirname(__file__), 'data')
 class TestSpecialFiles(FileBasedTesting):
     test_data_dir = TEST_DATA_DIR
 
-    def test_license_special_files(self):
-        tests = [
-            ('legal/COPYING', 'yes'),
-            ('legal/Copyrights', 'maybe'),
-            ('legal/LICENSE', 'yes'),
-            ('legal/Notice', 'yes'),
-            ('legal/no_license_in_here.java', 'maybe'),
-            ('legal/noticE.html', 'yes'),
-            ('legal/useless_notice.txt', 'maybe')
-        ]
+    def test_is_special_legal_file_COPYING(self):
+        test_loc = self.get_test_loc('legal/COPYING')
+        expected = 'yes'
+        assert expected == legal.is_special_legal_file(test_loc)
 
-        for tf, expected in tests:
-            assert expected == legal.is_special_legal_file(self.get_test_loc(tf))
+    def test_is_special_legal_file_Copyrights(self):
+        test_loc = self.get_test_loc('legal/Copyrights')
+        expected = 'yes'
+        assert expected == legal.is_special_legal_file(test_loc)
+
+    def test_is_special_legal_file_LICENSE(self):
+        test_loc = self.get_test_loc('legal/LICENSE')
+        expected = 'yes'
+        assert expected == legal.is_special_legal_file(test_loc)
+
+    def test_is_special_legal_file_Notice(self):
+        test_loc = self.get_test_loc('legal/Notice')
+        expected = 'yes'
+        assert expected == legal.is_special_legal_file(test_loc)
+
+    def test_is_special_legal_file_no_license_in_here_java(self):
+        test_loc = self.get_test_loc('legal/no_license_in_here.java')
+        expected = 'maybe'
+        assert expected == legal.is_special_legal_file(test_loc)
+
+    def test_is_special_legal_file_noticE_html(self):
+        test_loc = self.get_test_loc('legal/noticE.html')
+        expected = 'yes'
+        assert expected == legal.is_special_legal_file(test_loc)
+
+    def test_is_special_legal_file_useless_notice_txt(self):
+        test_loc = self.get_test_loc('legal/useless_notice.txt')
+        expected = 'maybe'
+        assert expected == legal.is_special_legal_file(test_loc)
