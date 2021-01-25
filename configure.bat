cd4e87beb91ea5e9380dfeb19c3530c0a92ff192
Philippe Ombredanne <pombredanne@nexb.com>
2021-01-25 12:56:18 +0100

    Do not force an upgrade on virtualenv.pyz embeds
    Signed-off-by: Philippe Ombredanne <pombredanne@nexb.com>

--- configure.bat
+++ configure.bat
@@ -89,7 +89,6 @@ call mkdir "%CFG_ROOT_DIR%tmp"
 call curl -o "%CFG_ROOT_DIR%tmp\virtualenv.pyz" https://bootstrap.pypa.io/virtualenv.pyz
 call %PYTHON_EXECUTABLE% "%CFG_ROOT_DIR%tmp\virtualenv.pyz"  --wheel embed --pip embed --setuptools embed --seeder pip  "%CFG_ROOT_DIR%tmp"
 call "%CFG_ROOT_DIR%tmp\Scripts\activate"
-call "%CFG_ROOT_DIR%tmp\Scripts\pip" install --upgrade pip virtualenv setuptools wheel
 call "%CFG_ROOT_DIR%tmp\Scripts\pip" install -e .[testing]
 
 @rem Return a proper return code on failure
