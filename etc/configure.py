4ece2d2ec0b4f80a23e822842abf66633abb5487
Philippe Ombredanne <pombredanne@nexb.com>
2021-01-25 13:00:58 +0100

    Use wheels embedded in virtualenv.pyz
    Also add proper notice
    
    Signed-off-by: Philippe Ombredanne <pombredanne@nexb.com>

--- etc/configure.py
+++ etc/configure.py
@@ -1,6 +1,24 @@
 #!/usr/bin/python
 
-# Copyright (c) nexB Inc. http://www.nexb.com/ - All rights reserved.
+#
+# Copyright (c) nexB Inc. and others.
+# SPDX-License-Identifier: Apache-2.0
+#
+# Visit https://aboutcode.org and https://github.com/nexB/ for support and download.
+# ScanCode is a trademark of nexB Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
 
 """
 A configuration helper that wraps virtualenv and pip to configure an Python
@@ -157,7 +175,13 @@ def create_virtualenv(root_dir, venv_pyz, quiet=False):
     standard_python = sys.executable
 
     # once we have a pyz, we do not want to download anything else nor ever update
-    vcmd = [standard_python, quote(venv_pyz), '--never-download', '--no-periodic-update']
+    vcmd = [
+        standard_python, quote(venv_pyz),
+        '--wheel', 'embed', '--pip', 'embed', '--setuptools', 'embed',
+        '--seeder', 'pip',
+        '--never-download',
+        '--no-periodic-update',
+    ]
 
     if quiet:
         vcmd += ['-qq']
