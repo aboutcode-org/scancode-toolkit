510b5870034d54b42460ec1f6fc1a6dc6b9ca575
Philippe Ombredanne <pombredanne@gmail.com>
2021-03-30 17:58:06 +0200

    Add header to new file #372
    Signed-off-by: Philippe Ombredanne <pombredanne@nexb.com>

--- src/cluecode/plugin_filter_ignorable_copyrights.py
+++ src/cluecode/plugin_filter_ignorable_copyrights.py
@@ -1,3 +1,12 @@
+#
+# Copyright (c) nexB Inc. and others. All rights reserved.
+# ScanCode is a trademark of nexB Inc.
+# SPDX-License-Identifier: Apache-2.0
+# See http://www.apache.org/licenses/LICENSE-2.0 for the license text.
+# See https://github.com/nexB/scancode-toolkit for support or download.
+# See https://aboutcode.org for more information about nexB OSS projects.
+#
+
 from commoncode.cliutils import OUTPUT_FILTER_GROUP
 from plugincode.output_filter import OutputFilterPlugin
 from plugincode.output_filter import output_filter_impl
