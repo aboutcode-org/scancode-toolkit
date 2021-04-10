5f39252c3c817cb26bfbf7f93ceea08d8ab0c3a2
Philippe Ombredanne <pombredanne@nexb.com>
2021-04-10 15:21:00 +0200

    Add new false positive rules for SPDX ids
    These are generated sequences of false positive licenses tyically
    found in license lists and license handling tools.
    
    Signed-off-by: Philippe Ombredanne <pombredanne@nexb.com>

--- etc/scripts/licenses/gen_spdx_lists_fp.py
+++ etc/scripts/licenses/gen_spdx_lists_fp.py
@@ -56,18 +56,28 @@ notes: a sequence of SPDX license ids and names is not a license
 
 
 @click.command()
-@click.argument('license_dir', type=click.Path(), metavar='DIR')
+@click.argument(
+    'license_dir', type=click.Path(), metavar='DIR')
+
 @click.argument(
     # 'A buildrules-formatted file used to generate new licenses rules.')
     'output', type=click.Path(), metavar='FILE')
+
 @click.option(
     '--commitish', type=str, default=None,
     help='An optional commitish to use for SPDX license data instead of the latest release.')
+
+@click.option(
+    # 'A buildrules-formatted file used to generate new licenses rules.')
+    '--from-list', default=None, type=click.Path(), metavar='LIST_FILE',
+    help='Use file with a list of entries to ignore instead')
+
 @click.option(
     '-t', '--trace', is_flag=True, default=False,
     help='Print execution trace.')
+
 @click.help_option('-h', '--help')
-def cli(license_dir, output, commitish=None, trace=False):
+def cli(license_dir, output, commitish=None, from_list=None, trace=False):
     """
     Generate ScanCode false-positive license detection rules from lists of SPDX
     license. Save these in FILE for use with buildrules.
@@ -77,77 +87,81 @@ def cli(license_dir, output, commitish=None, trace=False):
     global TRACE
     TRACE = trace
 
-    spdx_source = synclic.SpdxSource(external_base_dir=license_dir)
+    if not from_list:
+        spdx_source = synclic.SpdxSource(external_base_dir=license_dir)
 
-    spdx_by_key = spdx_source.get_licenses(
-        commitish=commitish,
-        skip_oddities=False,
-    )
+        spdx_by_key = spdx_source.get_licenses(
+            commitish=commitish,
+            skip_oddities=False,
+        )
 
-    all_licenses_and_exceptions = []
-    all_licenses_and_exceptions_non_deprecated = []
-    licenses = []
-    exceptions = []
-    licenses_non_deprecated = []
-    exceptions_non_deprecated = []
+        all_licenses_and_exceptions = []
+        all_licenses_and_exceptions_non_deprecated = []
+        licenses = []
+        exceptions = []
+        licenses_non_deprecated = []
+        exceptions_non_deprecated = []
 
-    lists_of_licenses = [
-        all_licenses_and_exceptions,
-        all_licenses_and_exceptions_non_deprecated,
-        licenses,
-        exceptions,
-        licenses_non_deprecated,
-        exceptions_non_deprecated,
-    ]
+        lists_of_licenses = [
+            all_licenses_and_exceptions,
+            all_licenses_and_exceptions_non_deprecated,
+            licenses,
+            exceptions,
+            licenses_non_deprecated,
+            exceptions_non_deprecated,
+        ]
 
-    for lspdx in spdx_by_key.values():
-        all_licenses_and_exceptions.append(lspdx)
-        is_deprecated = lspdx.is_deprecated
-        if not is_deprecated:
-            all_licenses_and_exceptions_non_deprecated.append(lspdx)
-        if lspdx.is_exception:
-            exceptions.append(lspdx)
+        for lspdx in spdx_by_key.values():
+            all_licenses_and_exceptions.append(lspdx)
+            is_deprecated = lspdx.is_deprecated
             if not is_deprecated:
-                exceptions_non_deprecated.append(lspdx)
-        else:
-            licenses.append(lspdx)
-            if not is_deprecated:
-                licenses_non_deprecated.append(lspdx)
+                all_licenses_and_exceptions_non_deprecated.append(lspdx)
+            if lspdx.is_exception:
+                exceptions.append(lspdx)
+                if not is_deprecated:
+                    exceptions_non_deprecated.append(lspdx)
+            else:
+                licenses.append(lspdx)
+                if not is_deprecated:
+                    licenses_non_deprecated.append(lspdx)
 
-    seen = set()
-    with open(output, 'w') as o:
+        lists_of_sorted_licenses = []
         for lic_list in lists_of_licenses:
             sorted_case_sensitive = sorted(lic_list, key=lambda x: x.spdx_license_key)
+
             as_ids = [l.spdx_license_key for l in sorted_case_sensitive]
-            for text in ['\n'.join(ngs) for ngs in ngrams(as_ids, ngram_length=6)]:
-                if text in seen:
-                    continue
-                seen.add(text)
-                o.write(template.format(text))
+            lists_of_sorted_licenses.append(as_ids)
 
             as_id_names = [f'{l.spdx_license_key}  {l.name}' for l in sorted_case_sensitive]
-            for text in ['\n'.join(ngs) for ngs in ngrams(as_id_names, ngram_length=6)]:
-                if text in seen:
-                    continue
-                seen.add(text)
-                o.write(template.format(text))
+            lists_of_sorted_licenses.append(as_id_names)
 
             sorted_case_insensitive = sorted(lic_list, key=lambda x: x.spdx_license_key.lower())
             as_ids = [l.spdx_license_key for l in sorted_case_insensitive]
-            for text in ['\n'.join(ngs) for ngs in ngrams(as_ids, ngram_length=6)]:
-                if text in seen:
-                    continue
-                seen.add(text)
-                o.write(template.format(text))
+            lists_of_sorted_licenses.append(as_ids)
 
             as_id_names = [f'{l.spdx_license_key}  {l.name}' for l in sorted_case_insensitive]
-            for text in ['\n'.join(ngs) for ngs in ngrams(as_id_names, ngram_length=6)]:
-                if text in seen:
-                    continue
-                seen.add(text)
-                o.write(template.format(text))
+            lists_of_sorted_licenses.append(as_id_names)
+
+    else:
+        with open(from_list) as inp:
+            lists_of_sorted_licenses = [inp.read().splitlines(False)]
+
+    with open(output, 'w') as o:
+        for lic_list in lists_of_sorted_licenses:
+            write_ngrams(texts=lic_list, output=o)
 
         o.write('----------------------------------------\n')
+
+
+def write_ngrams(texts, output, _seen=set(), ngram_length=6):
+    """
+    Write the texts list as ngrams to the output file-like object.
+    """
+    for text in ['\n'.join(ngs) for ngs in ngrams(texts, ngram_length=ngram_length)]:
+        if text in _seen:
+            continue
+        _seen.add(text)
+        output.write(template.format(text))
 
 
 if __name__ == '__main__':
