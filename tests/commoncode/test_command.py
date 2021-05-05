1ce1d9f12517bef5b6a038e46c04c4436c6601eb
Philippe Ombredanne <pombredanne@nexb.com>
2021-05-05 08:55:28 +0200

    Improve running command in subprocesses.
    Also add support to find a file in the PATH which is needed as a
    fallback option when searching for a certain command executable.
    
    Signed-off-by: Philippe Ombredanne <pombredanne@nexb.com>

--- tests/commoncode/test_command.py
+++ tests/commoncode/test_command.py
@@ -29,15 +29,17 @@ from commoncode.testcase import FileBasedTesting
 from commoncode.system import on_linux
 from commoncode.system import on_mac
 from commoncode.system import on_windows
+from commoncode.system import py36
 
 
 class TestCommand(FileBasedTesting):
     test_data_dir = os.path.join(os.path.dirname(__file__), 'data')
 
-    def test_execute2_non_ascii_output(self):
+    @skipIf(py36, 'This fails on Python 3.6 https://bugs.python.org/issue26919')
+    def test_execute_can_handle_non_ascii_output(self):
         # Popen returns a *binary* string with non-ascii chars: skips these
         python = sys.executable
-        rc, stdout, stderr = command.execute2(
+        rc, stdout, stderr = command.execute(
             python, ['-c', 'print("non ascii: été just passed it !")']
         )
         assert '' == stderr
@@ -46,7 +48,29 @@ class TestCommand(FileBasedTesting):
         # do not throw exception
         stdout.encode('ascii')
 
-    @skipIf(not on_linux, 'Linux py3 only')
+    def test_execute_(self):
+        python = sys.executable
+        rc, stdout, stderr = command.execute(
+            python, ['-c', 'print("foobar")']
+        )
+        assert '' == stderr
+        assert 'foobar' == stdout
+        assert 0 == rc
+        # do not throw exception
+        stdout.encode('ascii')
+
+    def test_execute2(self):
+        python = sys.executable
+        rc, stdout, stderr = command.execute2(
+            python, ['-c', 'print("foobar")']
+        )
+        assert '' == stderr
+        assert 'foobar' == stdout
+        assert 0 == rc
+        # do not throw exception
+        stdout.encode('ascii')
+
+    @skipIf(not on_linux, 'Linux only')
     def test_update_path_var_on_linux(self):
         existing_path_var = '/usr/bin:/usr/local'
 
@@ -70,7 +94,7 @@ class TestCommand(FileBasedTesting):
         updated_path = command.update_path_var(updated_path, new_path)
         assert '/bin/foo\udcb1bar:foo\udcb1bar:/usr/bin:/usr/local' == updated_path
 
-    @skipIf(not on_mac, 'Mac only py3')
+    @skipIf(not on_mac, 'Mac only')
     def test_update_path_var_on_mac(self):
         existing_path_var = '/usr/bin:/usr/local'
 
@@ -86,7 +110,7 @@ class TestCommand(FileBasedTesting):
         updated_path = command.update_path_var(updated_path, new_path)
         assert '/bin/foo\udcb1bar:foo\udcb1bar:/usr/bin:/usr/local' == updated_path
 
-    @skipIf(not on_windows, 'Windows only on Py3')
+    @skipIf(not on_windows, 'Windows only')
     def test_update_path_var_on_windows(self):
         existing_path_var = u'c:\\windows;C:Program Files'
 
@@ -97,3 +121,35 @@ class TestCommand(FileBasedTesting):
         new_path = u'foo\udcb1bar'
         updated_path = command.update_path_var(updated_path, new_path)
         assert u'foo\udcb1bar;c:\\windows;C:Program Files' == updated_path
+
+    def test_searchable_paths(self):
+        d1 = self.get_temp_dir('foo')
+        d2 = self.get_temp_dir('bar')
+        ps = os.pathsep
+        os.environ['FOO_SCANCODE_TEST1'] = f'{ps}{d1}{ps}{ps}{d2}{ps}'
+        os.environ['FOO_SCANCODE_TEST2'] = f'{ps}{d2}{ps}{ps}{d1}{ps}/NOTADIR'
+
+        env_vars = 'FOO_SCANCODE_TEST1', 'FOO_SCANCODE_TEST2'
+        expected = d1, d2, d2, d1
+        assert command.searchable_paths(env_vars=env_vars) == expected
+
+    def test_find_in_path(self):
+        d1 = self.get_temp_dir('foo')
+        d2 = self.get_temp_dir('bar')
+        filename = 'baz'
+
+        assert command.find_in_path(filename, searchable_paths=(d1, d2,)) == None
+
+        f2 = os.path.join(d2, filename)
+        with open(f2, 'w') as o:
+            o.write('some')
+
+        assert command.find_in_path(filename, searchable_paths=(d1, d2,)) == f2
+        assert command.find_in_path(filename, searchable_paths=(d2, d1,)) == f2
+
+        f1 = os.path.join(d1, filename)
+        with open(f1, 'w') as o:
+            o.write('some')
+
+        assert command.find_in_path(filename, searchable_paths=(d1, d2,)) == f1
+        assert command.find_in_path(filename, searchable_paths=(d2, d1,)) == f2
