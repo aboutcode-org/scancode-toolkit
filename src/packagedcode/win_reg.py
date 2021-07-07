4d3acda225a16b5dc242cfd4657bee193f4c5629
Jono Yang <jyang@nexb.com>
2021-07-07 16:24:51 -0700

    Refactor report_installed_programs #2565
        * Create get_registry_tree that returns a list of dicts that contain the registry entries
        * report_dotnet_versions and report_installed_programs is now broken up into two parts, first is a call it get_registry_tree and a second is a call to _report_installed_programs or _report_dotnet_versions. These functions prefixed with underscores takes in a list of dictionaries
        * Update tests by replacing the SOFTWARE registry file with json results
    
    Signed-off-by: Jono Yang <jyang@nexb.com>

--- src/packagedcode/win_reg.py
+++ src/packagedcode/win_reg.py
@@ -63,35 +63,49 @@ def get_registry_name_key_entry(registry_hive, registry_path):
     return name_key_entry
 
 
-def report_installed_dotnet_versions(location, registry_path='\\Microsoft\\NET Framework Setup\\NDP'):
+def get_registry_tree(registry_location, registry_path):
     """
-    Return the installed versions of .NET framework. The logic to retrieve
-    installed .NET version has been outlined here:
-    https://docs.microsoft.com/en-us/dotnet/framework/migration-guide/how-to-determine-which-versions-are-installed
-
-    If `registry_path` is provided, then we will load Registry entries starting
-    from `registry_path`
+    Return a list of dictionaries of Window registry entries found under a given
+    registry path
     """
-    registry_hive = RegistryHive(location)
+    registry_hive = RegistryHive(registry_location)
     name_key_entry = get_registry_name_key_entry(
         registry_hive=registry_hive,
         registry_path=registry_path
     )
     if not name_key_entry:
+        return []
+    return [attr.asdict(entry) for entry in registry_hive.recurse_subkeys(name_key_entry, as_json=True)]
+
+
+def report_installed_dotnet_versions(location, registry_path='\\Microsoft\\NET Framework Setup\\NDP'):
+    """
+    Yield the installed versions of .NET framework from `location`. The logic to
+    retrieve installed .NET version has been outlined here:
+    https://docs.microsoft.com/en-us/dotnet/framework/migration-guide/how-to-determine-which-versions-are-installed
+    """
+    registry_tree = get_registry_tree(location, registry_path)
+    return _report_installed_dotnet_versions(registry_tree)
+
+
+def _report_installed_dotnet_versions(registry_tree):
+    if not registry_tree:
         return
 
-    for entry in registry_hive.recurse_subkeys(name_key_entry):
+    for entry in registry_tree:
         # The .NET version can be found in the path whose last segment ends with
         # `Full`
-        if not entry.path.endswith('\\Full'):
+        if not entry.get('path', '').endswith('\\Full'):
             continue
 
         dotnet_info = {}
-        for dotnet_info_value in entry.values:
-            if dotnet_info_value.name == 'Version':
-                dotnet_info['version'] = dotnet_info_value.value
-            if dotnet_info_value.name == 'InstallPath':
-                dotnet_info['InstallPath'] = dotnet_info_value.value
+        for dotnet_info_value in entry.get('values', []):
+            dotnet_info_value_name = dotnet_info_value.get('name')
+            dotnet_info_value_value = dotnet_info_value.get('value')
+            if dotnet_info_value_name == 'Version':
+                dotnet_info['version'] = dotnet_info_value_value
+            if dotnet_info_value_name == 'InstallPath':
+                dotnet_info['InstallPath'] = dotnet_info_value_value
 
         version = dotnet_info.get('version')
         if not version:
@@ -102,9 +116,9 @@ def report_installed_dotnet_versions(location, registry_path='\\Microsoft\\NET F
         if install_path:
             extra_data['install_location'] = install_path
 
-        # Create package
+        # Yield package
         yield InstalledWindowsProgram(
-            name='.NET Framework',
+            name='Microsoft .NET Framework',
             version=version,
             extra_data=extra_data,
         )
@@ -112,48 +126,52 @@ def report_installed_dotnet_versions(location, registry_path='\\Microsoft\\NET F
 
 def report_installed_programs(location, registry_path='\\Microsoft\\Windows\\CurrentVersion\\Uninstall'):
     """
-    Return the installed programs from a Windows registry file. This is done by
-    looking at the entries of the uninstallable programs list.
+    Yield installed Windows packages from a Windows registry file at `location`.
+    This is done by looking at the entries of the uninstallable programs list.
 
     If `registry_path` is provided, then we will load Registry entries starting
     from `registry_path`
     """
-    registry_hive = RegistryHive(location)
-    name_key_entry = get_registry_name_key_entry(
-        registry_hive=registry_hive,
-        registry_path=registry_path
-    )
-    if not name_key_entry:
+    registry_tree = get_registry_tree(location, registry_path)
+    return _report_installed_programs(registry_tree)
+
+
+def _report_installed_programs(registry_tree):
+    if not registry_tree:
         return
 
     # Collect Package information and create Package if we have a valid Package name
-    for entry in registry_hive.recurse_subkeys(name_key_entry):
+    for entry in registry_tree:
         package_info = {}
-        for entry_value in entry.values:
-            if entry_value.name == 'DisplayName':
-                package_info['DisplayName'] = entry_value.value
-            if entry_value.name == 'DisplayVersion':
-                package_info['DisplayVersion'] = entry_value.value
-            if entry_value.name == 'InstallLocation':
-                package_info['InstallLocation'] = entry_value.value
-            if entry_value.name == 'Publisher':
-                package_info['Publisher'] = entry_value.value
-            if entry_value.name == 'URLInfoAbout':
-                package_info['URLInfoAbout'] = entry_value.value
+        for entry_value in entry.get('values', []):
+            name = entry_value.get('name')
+            value = entry_value.get('value')
 
-        name = package_info.get('DisplayName')
+            if name == 'DisplayName':
+                package_info['name'] = value
+            if name == 'DisplayVersion':
+                package_info['version'] = value
+            if name == 'InstallLocation':
+                package_info['install_location'] = value
+            if name == 'Publisher':
+                package_info['publisher'] = value
+            if name == 'URLInfoAbout':
+                package_info['homepage_url'] = value
+
+        name = package_info.get('name')
         if not name:
             continue
 
-        version = package_info.get('DisplayVersion')
-        install_location = package_info.get('InstallLocation')
-        publisher = package_info.get('Publisher')
+        version = package_info.get('version')
+        homepage_url = package_info.get('homepage_url')
+        install_location = package_info.get('install_location')
+        publisher = package_info.get('publisher')
 
         parties = []
         if publisher:
             parties.append(
                 models.Party(
-                    type=None,
+                    type=models.party_org,
                     role='publisher',
                     name=publisher
                 )
@@ -163,10 +181,14 @@ def report_installed_programs(location, registry_path='\\Microsoft\\Windows\\Cur
         if install_location:
             extra_data['install_location'] = install_location
 
+        # TODO: report installed files from "DisplayIcon" and "UninstallString",
+        # they may be placed in different locations than the install location of
+        # the program
         yield InstalledWindowsProgram(
             name=name,
             version=version,
             parties=parties,
+            homepage_url=homepage_url,
             extra_data=extra_data
         )
 
@@ -178,19 +200,23 @@ def reg_parse(location):
         yield installed_dotnet
 
 
-def get_installed_programs(root_dir):
+def get_installed_packages(root_dir, is_container=True):
     """
     Yield InstalledWindowsProgram objects for every detected installed program
     from Windows registry files in known locations
     """
 
     # These paths are relative to a Windows docker image layer root directory
-    software_delta_loc = os.path.join(root_dir, 'Hives', 'Software_Delta')
-    SOFTWARE_reg_file_loc = os.path.join(root_dir, 'Files/Windows/System32/config/SOFTWARE')
-    software_registry_locations = [
-        software_delta_loc,
-        SOFTWARE_reg_file_loc
-    ]
+    # TODO: have a way
+    if is_container:
+        software_registry_locations = [
+            os.path.join(root_dir, 'Hives', 'Software_Delta'),
+            os.path.join(root_dir, 'Files/Windows/System32/config/SOFTWARE')
+        ]
+    else:
+        # TODO: Add support for virtual machines
+        #
+        raise Exception('Unsuported file system type')
     for software_registry_loc in software_registry_locations:
         if not os.path.exists(software_registry_loc):
             continue
@@ -208,25 +234,30 @@ class InstalledWindowsProgram(models.Package):
         for installed in reg_parse(location):
             yield installed
 
-    def populate_installed_files(self, root_dir):
+    def populate_installed_files(self, root_dir=None):
         install_location = self.extra_data.get('install_location')
         if not install_location:
             return
 
-        # Manipulate install_location to fit extracted Docker image structure
+        # Remove leading drive letter ("C:\\")
         install_location = PureWindowsPath(install_location)
-        # Remove leading drive letter ("C:\\") and replace all spaces with underscore
         install_location_no_root = install_location.relative_to(*install_location.parts[:1])
-        install_location_no_root = str(install_location_no_root)
-        install_location_no_root = install_location_no_root.replace(' ', '_')
-        install_location_in_image = Path(root_dir).joinpath('Files', PureWindowsPath(install_location_no_root))
+        # POSIX-ize the path
+        install_location_no_root_posix = install_location_no_root.as_posix()
 
-        installed_files = []
-        for root, _, files in os.walk(install_location_in_image):
-            for file in files:
-                installed_file_path = os.path.join(root, file)
-                installed_files.append(
-                    models.PackageFile(path=installed_file_path)
-                )
+        if not root_dir:
+            self.installed_files = [
+                models.PackageFile(path=install_location_no_root_posix)
+            ]
+        else:
+            install_location = Path(root_dir).joinpath(install_location_no_root_posix)
 
-        self.installed_files = installed_files
+            installed_files = []
+            for root, _, files in os.walk(install_location):
+                for file in files:
+                    installed_file_path = os.path.join(root, file)
+                    installed_files.append(
+                        models.PackageFile(path=installed_file_path)
+                    )
+
+            self.installed_files = installed_files
--- tests/packagedcode/test_win_reg.py
+++ tests/packagedcode/test_win_reg.py
@@ -7,11 +7,11 @@
 # See https://aboutcode.org for more information about nexB OSS projects.
 #
 
+import json
 import os
 
-from packagedcode.win_reg import reg_parse
-from packagedcode.win_reg import report_installed_dotnet_versions
-from packagedcode.win_reg import report_installed_programs
+from packagedcode.win_reg import _report_installed_dotnet_versions
+from packagedcode.win_reg import _report_installed_programs
 
 from packages_test_utils import PackageTester
 
@@ -19,20 +19,18 @@ from packages_test_utils import PackageTester
 class TestWindowsHelpers(PackageTester):
     test_data_dir = os.path.join(os.path.dirname(__file__), 'data')
 
-    def test_report_installed_dotnet_versions(self):
-        test_file = self.get_test_loc('win_reg/Software_Delta')
-        expected_loc = self.get_test_loc('win_reg/Software_Delta-dotnet.expected')
-        package = report_installed_dotnet_versions(test_file)
-        self.check_packages(package, expected_loc, regen=False)
+    def test__report_installed_dotnet_versions(self):
+        test_file = self.get_test_loc('win_reg/SOFTWARE-registry-entries.json')
+        expected_loc = self.get_test_loc('win_reg/SOFTWARE-dotnet-installation.expected')
+        with open(test_file) as f:
+            software_registry_entries = json.load(f)
+        packages = _report_installed_dotnet_versions(software_registry_entries)
+        self.check_packages(packages, expected_loc, regen=False)
 
-    def test_report_installed_programs(self):
-        test_file = self.get_test_loc('win_reg/Software_Delta')
-        expected_loc = self.get_test_loc('win_reg/Software_Delta-installed.expected')
-        package = report_installed_programs(test_file)
-        self.check_packages(package, expected_loc, regen=False)
-
-    def test_reg_parse_SOFTWARE(self):
-        test_file = self.get_test_loc('win_reg/Software_Delta')
-        expected_loc = self.get_test_loc('win_reg/Software_Delta.expected')
-        package = reg_parse(test_file)
-        self.check_packages(package, expected_loc, regen=False)
+    def test__report_installed_programs(self):
+        test_file = self.get_test_loc('win_reg/SOFTWARE-registry-entries.json')
+        expected_loc = self.get_test_loc('win_reg/SOFTWARE-installed-programs.expected')
+        with open(test_file) as f:
+            software_registry_entries = json.load(f)
+        packages = _report_installed_programs(software_registry_entries)
+        self.check_packages(packages, expected_loc, regen=False)
