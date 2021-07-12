8732defbc7fb20f122698122c5fd0e8d7b0e350e
Philippe Ombredanne <pombredanne@nexb.com>
2021-07-12 14:24:39 +0200

    Enable pub detection in scans
    Signed-off-by: Philippe Ombredanne <pombredanne@nexb.com>

--- tests/packagedcode/test_plugin.py
+++ tests/packagedcode/test_plugin.py
@@ -139,3 +139,10 @@ class TestPlugins(PackageTester):
         expected_file = self.get_test_loc('plugin/cran-package-expected.json')
         run_scan_click(['--package', '--strip-root', '--processes', '-1', test_dir, '--json', result_file])
         check_json_scan(expected_file, result_file, regen=False)
+
+    def test_package_command_scan_pubspec_package(self):
+        test_dir = self.get_test_loc('pubspec/specs/authors-pubspec.yaml')
+        result_file = self.get_temp_file('json')
+        expected_file = self.get_test_loc('plugin/pubspec-expected.json', must_exist=False)
+        run_scan_click(['--package', '--strip-root', '--processes', '-1', test_dir, '--json', result_file])
+        check_json_scan(expected_file, result_file, regen=False)
