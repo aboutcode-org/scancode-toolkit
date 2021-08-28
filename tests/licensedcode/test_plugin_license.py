cdd4c0e148e10ce78e29effb25983e730e18ae98
Philippe Ombredanne <pombredanne@nexb.com>
2021-08-29 00:53:08 +0200

    Do not match referenced_filenames path suffixes #1364
    Only follow license references match an exact filename
    In #2616 we introduced matching path of referenced_filenames
    based on matching filename or path suffix. This removes path suffix
    matching which is problematic.
    
    Before this we were using .endswith(path) and this led to weird and
    incorrect license dereferences
    
    Signed-off-by: Philippe Ombredanne <pombredanne@nexb.com>

--- tests/licensedcode/test_plugin_license.py
+++ tests/licensedcode/test_plugin_license.py
@@ -9,8 +9,7 @@
 
 import os
 
-from click.testing import Result
-from licensedcode.plugin_license import find_referenced_resource, match_reference_license
+from licensedcode.plugin_license import find_referenced_resource, add_referenced_filenames_license_matches
 from licensedcode.plugin_license import get_referenced_filenames
 
 import pytest
@@ -92,12 +91,12 @@ def test_license_match_without_reference():
 
 def test_get_referenced_filenames():
     license_matches = [
-        {'matched_rule': {'referenced_filenames' : ['LICENSE.txt','COPYING']}},
-        {'matched_rule': {'referenced_filenames' : ['COPYING','LICENSE.txt']}},
+        {'matched_rule': {'referenced_filenames' : ['LICENSE.txt', 'COPYING']}},
+        {'matched_rule': {'referenced_filenames' : ['COPYING', 'LICENSE.txt']}},
         {'matched_rule': {'referenced_filenames' : ['copying']}},
         {'matched_rule': {'referenced_filenames' : []}},
     ]
-    expected = ['LICENSE.txt','COPYING','copying']
+    expected = ['LICENSE.txt', 'COPYING', 'copying']
     assert get_referenced_filenames(license_matches) == expected
 
 
@@ -109,14 +108,31 @@ def test_find_referenced_resource():
     run_scan_click(args)
 
     # test proper
-    from commoncode.resource import VirtualCodebase        
+    from commoncode.resource import VirtualCodebase
     codebase = VirtualCodebase(scan_loc)
     resource = codebase.get_resource_from_path('scan-ref/license-notice.txt')
-    result = find_referenced_resource(referenced_filename='LICENSE',resource=resource, codebase=codebase)
+    result = find_referenced_resource(referenced_filename='LICENSE', resource=resource, codebase=codebase)
     assert result.path == 'scan-ref/LICENSE'
 
 
-def test_match_reference_license():
+def test_find_referenced_resource_does_not_find_based_file_name_suffix():
+    # Setup: Create a new scan to use for a virtual codebase. This directory has
+    # two test file with the same name suffix which is also a referenced
+    # filename
+    test_dir = test_env.get_test_loc('plugin_license/license_reference/scan/scan-ref-dupe-name-suffix', copy=True)
+    scan_loc = test_env.get_temp_file('json')
+    args = ['--license', '--license-text', '--license-text-diagnostics', test_dir, '--json', scan_loc]
+    run_scan_click(args)
+
+    # test proper
+    from commoncode.resource import VirtualCodebase
+    codebase = VirtualCodebase(scan_loc)
+    resource = codebase.get_resource_from_path('scan-ref-dupe-name-suffix/license-notice.txt')
+    result = find_referenced_resource(referenced_filename='LICENSE', resource=resource, codebase=codebase)
+    assert result.path == 'scan-ref-dupe-name-suffix/LICENSE'
+
+
+def test_add_referenced_filenames_license_matches():
     # Setup: Create a new scan to use for a virtual codebase
     test_dir = test_env.get_test_loc('plugin_license/license_reference/scan/scan-ref', copy=True)
     scan_loc = test_env.get_temp_file('json')
@@ -124,11 +140,11 @@ def test_match_reference_license():
     run_scan_click(args)
 
     # test proper
-    from commoncode.resource import VirtualCodebase        
+    from commoncode.resource import VirtualCodebase
     codebase = VirtualCodebase(scan_loc)
     resource = codebase.get_resource_from_path('scan-ref/license-notice.txt')
     assert len(resource.licenses) == 2
-    result = match_reference_license(resource=resource, codebase=codebase)
+    result = add_referenced_filenames_license_matches(resource=resource, codebase=codebase)
     assert len(result.licenses) == 3
 
 
