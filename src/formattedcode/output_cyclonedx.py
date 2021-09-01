799d72d7a54637650e9a2f4876fe0831eddc9f80
Alexander Gschrei <alexander.gschrei@fau.de>
2021-09-01 16:28:35 +0200

    extract CycloneDX components from codebase packages #1888
    Signed-off-by: Alexander Gschrei <alexander.gschrei@fau.de>

--- src/formattedcode/output_cyclonedx.py
+++ src/formattedcode/output_cyclonedx.py
@@ -8,27 +8,155 @@
 # See https://aboutcode.org for more information about nexB OSS projects.
 #
 
+import attr
+import click
 from commoncode.cliutils import PluggableCommandLineOption
 from commoncode.cliutils import OUTPUT_GROUP
+from datetime import datetime
 from enum import Enum
 from formattedcode import FileOptionType
+import json
+from typing import List
 from plugincode.output import output_impl
 from plugincode.output import OutputPlugin
+import uuid
+
+hash_type_mapping = {
+    'md5': 'MD5',
+    'sha1': 'SHA-1',
+    'sha256': 'SHA-256',
+    'sha512': 'SHA-512',
+}
 
 
 class CycloneDxFlavor(Enum):
     XML = 0
     JSON = 1
 
+@attr.s
+class CycloneDxLicense:
+    id: str = attr.ib(default=None)
+    name: str = attr.ib(default=None)
+    url: str = attr.ib(default=None)
+
+@attr.s
+class CycloneDxLicenseEntry:
+    license: CycloneDxLicense = attr.ib()
+    expression: str = attr.ib(default=None)
+
+@attr.s
+class CycloneDxAttribute:
+    name: str = attr.ib()
+    value: str = attr.ib()
+
+@attr.s
+class CycloneDxMetadata():
+    timestamp = attr.ib(
+        default=datetime.utcnow().strftime('%Y-%m-%dT%H:%M:%SZ'))
+    tools: List[dict] = attr.ib(factory=list)
+    properties: List[CycloneDxAttribute] = attr.ib(factory=list)
+
+@attr.s
+class CycloneDxHashObject():
+    alg: str = attr.ib()
+    content: str = attr.ib()
+
+@attr.s
+class CycloneDxExternalRef:
+    url: str = attr.ib()
+    type: str = attr.ib(validator=attr.validators.in_(["vcs", "issue-tracker","website",
+                                                       "advisories", "bom", "mailing-list", "social", "chat",
+                                                       "documentation", "support", "distribution", "license",
+                                                       "build-meta", "build-system", "other"]))
+    comment: str = attr.ib(default=None)
+    hashes: List[CycloneDxHashObject] = attr.ib(factory=list)
+
+
+class CycloneDxComponentType(str, Enum):
+    APPLICATION = "application"
+    FRAMEWORK = "framework"
+    LIBRARY = "library"
+    CONTAINER = "container"
+    OPERATING_SYSTEM = "operating-system"
+    DEVICE = "device"
+    FIRMWARE = "firmware"
+    FILE = "file"
+
+
+@attr.s
+class CycloneDxComponent():
+    name: str = attr.ib()
+    version: str = attr.ib()
+    group: str = attr.ib()
+    description: str = attr.ib(default=None)
+    purl: str = attr.ib(default=None)
+    hashes: List[CycloneDxHashObject] = attr.ib(factory=list)
+    externalReferences: List[CycloneDxExternalRef] = attr.ib(factory=list)
+    type: CycloneDxComponentType = attr.ib(
+        default=CycloneDxComponentType.LIBRARY)
+    properties: List[CycloneDxAttribute] = attr.ib(factory=list)
+
+
+@attr.s
+class CycloneDxBom():
+    bomFormat: str = attr.ib(init=False, default="CycloneDX")
+    specVersion: str = attr.ib(init=False, default="1.3")
+    serialNumber: str = attr.ib(
+        init=False, default="urn:uuid:" + str(uuid.uuid4()))
+    version: int = attr.ib(init=False, default=1)
+    metadata: CycloneDxMetadata = attr.ib(default=CycloneDxMetadata())
+    components: List[CycloneDxComponent] = attr.ib(factory=list)
+
+
+def get_tool_header(version: str) -> dict:
+    return {
+        "vendor": "nexB Inc.",
+        "name": "scancode-toolkit",
+        "version": version
+    }
+
+def get_external_refs(package: dict)->List[CycloneDxExternalRef]:
+    ext_refs = []
+
+    url = package.get("repository_homepage_url")
+    if url is not None:
+        ext_refs.append(
+            CycloneDxExternalRef(url=url, type="website")
+        )
+
+    url = package.get("repository_download_url")
+    if url is not None:
+        ext_refs.append(
+            CycloneDxExternalRef(url=url, type="distribution")
+        )
+
+    url = package.get("api_data_url")
+    if url is not None:
+        ext_refs.append(
+            CycloneDxExternalRef(url=url, type="bom")
+        )
+
+    return ext_refs
+
+def get_hashes_list(package: dict) -> List[CycloneDxHashObject]:
+    hashes = []
+    for alg in hash_type_mapping:
+        if alg in package:
+            digest = package[alg]
+            if alg is not None:
+                hashes.append(CycloneDxHashObject(
+                    hash_type_mapping[alg], digest))
+    return hashes
+
 
 """
 Output plugin to write scan results in CycloneDX format.
-For additional information on the format, 
+For additional information on the format,
 please see https://cyclonedx.org/specification/overview/
 """
 
 
-@output_impl
+@ output_impl
 class CycloneDxOutput(OutputPlugin):
     options = [
         PluggableCommandLineOption(('--cyclonedx', 'output_cyclonedx',),
@@ -47,7 +175,7 @@ class CycloneDxOutput(OutputPlugin):
         return super().process_codebase(codebase, output_cyclonedx, **kwargs)
 
 
-@output_impl
+@ output_impl
 class CycloneDxJsonOutput(OutputPlugin):
     options = [
         PluggableCommandLineOption(('--cyclonedx-json', 'output_cyclonedx_json',),
@@ -63,10 +191,45 @@ class CycloneDxJsonOutput(OutputPlugin):
         return output_cyclonedx_json
 
     def process_codebase(self, codebase, output_cyclonedx_json, **kwargs):
-        write_results(codebase, output_file=output_cyclonedx_json,
+        bom = build_bom(codebase)
+        write_results(bom, output_file=output_cyclonedx_json,
                       cyclonedx_flavor=CycloneDxFlavor.JSON, **kwargs)
-        print(codebase)
 
 
-def write_results(codebase, output_file, cyclonedx_flavor: CycloneDxFlavor, **kwargs):
-    pass
+def build_bom(codebase) -> CycloneDxBom:
+    # TODO: find out if we can always expect that header to be present
+    scancode_header = codebase.get_headers()[0]
+
+    scancode_version = scancode_header.get("tool_version")
+
+    tool_header = get_tool_header(scancode_version)
+    bom_metadata = CycloneDxMetadata(tools=[tool_header])
+    bom = CycloneDxBom(components=generate_component_list(
+        codebase), metadata=bom_metadata)
+
+    return bom
+
+
+def write_results(bom, output_file, cyclonedx_flavor: CycloneDxFlavor, **kwargs):
+
+    close_fd = False
+    if isinstance(output_file, str):
+        output_file = open(output_file, 'w')
+        close_fd = True
+
+    json.dump(bom, output_file, default=vars)
+
+
+def generate_component_list(codebase, **kwargs) -> List[CycloneDxComponent]:
+    files = OutputPlugin.get_files(codebase, **kwargs)
+    components = []
+    for file in files:
+        for package in file["packages"]:
+            hashes = get_hashes_list(package)
+            refs = get_external_refs(package)
+
+            components.append(CycloneDxComponent(
+                name=package.get("name"), version=package.get("version"),
+                group=package.get("namespace"), purl=package.get("purl"),
+                hashes=hashes, externalReferences=refs))
+    return components
--- tests/formattedcode/test_output_cyclonedx.py
+++ tests/formattedcode/test_output_cyclonedx.py
@@ -12,6 +12,7 @@ import os
 
 from commoncode.testcase import FileDrivenTesting
 from scancode.cli_test_utils import run_scan_click
+from formattedcode.output_cyclonedx import get_hashes_list
 
 test_env = FileDrivenTesting()
 test_env.test_data_dir = os.path.join(os.path.dirname(__file__), 'data')
@@ -19,6 +20,19 @@ test_env.test_data_dir = os.path.join(os.path.dirname(__file__), 'data')
 
 def test_cyclonedx_xml():
     pass
+
+
+def test_can_get_hashes_from_package():
+    package = {
+        "md5": "cc5ea2445954a282f622042252a6eef9",
+        "sha1": "b754cee9a3d3501d32ae200e07f4e518cd8294b8",
+        "sha256": "4795dae6518d60e4e5bd7103f50f5f86f8c07cbdc36aa76f410a0d534edc8ec5",
+        "sha512": "f385a335b20e0bd933b3d59abc41e9b05f4333db6cd948b579b2562" +
+                  "d617a6fa35e13ffa0c2eceae031afcf73c5fb7e1660fade207ddf2a0e312e006eb115e9b0"}
+    hashes = get_hashes_list(package=package)
+    assert len(hashes) == 4
+    for hash_entry in hashes:
+        assert hash_entry.content is not None
 
 
 def test_cyclonedx_json():
