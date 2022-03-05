6aa09d1c88e17032a2d65b2cebbcd5ebf21e2ed1
Philippe Ombredanne <pombredanne@nexb.com>
2022-03-05 07:38:34 +0100

    Sync bootstrap scripts with skeleton
    Signed-off-by: Philippe Ombredanne <pombredanne@nexb.com>

--- configure.bat
+++ configure.bat
@@ -14,6 +14,8 @@
 @rem # Source this script for initial configuration
 @rem # Use configure --help for details
 
+@rem # NOTE: please keep in sync with POSIX script configure
+
 @rem # This script will search for a virtualenv.pyz app in etc\thirdparty\virtualenv.pyz
 @rem # Otherwise it will download the latest from the VIRTUALENV_PYZ_URL default
 @rem ################################
@@ -49,12 +51,11 @@ set "CFG_BIN_DIR=%CFG_ROOT_DIR%\%VIRTUALENV_DIR%\Scripts"
 
 @rem ################################
 @rem # Thirdparty package locations and index handling
-if exist ""%CFG_ROOT_DIR%\thirdparty"" (
-    set "PIP_EXTRA_ARGS=--find-links %CFG_ROOT_DIR%\thirdparty "
+@rem # Find packages from the local thirdparty directory or from thirdparty.aboutcode.org
+if exist "%CFG_ROOT_DIR%\thirdparty" (
+    set PIP_EXTRA_ARGS=--find-links "%CFG_ROOT_DIR%\thirdparty"
 )
-
-set "PIP_EXTRA_ARGS=%PIP_EXTRA_ARGS% --find-links https://thirdparty.aboutcode.org/pypi" & %INDEX_ARG%
-@rem ################################
+set "PIP_EXTRA_ARGS=%PIP_EXTRA_ARGS% --find-links https://thirdparty.aboutcode.org/pypi/simple/links.html"
 
 
 @rem ################################
@@ -66,7 +67,6 @@ if not defined CFG_QUIET (
 
 @rem ################################
 @rem # Main command line entry point
-set CFG_DEV_MODE=0
 set "CFG_REQUIREMENTS=%REQUIREMENTS%"
 set "NO_INDEX=--no-index"
 
@@ -76,7 +76,6 @@ if not "%1" == "" (
     if "%1" EQU "--clean"  (goto clean)
     if "%1" EQU "--dev"    (
         set "CFG_REQUIREMENTS=%DEV_REQUIREMENTS%"
-        set CFG_DEV_MODE=1
     )
     if "%1" EQU "--init"   (
         set "NO_INDEX= "
@@ -89,30 +88,32 @@ set "PIP_EXTRA_ARGS=%PIP_EXTRA_ARGS% %NO_INDEX%"
 
 
 @rem ################################
-@rem # find a proper Python to run
+@rem # Find a proper Python to run
 @rem # Use environment variables or a file if available.
 @rem # Otherwise the latest Python by default.
 if not defined PYTHON_EXECUTABLE (
     @rem # check for a file named PYTHON_EXECUTABLE
-    if exist ""%CFG_ROOT_DIR%\PYTHON_EXECUTABLE"" (
-        set /p PYTHON_EXECUTABLE=<""%CFG_ROOT_DIR%\PYTHON_EXECUTABLE""
+    if exist "%CFG_ROOT_DIR%\PYTHON_EXECUTABLE" (
+        set /p PYTHON_EXECUTABLE=<"%CFG_ROOT_DIR%\PYTHON_EXECUTABLE"
     ) else (
         set "PYTHON_EXECUTABLE=py"
     )
 )
 
+
+@rem ################################
 :create_virtualenv
 @rem # create a virtualenv for Python
 @rem # Note: we do not use the bundled Python 3 "venv" because its behavior and
 @rem # presence is not consistent across Linux distro and sometimes pip is not
 @rem # included either by default. The virtualenv.pyz app cures all these issues.
 
-if not exist ""%CFG_BIN_DIR%\python.exe"" (
+if not exist "%CFG_BIN_DIR%\python.exe" (
     if not exist "%CFG_BIN_DIR%" (
-        mkdir %CFG_BIN_DIR%
+        mkdir "%CFG_BIN_DIR%"
     )
 
-    if exist ""%CFG_ROOT_DIR%\etc\thirdparty\virtualenv.pyz"" (
+    if exist "%CFG_ROOT_DIR%\etc\thirdparty\virtualenv.pyz" (
         %PYTHON_EXECUTABLE% "%CFG_ROOT_DIR%\etc\thirdparty\virtualenv.pyz" ^
             --wheel embed --pip embed --setuptools embed ^
             --seeder pip ^
@@ -120,9 +121,9 @@ if not exist ""%CFG_BIN_DIR%\python.exe"" (
             --no-periodic-update ^
             --no-vcs-ignore ^
             %CFG_QUIET% ^
-            %CFG_ROOT_DIR%\%VIRTUALENV_DIR%
+            "%CFG_ROOT_DIR%\%VIRTUALENV_DIR%"
     ) else (
-        if not exist ""%CFG_ROOT_DIR%\%VIRTUALENV_DIR%\virtualenv.pyz"" (
+        if not exist "%CFG_ROOT_DIR%\%VIRTUALENV_DIR%\virtualenv.pyz" (
             curl -o "%CFG_ROOT_DIR%\%VIRTUALENV_DIR%\virtualenv.pyz" %VIRTUALENV_PYZ_URL%
 
             if %ERRORLEVEL% neq 0 (
@@ -136,7 +137,7 @@ if not exist ""%CFG_BIN_DIR%\python.exe"" (
             --no-periodic-update ^
             --no-vcs-ignore ^
             %CFG_QUIET% ^
-            %CFG_ROOT_DIR%\%VIRTUALENV_DIR%
+            "%CFG_ROOT_DIR%\%VIRTUALENV_DIR%"
     )
 )
 
@@ -145,6 +146,7 @@ if %ERRORLEVEL% neq 0 (
 )
 
 
+@rem ################################
 :install_packages
 @rem # install requirements in virtualenv
 @rem # note: --no-build-isolation means that pip/wheel/setuptools will not
@@ -152,18 +154,21 @@ if %ERRORLEVEL% neq 0 (
 @rem # speeds up the installation.
 @rem # We always have the PEP517 build dependencies installed already.
 
-%CFG_BIN_DIR%\pip install ^
+"%CFG_BIN_DIR%\pip" install ^
     --upgrade ^
     --no-build-isolation ^
     %CFG_QUIET% ^
     %PIP_EXTRA_ARGS% ^
     %CFG_REQUIREMENTS%
 
+
+@rem ################################
+:create_bin_junction
 @rem # Create junction to bin to have the same directory between linux and windows
 if exist "%CFG_ROOT_DIR%\%VIRTUALENV_DIR%\bin" (
     rmdir /s /q "%CFG_ROOT_DIR%\%VIRTUALENV_DIR%\bin"
 )
-mklink /J %CFG_ROOT_DIR%\%VIRTUALENV_DIR%\bin %CFG_ROOT_DIR%\%VIRTUALENV_DIR%\Scripts
+mklink /J "%CFG_ROOT_DIR%\%VIRTUALENV_DIR%\bin" "%CFG_ROOT_DIR%\%VIRTUALENV_DIR%\Scripts"
 
 if %ERRORLEVEL% neq 0 (
     exit /b %ERRORLEVEL%
@@ -173,7 +178,6 @@ exit /b 0
 
 
 @rem ################################
-
 :cli_help
     echo An initial configuration script
     echo "  usage: configure [options]"
@@ -197,6 +201,7 @@ exit /b 0
     exit /b 0
 
 
+@rem ################################
 :clean
 @rem # Remove cleanable file and directories and files from the root dir.
 echo "* Cleaning ..."
