03d4799ac44a4def7dba1eb3a0ef3a280c663e43
Philippe Ombredanne <pombredanne@nexb.com>
2022-03-09 20:39:36 +0100

    Do not depend on click. Use argparse.
    These boostrap scripts cannot depend on click.
    
    Signed-off-by: Philippe Ombredanne <pombredanne@nexb.com>

--- etc/scripts/gen_requirements.py
+++ etc/scripts/gen_requirements.py
@@ -8,37 +8,48 @@
 # See https://github.com/nexB/skeleton for support or download.
 # See https://aboutcode.org for more information about nexB OSS projects.
 #
-import click
+import argparse
+import pathlib
+
 import utils_requirements
 
+"""
+Utilities to manage requirements files.
+NOTE: this should use ONLY the standard library and not import anything else
+because this is used for boostrapping with no requirements installed.
+"""
 
-@click.command()
-@click.option(
-    "-s",
-    "--site-packages-dir",
-    type=click.Path(exists=True, readable=True, path_type=str, file_okay=False, resolve_path=True),
-    required=True,
-    metavar="DIR",
-    help='Path to the "site-packages" directory where wheels are installed such as lib/python3.6/site-packages',
-)
-@click.option(
-    "-r",
-    "--requirements-file",
-    type=click.Path(path_type=str, dir_okay=False),
-    metavar="FILE",
-    default="requirements.txt",
-    show_default=True,
-    help="Path to the requirements file to update or create.",
-)
-@click.help_option("-h", "--help")
-def gen_requirements(site_packages_dir, requirements_file):
-    """
+
+def gen_requirements():
+    description = """
     Create or replace the `--requirements-file` file FILE requirements file with all
     locally installed Python packages.all Python packages found installed in `--site-packages-dir`
     """
+    parser = argparse.ArgumentParser(description=description)
+
+    parser.add_argument(
+        "-s",
+        "--site-packages-dir",
+        dest="site_packages_dir",
+        type=pathlib.Path,
+        required=True,
+        metavar="DIR",
+        help="Path to the 'site-packages' directory where wheels are installed such as lib/python3.6/site-packages",
+    )
+    parser.add_argument(
+        "-r",
+        "--requirements-file",
+        type=pathlib.Path,
+        metavar="FILE",
+        default="requirements.txt",
+        help="Path to the requirements file to update or create.",
+    )
+
+    args = parser.parse_args()
+
     utils_requirements.lock_requirements(
-        requirements_file=requirements_file,
-        site_packages_dir=site_packages_dir,
+        site_packages_dir=args.site_packages_dir,
+        requirements_file=args.requirements_file,
     )
 
 
