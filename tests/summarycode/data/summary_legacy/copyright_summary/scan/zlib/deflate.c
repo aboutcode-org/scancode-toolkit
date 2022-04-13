20cd6c05a2b99aa97d26f0965bc594f90d5eb4f7
Jono Yang <jyang@nexb.com>
2022-04-12 18:55:04 -0700

    Remove legacy summarizer and tests
    Reference: https://github.com/nexB/scancode-toolkit/issues/2842
    Signed-off-by: Jono Yang <jyang@nexb.com>

--- tests/summarycode/data/summary_legacy/copyright_summary/scan/zlib/deflate.c
+++
@@ -1,5 +0,0 @@
-/* deflate.c -- compress data using the deflation algorithm
- * Copyright (C) 1995-2013 Jean-loup Gailly and Mark Adler
- * For conditions of distribution and use, see copyright notice in zlib.h
- */
-
