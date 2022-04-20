91f143eb567002c0f06ae61795d68606344bd12f
Philippe Ombredanne <pombredanne@nexb.com>
2022-04-20 11:14:21 +0200

    Test that no file name exceeds max len of 143
    LUCKS and most Linux encrypted FS only support up to 143-long file names
    and long names will make it impossible to install or checkout ScanCode
    
    Reference: https://github.com/nexB/scancode-toolkit/issues/2901
    Reported-by: Michael McMahon <michael@gnu.org>
    Signed-off-by: Philippe Ombredanne <pombredanne@nexb.com>

--- tests/scancode/test_scancode_checks.py
+++ tests/scancode/test_scancode_checks.py
@@ -12,17 +12,16 @@ from os.path import dirname
 import subprocess
 import sys
 import unittest
-
+from commoncode.fileutils import resource_iter
 
 """
-This test suite runs code checks such as:
+This test suite runs various code checks such as:
  - code style
  - ABOUT files
  - release archives creation
- 
+
 NOTE: it has imports limited to the standard library by design
 """
-
 
 root_dir = dirname(dirname(dirname(__file__)))
 
@@ -31,6 +30,7 @@ on_linux = str(sys.platform).lower().startswith('linux')
 
 @unittest.skipIf(not on_linux, 'Check about files only on one OS')
 class TestCheckAboutFiles(unittest.TestCase):
+
     def test_about_files_src(self):
         subprocess.check_output('venv/bin/about check src/'.split(), cwd=root_dir)
 
@@ -50,3 +50,18 @@ class TestCheckCode(unittest.TestCase):
             'venv/bin/pycodestyle --ignore E501,W503,W504,W605 '
             '--exclude=lib,lib64,thirdparty,'
             'docs,bin,migrations,settings,local,tmp .'.split(), cwd=root_dir)
+
+
+@unittest.skipIf(not on_linux, 'Check file length only on one OS')
+class TestFilenameMaxLength(unittest.TestCase):
+
+    def test_file_name_are_not_too_long(self):
+
+        # See https://unix.stackexchange.com/questions/32795/what-is-the-maximum-allowed-filename-and-folder-size-with-ecryptfs
+        # 143 is the max filename length that luks and ecryptfs support!
+
+        long_filenames = [r for r in resource_iter('src') if len(r.split('/')[-1]) > 143]
+        long_filenames.extend(r for r in resource_iter('tests') if len(r.split('/')[-1]) > 143)
+        if long_filenames:
+            msg = '\n'.join(long_filenames)
+            raise Exception(f'These filenames are too long (over 143 characters):\n{msg}')
