dd93bd7d0a2fb87ccdee31047bb42dc0227d7652
John M. Horan <johnmhoran@gmail.com>
2022-05-16 17:59:22 -0700

    Save test code comments #2945
    Signed-off-by: John M. Horan <johnmhoran@gmail.com>

--- tests/summarycode/test_file_cat.py
+++ tests/summarycode/test_file_cat.py
@@ -10,6 +10,8 @@
 import io
 import os
 
+import attr
+
 from commoncode.testcase import FileBasedTesting
 from commoncode.testcase import FileDrivenTesting
 from commoncode.resource import Resource
@@ -17,7 +19,21 @@ from commoncode.resource import Resource
 from scancode.cli_test_utils import run_scan_click
 from scancode.cli_test_utils import check_json_scan
 from scancode_config import REGEN_TEST_FIXTURES
+from scancode.plugin_info import InfoScanner
 from summarycode import file_cat
+
+
+resource_class = attr.make_class(
+        name='TestResource',
+        attrs=InfoScanner.resource_attributes,
+        # attrs={
+        #     "mime_type": attr.ib(default=None, repr=False),
+        #     "file_type": attr.ib(default=None, repr=False),
+        #     "programming_language": attr.ib(default=None, repr=False)
+        # },
+        slots=True,
+        bases=(Resource,)
+    )
 
 
 class TestFileCat(FileBasedTesting):
@@ -25,15 +41,56 @@ class TestFileCat(FileBasedTesting):
     test_data_dir = os.path.join(os.path.dirname(__file__), 'data')
 
     def test_ArchiveAndroid(self):
-        test_resource = Resource(
-            path='',
-            is_file='file',
+    # def test_ArchiveAndroid(self, resource_class):
+        test_resource_01 = resource_class(
             name='baloney.apk',
+            location='',
+            path='',
+            rid='',
+            pid='',
+            is_file='file',
             mime_type='',
             file_type='',
             programming_language=''
         )
-        assert file_cat.ArchiveAndroid.categorize(test_resource)
+        assert file_cat.ArchiveAndroid.categorize(test_resource_01)
+
+
+    # Test with commented-out efforts:
+
+    # def test_ArchiveAndroid(self):
+    # # def test_ArchiveAndroid(self, test_resource_class):
+    #     test_resource_class = attr.make_class(
+    #             name='TestResource',
+    #             # attrs=self.resource_attributes or {},
+    #             # attrs=Codebase.resource_attributes or {},
+    #             # attrs=Codebase.resource_attributes,
+    #             # attrs=Codebase._populate(self),
+    #             attrs={
+    #                 "mime_type": attr.ib(default=None, repr=False),
+    #                 "file_type": attr.ib(default=None, repr=False),
+    #                 "programming_language": attr.ib(default=None, repr=False)
+    #             },
+    #             slots=True,
+    #             # frozen=True,
+    #             bases=(Resource,)
+    #         )
+
+    #     test_resource = test_resource_class(
+    #     # test_resource = self.test_resource_class(
+    #         name='baloney.apk',
+    #         location='',
+    #         path='',
+    #         rid='',
+    #         pid='',
+    #         is_file='file',
+    #         mime_type='',
+    #         file_type='',
+    #         programming_language=''
+    #     )
+    #     assert file_cat.ArchiveAndroid.categorize(test_resource)
+
+
 
 # Original test from SPATS file-cat:
 
