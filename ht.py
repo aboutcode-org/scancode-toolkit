5471789dbf7a621eb0e8e25080c78335ba040989
Philippe Ombredanne <pombredanne@nexb.com>
2022-09-13 09:10:42 +0200

    Revert commit of test files
    Signed-off-by: Philippe Ombredanne <pombredanne@nexb.com>

--- ht.py
+++
@@ -1,7 +0,0 @@
-from licensedcode.cache import get_index
-idx = get_index()
-file= "sample.license.txt"
-matches = idx.match(location=file)
-for match in matches:
-    ht = match.get_highlighted_text()
-    print(ht)
