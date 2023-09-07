6b94d049a44b992e2f31bb88982c59987095ed6c
Jay <jaykumar20march@gmail.com>
2023-09-08 02:01:03 +0530

    add: added files for testing.
    Signed-off-by: Jay <jaykumar20march@gmail.com>

---
+++ tests/fails/test1.py
@@ -0,0 +1,44 @@
+import os
+
+from commoncode.testcase import FileBasedTesting
+
+from licensedcode import index
+from licensedcode.index import LicenseIndex
+from licensedcode.match_unknown import match_unknowns
+from licensedcode.models import get_all_spdx_key_tokens, load_licenses, get_license_tokens, get_rules
+from licensedcode.query import build_query
+from licensedcode_test_utils import mini_legalese
+
+
+def MiniLicenseIndex(*args, **kwargs):
+    return index.LicenseIndex(*args, _legalese=mini_legalese, **kwargs)
+
+
+TEST_DATA_DIR = os.path.join(os.path.dirname(__file__), 'data')
+
+
+class TestFailed(FileBasedTesting):
+    test_data_dir = TEST_DATA_DIR
+
+    def test_stable_firmwar_realtek_copyright_detailed_expected_yml(self):
+        rule_dir = self.get_test_loc('rules/')
+        license_dir = self.get_test_loc('licenses/')
+        licenses_db = load_licenses(license_dir)
+        rules = list(get_rules(licenses_db=licenses_db, rules_data_dir=rule_dir))
+        spdx_tokens = set(get_all_spdx_key_tokens(licenses_db))
+        license_tokens = set(get_license_tokens())
+        idx = LicenseIndex(
+            rules=rules,
+            _spdx_tokens=spdx_tokens,
+            _license_tokens=license_tokens,
+        )
+
+        query_loc = self.get_test_loc('debian/stable_firmware-realtek.copyright')
+        qry = build_query(location=query_loc, idx=idx)
+
+        match = match_unknowns(
+            idx=idx,
+            query_run=qry.whole_query_run(),
+            automaton=idx.unknown_automaton,
+        )
+        match.set_lines(qry.line_by_pos)
