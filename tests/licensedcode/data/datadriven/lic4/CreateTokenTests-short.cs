15f6e47e5495724a27b4852c82d5a20f66303d29
Philippe Ombredanne <pombredanne@nexb.com>
2023-10-11 00:54:15 +0200

    Add license detection test
    Reference: https://github.com/nexB/scancode-toolkit/issues/3532
    Reported-by: Omair Majid @omajid
    Signed-off-by: Philippe Ombredanne <pombredanne@nexb.com>

---
+++ tests/licensedcode/data/datadriven/lic4/CreateTokenTests-short.cs
@@ -0,0 +1,8 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using System.Security.Claims;
+using BenchmarkDotNet.Attributes;
+using Microsoft.IdentityModel.JsonWebTokens;
+using Microsoft.IdentityModel.TestUtils;
+using Microsoft.IdentityModel.Tokens;
