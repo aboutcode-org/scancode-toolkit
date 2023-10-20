0d20d10c16a9fce1420719a4134d241d062e03f1
Adrien Schildknecht <adrs@fb.com>
2023-10-20 02:23:46 +0200

    npm: more tests for alias in yarn.lock v1
    An assert in the code was making aliases using `@` fail.
    
    Signed-off-by: Adrien Schildknecht <adrs@fb.com>

--- src/packagedcode/npm.py
+++ src/packagedcode/npm.py
@@ -623,11 +623,13 @@ class YarnLockV1Handler(BaseNpmHandler):
                 elif not line.startswith(' ') and stripped.endswith(':'):
                     # the first line of a dependency has the name and requirements
                     # "@babel/core@^7.1.0", "@babel/core@^7.3.4":
+                    # For aliases: "@alias@npm:@package@^12":
                     requirements = stripped.strip(':').split(', ')
                     requirements = [r.strip().strip("\"'") for r in requirements]
                     for req in requirements:
                         if req.startswith('@'):
-                            assert req.count('@') == 2
+                            # 2 = package, 4 = alias
+                            assert req.count('@') in [2, 4]
 
                         ns_name, _, constraint = req.rpartition('@')
                         ns, _ , name = ns_name.rpartition('/')
