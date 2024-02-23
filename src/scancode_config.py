cfe6d1350b90404ae40ff998f623604854bd2655
Ayan Sinha Mahapatra <ayansmahapatra@gmail.com>
2024-02-23 20:17:55 +0530

    Update CHANGELOG with new SPDX License List
    Signed-off-by: Ayan Sinha Mahapatra <ayansmahapatra@gmail.com>

--- src/scancode_config.py
+++ src/scancode_config.py
@@ -144,7 +144,7 @@ __release_date__ = datetime.datetime(2023, 10, 11)
 __output_format_version__ = '3.1.0'
 
 #
-spdx_license_list_version = '3.22'
+spdx_license_list_version = '3.23'
 
 ################################################################################
 # USAGE MODE-, INSTALLATION- and IMPORT- and RUN-SPECIFIC DIRECTORIES
