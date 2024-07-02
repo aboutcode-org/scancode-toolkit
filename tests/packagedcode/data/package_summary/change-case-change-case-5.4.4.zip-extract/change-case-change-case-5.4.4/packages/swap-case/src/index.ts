e60fca8806ec263ec8586d2bba93e842bf5bb312
swastik <swastkk@gmail.com>
2024-07-02 11:38:37 +0530

    update: Remove unnecessary data in testfiles that are irrelevant, #3817
    Signed-off-by: swastik <swastkk@gmail.com>

--- tests/packagedcode/data/package_summary/change-case-change-case-5.4.4.zip-extract/change-case-change-case-5.4.4/packages/swap-case/src/index.ts
+++
@@ -1,8 +0,0 @@
-export function swapCase(input: string, locale?: string[] | string) {
-  let result = "";
-  for (const char of input) {
-    const lower = char.toLocaleLowerCase(locale);
-    result += char === lower ? char.toLocaleUpperCase(locale) : lower;
-  }
-  return result;
-}
