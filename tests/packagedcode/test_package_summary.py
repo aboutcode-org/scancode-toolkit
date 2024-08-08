150967c63d1c763e33ca17c9b903fc7c981621af
swastik <swastkk@gmail.com>
2024-08-08 07:08:52 +0530

    Add Python whl package ecosystem test in test_package_summary #3889
    Signed-off-by: swastik <swastkk@gmail.com>

--- tests/packagedcode/test_package_summary.py
+++ tests/packagedcode/test_package_summary.py
@@ -14,20 +14,14 @@ class TestPackageSummary(PackageTester):
         
         run_scan_click(['--package', '--classify', '--package-summary', test_dir, '--json', result_file])
         check_json_scan(expected_file, result_file, remove_uuid=True, regen=REGEN_TEST_FIXTURES)
-    #     test_dir = self.get_test_loc('package_summary/change-case-change-case-5.4.4.zip-extract') 
-    #     result_file = self.get_temp_file('json')
-    #     expected_file = self.get_test_loc('package_summary/expected.json')
-
-    #     run_scan_click(['--package','--license','--copyright', '--strip-root', '--processes', '-1', '--package-summary', '--classify', '--json-pp', result_file, test_dir])
-    #     check_json_scan(expected_file, result_file, remove_uuid=True, remove_file_date=True, regen=REGEN_TEST_FIXTURES)
     
-    # def test_py_whl_ecosystem_with_package_summary(self):
-    #     test_dir = self.get_test_loc('package_summary/python.whl-extract') 
-    #     result_file = self.get_temp_file('json')
-    #     expected_file = self.get_test_loc('package_summary/python-whl-expected.json')
+    def test_package_summary_for_python_whl(self):
+        test_dir = self.get_test_loc('package_summary/python_whl') 
+        result_file = self.get_temp_file('json')
+        expected_file = self.get_test_loc('package_summary/python_whl-expected.json')
 
-    #     run_scan_click(['--package','--license','--copyright', '--strip-root', '--processes', '-1', '--package-summary', '--summary' , '--classify', '--json-pp', result_file, test_dir])
-    #     check_json_scan(expected_file, result_file, remove_uuid=True, remove_file_date=True, regen=REGEN_TEST_FIXTURES)
+        run_scan_click(['--package','--license','--copyright', '--strip-root', '--processes', '-1', '--package-summary', '--summary' , '--classify', '--json-pp', result_file, test_dir])
+        check_json_scan(expected_file, result_file, remove_uuid=True, remove_file_date=True, regen=REGEN_TEST_FIXTURES)
     
     # # Package Attribute tests
     # def test_package_summary__does_summarize_npm_copyright(self): 
