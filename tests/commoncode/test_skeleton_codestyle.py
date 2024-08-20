b9474b9f9baf0ae8146d9ff0bf8f18cf5f1a5559
Chin Yeung Li <tli@nexb.com>
2024-08-20 16:46:20 +0800

    Update link references of ownership from nexB to aboutcode-org
    Signed-off-by: Chin Yeung Li <tli@nexb.com>

--- tests/commoncode/test_skeleton_codestyle.py
+++ tests/commoncode/test_skeleton_codestyle.py
@@ -3,7 +3,7 @@
 # ScanCode is a trademark of nexB Inc.
 # SPDX-License-Identifier: Apache-2.0
 # See http://www.apache.org/licenses/LICENSE-2.0 for the license text.
-# See https://github.com/nexB/skeleton for support or download.
+# See https://github.com/aboutcode-org/skeleton for support or download.
 # See https://aboutcode.org for more information about nexB OSS projects.
 #
 
