24e18d3b117522c002c53706b9120c5d1e516855
Asim kumar <aktech701@gmail.com>
2025-01-15 19:23:35 +0000

    added documentation_url to the Package class
    Signed-off-by: Asim kumar <aktech701@gmail.com>

--- src/packagedcode/models.py
+++ src/packagedcode/models.py
@@ -1639,10 +1639,11 @@ class Package(PackageData):
         mapping.pop('package_uid', None)
         mapping.pop('datafile_paths', None)
         mapping.pop('datasource_ids', None)
+        mapping.pop('documentation_url', None)
         return PackageData.from_dict(mapping)
 
     @classmethod
-    def from_package_data(cls, package_data, datafile_path=None, package_only=False):
+    def from_package_data(cls, package_data, datafile_path=None, package_only=False , documentation_url=None):
         """
         Return a Package from a ``package_data`` PackageData object
         or mapping. Or None.
@@ -1666,6 +1667,8 @@ class Package(PackageData):
                 for license_match in detection['matches']:
                     if not license_match['from_file']:
                         license_match['from_file'] = datafile_path
+        if documentation_url:
+            package_data_mapping['documentation_url'] = documentation_url
 
         package = cls.from_dict(package_data_mapping)
 
