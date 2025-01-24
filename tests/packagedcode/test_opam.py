ae1ef4068a7610d2a8818bad577ec9ab054e695c
Shivam11702 <shivam.singh.05.09.03@gmail.com>
2025-01-24 10:31:07 +0530

    Transformed the whole comment block of https://github.com/aboutcode-org/scancode-toolkit/blob/develop/tests/packagedcode/test_opam.py  in test function.
    Signed-off-by: Shivam11702 <shivam.singh.05.09.03@gmail.com>

--- tests/packagedcode/test_opam.py
+++ tests/packagedcode/test_opam.py
@@ -1,11 +1,17 @@
+def test_license_info():
+    license_text = """
+    Copyright (c) nexB Inc. and others. All rights reserved.
+    ScanCode is a trademark of nexB Inc.
+    SPDX-License-Identifier: Apache-2.0
+    See http://www.apache.org/licenses/LICENSE-2.0 for the license text.
+    See https://github.com/nexB/scancode-toolkit for support or download.
+    See https://aboutcode.org for more information about nexB OSS projects.
+    """
 
-# Copyright (c) nexB Inc. and others. All rights reserved.
-# ScanCode is a trademark of nexB Inc.
-# SPDX-License-Identifier: Apache-2.0
-# See http://www.apache.org/licenses/LICENSE-2.0 for the license text.
-# See https://github.com/nexB/scancode-toolkit for support or download.
-# See https://aboutcode.org for more information about nexB OSS projects.
-#
+    assert "ScanCode is a trademark of nexB Inc." in license_text
+    assert "SPDX-License-Identifier: Apache-2.0" in license_text
+    assert "https://github.com/nexB/scancode-toolkit" in license_text
+    assert "http://www.apache.org/licenses/LICENSE-2.0" in license_text
 
 import os
 
