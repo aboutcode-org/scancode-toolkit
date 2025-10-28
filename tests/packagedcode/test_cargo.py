52499671f8d4269223ec0cfccebfd419b1aef016
Om Santosh Suneri <142336291+omsuneri@users.noreply.github.com>
2025-10-28 23:27:37 +0530

    refactor: test with a full --package scan
    Signed-off-by: Om Santosh Suneri <142336291+omsuneri@users.noreply.github.com>

--- tests/packagedcode/test_cargo.py
+++ tests/packagedcode/test_cargo.py
@@ -85,9 +85,12 @@ class TestCargo(PackageTester):
 
     def test_parse_cargo_toml_single_file_no_crash(self):
         test_file = self.get_test_loc('cargo/cargo_toml/single-file-scan/Cargo.toml')
-        expected_loc = self.get_test_loc('cargo/cargo_toml/single-file-scan/Cargo.toml.expected')
-        packages_data = cargo.CargoTomlHandler.parse(test_file)
-        self.check_packages_data(packages_data, expected_loc, regen=REGEN_TEST_FIXTURES)
+        expected_file = self.get_test_loc('cargo/cargo_toml/single-file-scan/Cargo.toml.expected')
+        result_file = self.get_temp_file('results.json')
+        run_scan_click(['--package', test_file, '--json', result_file])
+        check_json_scan(
+            expected_file, result_file, remove_uuid=True, regen=REGEN_TEST_FIXTURES
+        )
 
     def test_parse_cargo_toml_tauri_workspace_in_version(self):
         test_file = self.get_test_loc('cargo/cargo_toml/tauri-examples/Cargo.toml')
