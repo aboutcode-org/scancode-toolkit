c0df0e9f4e412c79173b7c70a6d6e931b39e4fa0
karthiknew07 <karthik2005207@gmail.com>
2025-11-13 09:07:44 +0530

    Add .SRCINFO parser for Arch Linux packages
    Closes #4406
    
    Signed-off-by: karthiknew07 <karthik2005207@gmail.com>

---
+++ tests/packagedcode/test_srcinfo.py
@@ -0,0 +1,75 @@
+#
+# Copyright (c) nexB Inc. and others. All rights reserved.
+# ScanCode is a trademark of nexB Inc.
+# SPDX-License-Identifier: Apache-2.0
+#
+
+import os
+
+from packagedcode import srcinfo
+from packages_test_utils import PackageTester
+
+
+class TestSrcinfo(PackageTester):
+    test_data_dir = os.path.join(os.path.dirname(__file__), 'data')
+
+    def test_parse_srcinfo_basic(self):
+        test_file = self.get_test_loc('srcinfo/rust-basic/.SRCINFO')
+        packages = list(srcinfo.SrcinfoHandler.parse(test_file))
+        
+        assert len(packages) == 1
+        package = packages[0]
+        
+        assert package.type == 'arch'
+        assert package.name == 'rust-basic'
+        assert '1.0.0' in package.version
+        assert package.description
+        
+    def test_parse_srcinfo_with_dependencies(self):
+        test_file = self.get_test_loc('srcinfo/with-deps/.SRCINFO')
+        packages = list(srcinfo.SrcinfoHandler.parse(test_file))
+        
+        assert len(packages) == 1
+        package = packages[0]
+        
+        # Should have runtime, build, and optional dependencies
+        runtime_deps = [d for d in package.dependencies if d.is_runtime and not d.is_optional]
+        build_deps = [d for d in package.dependencies if not d.is_runtime]
+        opt_deps = [d for d in package.dependencies if d.is_optional]
+        
+        assert len(runtime_deps) > 0
+        assert len(build_deps) > 0
+        assert len(opt_deps) > 0
+
+    def test_parse_srcinfo_arch_specific(self):
+        test_file = self.get_test_loc('srcinfo/arch-specific/.SRCINFO')
+        packages = list(srcinfo.SrcinfoHandler.parse(test_file))
+        
+        assert len(packages) == 1
+        package = packages[0]
+        
+        # Check for architecture-specific dependencies
+        arch_specific = [d for d in package.dependencies if 'x86_64' in d.scope or 'aarch64' in d.scope]
+        assert len(arch_specific) > 0
+
+    def test_parse_srcinfo_split_package(self):
+        """Test parsing split packages (multiple pkgname sections)"""
+        test_file = self.get_test_loc('srcinfo/split-package/.SRCINFO')
+        packages = list(srcinfo.SrcinfoHandler.parse(test_file))
+        
+        # Should yield multiple packages
+        assert len(packages) > 1
+        
+        # All should share same pkgbase but different pkgname
+        names = [p.name for p in packages]
+        assert len(set(names)) == len(names)  # All unique
+
+    def test_parse_srcinfo_with_checksums(self):
+        """Test that checksums are captured in extra_data"""
+        test_file = self.get_test_loc('srcinfo/with-checksums/.SRCINFO')
+        packages = list(srcinfo.SrcinfoHandler.parse(test_file))
+        
+        assert len(packages) == 1
+        package = packages[0]
+        
+        assert 'sha256sums' in package.extra_data or 'md5sums' in package.extra_data
