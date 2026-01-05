2324db5a34db975a68f01bb2281c5cd66ac77a98
Jono Yang <jyang@nexb.com>
2026-01-05 14:02:05 -0800

    Add arguments to provide custom training data to gibberish detector training #2402
    Signed-off-by: Jono Yang <jyang@nexb.com>

--- src/textcode/train_gibberish_model.py
+++ src/textcode/train_gibberish_model.py
@@ -13,12 +13,43 @@ from commoncode.cliutils import PluggableCommandLineOption
 from textcode.gibberish import Gibberish
 
 @click.command(name='scancode-train-gibberish-model')
+@click.option(
+    '--big',
+    type=click.Path(exists=True, readable=True, dir_okay=False, resolve_path=True, path_type=str),
+    default=None,
+    help='Text file containing main training corpus for the gibberish detector',
+    cls=PluggableCommandLineOption,
+)
+@click.option(
+    '--good',
+    type=click.Path(exists=True, readable=True, dir_okay=False, resolve_path=True, path_type=str),
+    default=None,
+    help='Text file containing text considered to be not gibberish (good)',
+    cls=PluggableCommandLineOption,
+)
+@click.option(
+    '--bad',
+    type=click.Path(exists=True, readable=True, dir_okay=False, resolve_path=True, path_type=str),
+    default=None,
+    help='Text file containing text considered to be gibberish (bad)',
+    cls=PluggableCommandLineOption,
+)
 @click.help_option('-h', '--help')
-def train_gibberish_model(*args, **kwargs,):
+def train_gibberish_model(big, good, bad, *args, **kwargs,):
     """Train model used by textcode.Gibberish to detect gibberish"""
+    gibberish_detector_train_args = {}
+    if big:
+        gibberish_detector_train_args["bigfile"] = big
+    if good:
+        gibberish_detector_train_args["goodfile"] = good
+    if bad:
+        gibberish_detector_train_args["badfile"] = bad
+
     click.echo('Training gibberish detector model...')
     gibberish_detector = Gibberish()
-    gibberish_detector.train()
+    gibberish_detector.train(
+        **gibberish_detector_train_args
+    )
 
 
 if __name__ == '__main__':
