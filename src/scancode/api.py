a03160d2aa1116ec6624b415725c2015e58f0064
NAIDU YASWANTH REDDY <yeswanthreddynaidu@gmail.com>
2026-01-14 21:03:26 +0530

    Fix stale URLs pointing to nexB instead of aboutcode-org
    Signed-off-by: naiduyaswanthreddy <yeswanthreddynaidu@gmail.com>

--- src/scancode/api.py
+++ src/scancode/api.py
@@ -142,7 +142,7 @@ def get_urls(location, threshold=50, **kwargs):
 SPDX_LICENSE_URL = 'https://spdx.org/licenses/{}'
 DEJACODE_LICENSE_URL = 'https://enterprise.dejacode.com/urn/urn:dje:license:{}'
 SCANCODE_LICENSEDB_URL = 'https://scancode-licensedb.aboutcode.org/{}'
-SCANCODE_DATA_BASE_URL = 'https://github.com/nexB/scancode-toolkit/tree/develop/src/licensedcode/data'
+SCANCODE_DATA_BASE_URL = 'https://github.com/aboutcode-org/scancode-toolkit/tree/develop/src/licensedcode/data'
 SCANCODE_LICENSE_URL = f'{SCANCODE_DATA_BASE_URL}/licenses/{{}}.LICENSE'
 SCANCODE_RULE_URL = f'{SCANCODE_DATA_BASE_URL}/rules/{{}}'
 
